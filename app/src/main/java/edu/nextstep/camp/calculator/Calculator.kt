73000b7ccf37782e2d6066fa512a7b0e0b814d42
Joseph <wjsdudtkd95@gmail.com>
2021-07-21 08:02:01 +0900

    test(calculator): 나머지 사칙연산들트을 테스

--- app/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
@@ -3,4 +3,10 @@ package edu.nextstep.camp.calculator
 class Calculator {
 
     fun plus(number1: Int, number2: Int) = number1 + number2
+
+    fun minus(number1: Int, number2: Int) = number1 - number2
+
+    fun multiply(number1: Int, number2: Int) = number1 * number2
+
+    fun divide(number1: Int, number2: Int) = number1 / number2
 }
--- app/src/test/java/edu/nextstep/camp/calculator/CalculatorTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/CalculatorTest.kt
@@ -12,5 +12,24 @@ class CalculatorTest {
         Truth.assertThat(result).isEqualTo(13)
     }
 
+    @Test
+    fun `6과 7을 minus함수로 빼면 -1이 나와야 한다`() {
+        val calculator = Calculator()
+        val result = calculator.minus(6, 7)
+        Truth.assertThat(result).isEqualTo(-1)
+    }
 
+    @Test
+    fun `6과 7을 multiply함수로 곱하면 42 나와야 한다`() {
+        val calculator = Calculator()
+        val result = calculator.multiply(6, 7)
+        Truth.assertThat(result).isEqualTo(42)
+    }
+
+    @Test
+    fun `42와 6을 divide함수로 나누면 7이 나와야 한다`() {
+        val calculator = Calculator()
+        val result = calculator.divide(42, 6)
+        Truth.assertThat(result).isEqualTo(7)
+    }
 }
