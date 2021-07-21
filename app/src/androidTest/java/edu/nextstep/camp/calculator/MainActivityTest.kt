ee19713f5083cdcd7b13fb03e72eb6411f01cd3b
강현석 <kekemusa37@gmail.com>
2021-07-21 21:37:35 +0900

    test(MainActivity) : 추가 - '1' 버튼을 누르면 '1'이 보여야한다 함수

---
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package edu.nextstep.camp.calculator
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun pressedOneButton_shouldShowOne() {
+        onView(withId(R.id.button1)).perform(click())
+
+        onView(withId(R.id.textView)).check(matches(withText("1")))
+    }
+}
