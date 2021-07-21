bfa5fc5668b28a9dad84f9040b08bf739cf462de
강현석 <kekemusa37@gmail.com>
2021-07-21 21:41:04 +0900

    test(MainActivity) : 수정 - '1' 버튼을 누르면 '1'이 보여야한다 테스트 성공

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -11,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            binding.textView.text = "1"
+        }
     }
 }
