8072300e282606a68c1f8d28fc118983961c17dd
Malibin <nightmare73@naver.com>
2021-07-23 00:11:23 +0900

    [Feat] Operator 객체 작성

---
+++ domain/src/test/java/edu/nextstep/camp/domain/stringcalculator/OperatorTest.kt
@@ -0,0 +1,83 @@
+package edu.nextstep.camp.domain.stringcalculator
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+
+/**
+ * Created By Malibin
+ * on 7월 22, 2021
+ */
+
+class OperatorTest {
+    @Test
+    fun `연산자 목록에 없는 문자가 들어오면 Exception`() {
+        // given
+        val weirdToken = "#"
+
+        // then
+        assertThrows(IllegalArgumentException::class.java) { Operator.find(weirdToken) }
+            .also { assertThat(it).hasMessageThat().contains("$weirdToken 을 가진 연산자를 찾을 수 없습니다.") }
+    }
+}
+
+@RunWith(Parameterized::class)
+class OperatorFindParameterTest(
+    private val operatorToken: String,
+    private val expectedOperator: Operator,
+) {
+    companion object {
+        @JvmStatic
+        @Parameters(name = "{0} 토큰으로 {1} 연산자를 찾는다")
+        fun tokenAndOperators(): Collection<Array<Any>> {
+            return listOf(
+                arrayOf("+", Operator.PLUS),
+                arrayOf("-", Operator.MINUS),
+                arrayOf("*", Operator.MULTIPLY),
+                arrayOf("/", Operator.DIVIDE),
+            )
+        }
+    }
+
+    @Test
+    fun `입력 문자에 맞는 연산자를 찾는다`() {
+        // when
+        val actualOperator = Operator.find(operatorToken)
+
+        // then
+        assertThat(actualOperator).isEqualTo(expectedOperator)
+    }
+}
+
+@RunWith(Parameterized::class)
+class OperatorCalculateParameterTest(
+    private val operator: Operator,
+    private val left: Int,
+    private val right: Int,
+    private val expectedResult: Int,
+) {
+    companion object {
+        @JvmStatic
+        @Parameters(name = "{0} 연산자의 계산 전략 수행 테스트 : {1} {0} {2} = {3}")
+        fun tokenAndOperators(): Collection<Array<Any>> {
+            return listOf(
+                arrayOf(Operator.PLUS, 1, 2, 3),
+                arrayOf(Operator.MINUS, 3, 2, 1),
+                arrayOf(Operator.MULTIPLY, 2, 5, 10),
+                arrayOf(Operator.DIVIDE, 10, 2, 5),
+            )
+        }
+    }
+
+    @Test
+    fun `각 연산자에 맞는 계산 전략에 맞게 계산을 수행한다`() {
+        // when
+        val actualResult = operator.calculate(left, right)
+
+        // then
+        assertThat(actualResult).isEqualTo(expectedResult)
+    }
+}
