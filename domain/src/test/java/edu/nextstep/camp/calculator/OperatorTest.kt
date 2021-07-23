0411c3523c890d836d46eea90da41d0f992fc1ec
강현석 <kekemusa37@gmail.com>
2021-07-23 21:30:35 +0900

    :white_check_mark: test(operator) : 추가 - 두 개의 피연산자를 더합니다 테스트 함수

---
+++ domain/src/test/java/edu/nextstep/camp/calculator/OperatorTest.kt
@@ -0,0 +1,15 @@
+package edu.nextstep.camp.calculator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class OperatorTest {
+
+    private val firstOperand = 20.0
+    private val secondOperand = 10.0
+
+    @Test
+    fun `두 개의 피연산자를 더합니다`() {
+        assertThat(firstOperand + secondOperand).isEqualTo(30.0)
+    }
+}
