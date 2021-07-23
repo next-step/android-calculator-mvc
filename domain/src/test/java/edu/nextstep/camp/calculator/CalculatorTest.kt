fcc773e8435364aa87c08741113430f90abdcde2
강현석 <kekemusa37@gmail.com>
2021-07-23 23:45:59 +0900

    :recycle: refact(calculator) : 구현 - calculate 함수

--- domain/src/test/java/edu/nextstep/camp/calculator/CalculatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/CalculatorTest.kt
@@ -5,8 +5,46 @@ import org.junit.jupiter.api.Test
 
 object Calculator {
 
+    private const val ASCII_ZERO = 48
+    private const val ASCII_NINE = 59
+    private val ASCII_NUMBER_RANGE = ASCII_ZERO..ASCII_NINE
+
     fun calculate(formula: String): Double {
-        return 10.0
+        val (numberGroup: List<Double>, operatorGroup: List<Operator>) =
+            mutableListOf<Double>() to mutableListOf<Operator>()
+
+        val numberBuilder = StringBuilder()
+        formula.forEachIndexed { index, item ->
+            when {
+                index == formula.lastIndex -> {
+                    numberBuilder.append(item)
+                    numberGroup.add(
+                        numberBuilder
+                            .toString()
+                            .toDouble()
+                    )
+                }
+                item.code in ASCII_NUMBER_RANGE -> {
+                    numberBuilder.append(item)
+                }
+                else -> {
+                    numberGroup.add(
+                        numberBuilder
+                            .toString()
+                            .toDouble()
+                    )
+                    numberBuilder.clear()
+                    operatorGroup.add(Operator.findBySymbol(item))
+                }
+            }
+        }
+        var result = numberGroup.removeAt(0)
+        operatorGroup
+            .zip(numberGroup)
+            .map {
+                result = it.first.formula(result, it.second)
+            }
+        return result
     }
 }
 
