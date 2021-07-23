b97eb3cefa74802c5d0c9b08086b0ee993be729b
강현석 <kekemusa37@gmail.com>
2021-07-24 00:45:19 +0900

    :recycle: test(groupings) : 정리 - 수식 중에서 숫자들만 그룹화합니다 테스트 함수

--- domain/src/test/java/edu/nextstep/camp/calculator/GroupingsTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/GroupingsTest.kt
@@ -1,26 +1,14 @@
 package edu.nextstep.camp.calculator
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 internal class GroupingsTest {
 
-    @Test
-    fun `수식 중에서 숫자들만 그룹화합니다`() {
-        val formula = "2+3*4/2"
-        val numberGroup = listOf(2.0, 3.0, 4.0, 2.0)
-        val actual = Groupings.numberGroup(formula)
-
-        actual
-            .zip(numberGroup)
-            .forEach {
-                assertThat(it.first).isEqualTo(it.second)
-            }
-    }
-
-    @Test
-    fun `수식에 공백이 들어있어도 숫자들만 그룹화합니다`() {
-        val formula = "2 + 3 * 4 / 2"
+    @ParameterizedTest
+    @ValueSource(strings = ["2+3*4/2", "2 + 3 * 4 / 2"])
+    fun `수식 중에서 숫자들만 그룹화합니다`(formula: String) {
         val numberGroup = listOf(2.0, 3.0, 4.0, 2.0)
         val actual = Groupings.numberGroup(formula)
 
