195c3253ec8e71759614dd22d2b0a1f33cc084d7
강현석 <kekemusa37@gmail.com>
2021-07-24 00:32:20 +0900

    :sparkles: feat(groupings) : 구현 - Groupings

--- domain/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
@@ -2,13 +2,9 @@ package edu.nextstep.camp.calculator
 
 object Calculator {
 
-    private const val ASCII_ZERO = 48
-    private const val ASCII_NINE = 59
-    private val ASCII_NUMBER_RANGE = ASCII_ZERO..ASCII_NINE
-
     fun calculate(formula: String): Double {
         val (numberGroup, operatorGroup) =
-            numberGroup(formula = formula) to operatorGroup(formula = formula)
+            Groupings.numberGroup(formula = formula) to Groupings.operatorGroup(formula = formula)
         var result = numberGroup.last()
         operatorGroup
             .zip(numberGroup.takeLast(numberGroup.lastIndex))
@@ -16,35 +12,5 @@ object Calculator {
                 result = it.first.formula(result, it.second)
             }
         return result
-    }
-
-    private fun numberGroup(formula: String): List<Double> {
-        val (numberBuilder, numberGroup) =
-            StringBuilder() to mutableListOf<Double>()
-        formula.forEachIndexed { index, item ->
-            when {
-                index == formula.lastIndex -> {
-                    numberBuilder.append(item)
-                    numberGroup.add(numberBuilder.toString().toDouble())
-                }
-                item.code in ASCII_NUMBER_RANGE -> {
-                    numberBuilder.append(item)
-                }
-                else -> {
-                    numberGroup.add(numberBuilder.toString().toDouble())
-                    numberBuilder.clear()
-                }
-            }
-        }
-        return numberGroup
-    }
-
-    private fun operatorGroup(formula: String): List<Operator> {
-        val operatorGroup = mutableListOf<Operator>()
-        formula.forEach { item ->
-            if (item.code in ASCII_NUMBER_RANGE) return@forEach
-            operatorGroup.add(Operator.findBySymbol(item))
-        }
-        return operatorGroup
     }
 }
---
+++ domain/src/main/java/edu/nextstep/camp/calculator/Groupings.kt
@@ -0,0 +1,38 @@
+package edu.nextstep.camp.calculator
+
+object Groupings {
+
+    private const val ASCII_ZERO = 48
+    private const val ASCII_NINE = 59
+    private val ASCII_NUMBER_RANGE = ASCII_ZERO..ASCII_NINE
+
+    fun numberGroup(formula: String): List<Double> {
+        val (numberBuilder, numberGroup) =
+            StringBuilder() to mutableListOf<Double>()
+        formula.forEachIndexed { index, item ->
+            when {
+                index == formula.lastIndex -> {
+                    numberBuilder.append(item)
+                    numberGroup.add(numberBuilder.toString().toDouble())
+                }
+                item.code in ASCII_NUMBER_RANGE -> {
+                    numberBuilder.append(item)
+                }
+                else -> {
+                    numberGroup.add(numberBuilder.toString().toDouble())
+                    numberBuilder.clear()
+                }
+            }
+        }
+        return numberGroup
+    }
+
+    fun operatorGroup(formula: String): List<Operator> {
+        val operatorGroup = mutableListOf<Operator>()
+        formula.forEach { item ->
+            if (item.code in ASCII_NUMBER_RANGE) return@forEach
+            operatorGroup.add(Operator.findBySymbol(item))
+        }
+        return operatorGroup
+    }
+}
