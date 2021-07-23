212813885258a392be9f77b9ca448a90edeb3964
강현석 <kekemusa37@gmail.com>
2021-07-24 00:40:40 +0900

    :white_check_mark: test(groupings) : 추가 - 수식에 공백이 들어있어도 숫자들만 그룹화합니다 테스트 함수 (실패)

--- domain/src/test/java/edu/nextstep/camp/calculator/GroupingsTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/GroupingsTest.kt
@@ -17,4 +17,17 @@ internal class GroupingsTest {
                 assertThat(it.first).isEqualTo(it.second)
             }
     }
+
+    @Test
+    fun `수식에 공백이 들어있어도 숫자들만 그룹화합니다`() {
+        val formula = "2 + 3 * 4 / 2"
+        val numberGroup = listOf(2.0, 3.0, 4.0, 2.0)
+        val actual = Groupings.numberGroup(formula)
+
+        actual
+            .zip(numberGroup)
+            .forEach {
+                assertThat(it.first).isEqualTo(it.second)
+            }
+    }
 }
