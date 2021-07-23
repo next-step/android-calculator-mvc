66d9ed8e93bf3011f3684decea608338dcf4f365
강현석 <kekemusa37@gmail.com>
2021-07-23 21:38:39 +0900

    :white_check_mark: test(operator) : 추가 - 피연산자를 0으로 나누면 에러가 발생합니다 테스트 함수 (실패)

--- domain/src/test/java/edu/nextstep/camp/calculator/OperatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/OperatorTest.kt
@@ -1,6 +1,7 @@
 package edu.nextstep.camp.calculator
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
 
 internal class OperatorTest {
@@ -26,5 +27,12 @@ internal class OperatorTest {
     @Test
     fun `두 개의 피연산자를 나눕니다`() {
         assertThat(Operator.DIVIDE.formula(firstOperand, secondOperand)).isEqualTo(2.0)
+    }
+
+    @Test
+    fun `피연산자를 0으로 나누면 에러가 발생합니다`(){
+        assertThatExceptionOfType(Exception::class.java).isThrownBy {
+            Operator.DIVIDE.formula(firstOperand, 0.0)
+        }
     }
 }
