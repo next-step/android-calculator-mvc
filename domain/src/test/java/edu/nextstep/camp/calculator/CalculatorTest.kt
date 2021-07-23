373003404c3f954369501b8068aa793aa69393b7
강현석 <kekemusa37@gmail.com>
2021-07-24 01:01:30 +0900

    :white_check_mark: test(calculator) : 추가 - 사칙 연산 기호가 아닌 수식이 들어가면 에러가 발생합니다 테스트 함수

--- domain/src/test/java/edu/nextstep/camp/calculator/CalculatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/CalculatorTest.kt
@@ -1,6 +1,7 @@
 package edu.nextstep.camp.calculator
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
 
 internal class CalculatorTest {
@@ -9,5 +10,13 @@ internal class CalculatorTest {
     fun `2 더하기 3 곱하기 4 나누기 2 를 연산하면 10이 출력됩니다`() {
         val formula = "2+3*4/2"
         assertThat(Calculator.calculate(formula)).isEqualTo(10.0)
+    }
+
+    @Test
+    fun `사칙 연산 기호가 아닌 수식이 들어가면 에러가 발생합니다`() {
+        val formula = "2:3*4/2"
+        assertThatIllegalArgumentException().isThrownBy {
+            Calculator.calculate(formula)
+        }
     }
 }
