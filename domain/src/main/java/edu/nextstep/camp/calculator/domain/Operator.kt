8e43eb4b7d6506dfd1ece852ff5cd9895df45b24
namjackson <skawo32167@naver.com>
2021-07-24 20:13:06 +0900

    feat(domain) : Add Operator class And Test

---
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Operator.kt
@@ -0,0 +1,25 @@
+package edu.nextstep.camp.calculator.domain
+
+enum class Operator(
+    val code: String,
+    val operateFun: (Double, Double) -> Double,
+) {
+    PLUS("+", { a, b -> a + b }),
+    MINUS("-", { a, b -> a - b }),
+    MULTIPLY("*", { a, b -> a * b }),
+    DIVISION("/", { a, b -> a / b });
+
+    fun calculate(first: Double, second: Double): Double {
+        if (this == DIVISION && second.isZero()) {
+            throw IllegalArgumentException("Can not DIVISION Zero")
+        }
+        return operateFun(first, second)
+    }
+
+    companion object {
+        fun getOperator(code: String): Operator =
+            values().find { it.code == code } ?: throw IllegalArgumentException("Unknown Operator")
+    }
+}
+
+internal fun Double.isZero() = this == 0.0
