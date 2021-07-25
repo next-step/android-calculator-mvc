309c2a107dd907d999ff79e342b428e64921e113
강현석 <kekemusa37@gmail.com>
2021-07-25 23:20:35 +0900

    :white_check_mark: test(mainactivity) : 수정 - 입력된 피연산자가 있을 때 피연산자를 입력하면 기존 숫자 뒤에 해당 숫자가 화면에 보여야한다 (성공)

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,43 +18,43 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupListener() = with(binding){
         button0.setOnClickListener {
-            textView.text = "0"
+            textView.text = getString(R.string.string_string, textView.text, "0")
         }
 
         button1.setOnClickListener {
-            textView.text = "1"
+            textView.text = getString(R.string.string_string, textView.text, "1")
         }
 
         button2.setOnClickListener {
-            textView.text = "2"
+            textView.text = getString(R.string.string_string, textView.text, "2")
         }
 
         button3.setOnClickListener {
-            textView.text = "3"
+            textView.text = getString(R.string.string_string, textView.text, "3")
         }
 
         button4.setOnClickListener {
-            textView.text = "4"
+            textView.text = getString(R.string.string_string, textView.text, "4")
         }
 
         button5.setOnClickListener {
-            textView.text = "5"
+            textView.text = getString(R.string.string_string, textView.text, "5")
         }
 
         button6.setOnClickListener {
-            textView.text = "6"
+            textView.text = getString(R.string.string_string, textView.text, "6")
         }
 
         button7.setOnClickListener {
-            textView.text = "7"
+            textView.text = getString(R.string.string_string, textView.text, "7")
         }
 
         button8.setOnClickListener {
-            textView.text = "8"
+            textView.text = getString(R.string.string_string, textView.text, "8")
         }
 
         button9.setOnClickListener {
-            textView.text = "9"
+            textView.text = getString(R.string.string_string, textView.text, "9")
         }
     }
 }
