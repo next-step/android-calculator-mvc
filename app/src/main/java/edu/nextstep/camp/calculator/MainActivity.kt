c0abf9dbe243c895df0d0bbb818908b5c3b205ff
impactrudia <impactyoung@gmail.com>
2021-08-12 20:27:18 +0900

    feat : 피연산자 누른 경우 해당 숫자 보이기

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -11,5 +11,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initButtonClickListener()
+    }
+
+    private fun initButtonClickListener() {
+        with(binding) {
+            button0.setOnClickListener {
+                textFormula.append("0")
+            }
+            button1.setOnClickListener {
+                textFormula.append("1")
+            }
+            button2.setOnClickListener {
+                textFormula.append("2")
+            }
+            button3.setOnClickListener {
+                textFormula.append("3")
+            }
+            button4.setOnClickListener {
+                textFormula.append("4")
+            }
+            button5.setOnClickListener {
+                textFormula.append("5")
+            }
+            button6.setOnClickListener {
+                textFormula.append("6")
+            }
+            button7.setOnClickListener {
+                textFormula.append("7")
+            }
+            button8.setOnClickListener {
+                textFormula.append("8")
+            }
+            button9.setOnClickListener {
+                textFormula.append("9")
+            }
+        }
     }
 }
