2aaaa487a3a9ba4044770b5f9c3a8d333bd0d15c
impactrudia <impactyoung@gmail.com>
2021-08-13 20:53:17 +0900

    fix :  불필요한 var 선언

--- app/src/main/java/edu/nextstep/camp/calculator/model/Calculator.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/model/Calculator.kt
@@ -1,14 +1,14 @@
 package edu.nextstep.camp.calculator.model
 
-class Calculator(var inputs : String) {
+class Calculator(private val inputs : String) {
     companion object {
         private const val INDEX_INITIAL = 0
         private const val ASCII_ZERO = 48
         private const val ASCII_NINE = 57
     }
 
-    fun splitText(): List<String> {
-        var splits = inputs.split(" ")
+    private fun splitText(): List<String> {
+        val splits = inputs.split(" ")
         if(splits.size <= 1){
             throw IllegalArgumentException("문자열 계산을 할 수 없습니다.")
         }
@@ -23,11 +23,11 @@ class Calculator(var inputs : String) {
         return splits
     }
 
-    fun isNumber(text : String): Boolean {
+    private fun isNumber(text : String): Boolean {
         return text.elementAt(INDEX_INITIAL).toInt() in ASCII_ZERO..ASCII_NINE
     }
 
-    fun isSplitLengthOperator(): Boolean {
+    private fun isSplitLengthOperator(): Boolean {
         return splitText().size % 2 == 1
     }
 
