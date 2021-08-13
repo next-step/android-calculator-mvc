d683b26c6823d353974e41fca589c6023e22e4c4
impactrudia <impactyoung@gmail.com>
2021-08-13 20:48:26 +0900

    fix : 테스트코드 함수명 _ 제거

--- app/src/test/java/edu/nextstep/camp/calculator/model/CalculatorTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/model/CalculatorTest.kt
@@ -8,28 +8,28 @@ import org.junit.Test
 
 class CalculatorTest {
     @Test
-    fun 문자열계산식_없을_때_예외발생(){
+    fun `문자열계산식 없을 때 예외발생`(){
         Assertions.assertThatThrownBy { Calculator("").splitText() }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("문자열 계산을 할 수 없습니다.")
     }
 
     @Test
-    fun 문자열계산식_없을_때_숫자가_없을_때_예외발생(){
+    fun `문자열계산식 없을 때 숫자가 없을 때 예외발생`(){
         Assertions.assertThatThrownBy { Calculator("? + 2").splitText() }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("숫자를 입력해주세요.")
     }
 
     @Test
-    fun 문자열계산식_없을_때_연산자가_아닐_때_예외발생(){
+    fun `문자열계산식 없을 때 연산자가 아닐 때 예외발생`(){
         Assertions.assertThatThrownBy { Calculator("2 # 4").splitText() }
             .isInstanceOf(IllegalArgumentException::class.java)
             .hasMessageContaining("올바른 연산자 부호를 입력해주세요.")
     }
 
     @Test
-    fun 띄어쓰기문자를_배열로_담을_수_있다(){
+    fun `띄어쓰기문자를 배열로 담을 수 있다`(){
         //given
         //when
         var input = Calculator("2 + 3")
@@ -40,7 +40,7 @@ class CalculatorTest {
     }
 
     @Test
-    fun 배열에서_인덱스가_짝수인_경우_숫자면_참(){
+    fun `배열에서 인덱스가 짝수인 경우 숫자면 참`(){
         var input = Calculator("11 + 3")
         var inputs = input.splitText()
 
@@ -49,7 +49,7 @@ class CalculatorTest {
     }
 
     @Test
-    fun 배열에서_인덱스가_홀수인_경우_문자이면_참(){
+    fun `배열에서 인덱스가 홀수인 경우 문자이면 참`(){
         var input = Calculator("2 + 3")
         var inputs = input.splitText()
 
@@ -57,62 +57,62 @@ class CalculatorTest {
     }
 
     @Test
-    fun 배열_인덱스길이가_홀수이면_참(){
+    fun `배열 인덱스길이가 홀수이면 참`(){
         var input = Calculator("2 + 3 / 2")
 
         assertTrue(input.isSplitLengthOperator())
     }
 
     @Test
-    fun 배열이_세개일_때_문자열_계산이_참_덧셈(){
+    fun `배열이 세개일 때 문자열 계산이 참 덧셈`(){
         var input = Calculator("2 + 3")
         assertThat(input.calculate(), equalTo(5.0))
     }
 
     @Test
-    fun 배열이_세개일_때_문자열_계산이_참_뺄셈() {
+    fun `배열이 세개일 때 문자열 계산이 참 뺄셈`() {
         var input = Calculator("3 - 2")
         assertThat(input.calculate(), equalTo(1.0))
     }
 
     @Test
-    fun 배열이_세개일_때_문자열_계산이_참_곱셈() {
+    fun `배열이 세개일 때 문자열 계산이 참 곱셈`() {
         var input = Calculator("3 * 2")
         assertThat(input.calculate(), equalTo(6.0))
     }
 
     @Test
-    fun 배열이_세개일_때_문자열_계산이_참_나눗셈() {
+    fun `배열이 세개일 때 문자열 계산이 참 나눗셈`() {
         var input = Calculator("4 / 2")
         assertThat(input.calculate(), equalTo(2.0))
     }
 
     @Test
-    fun 배열이_다섯개일_때_문자열_계산이_참_덧셈(){
+    fun `배열이 다섯개일 때 문자열 계산이 참 덧셈`(){
         var input = Calculator("2 + 3 + 3")
         assertThat(input.calculate(), equalTo(8.0))
     }
 
     @Test
-    fun 배열이_다섯개일_때_문자열_계산이_참_뺄셈() {
+    fun `배열이 다섯개일 때 문자열 계산이 참 뺄셈`() {
         var input = Calculator("5 - 2 - 1")
         assertThat(input.calculate(), equalTo(2.0))
     }
 
     @Test
-    fun 배열이_다섯개일_때_문자열_계산이_참_곱셈() {
+    fun `배열이 다섯개일 때 문자열 계산이 참 곱셈`() {
         var input = Calculator("3 * 2 * 2")
         assertThat(input.calculate(), equalTo(12.0))
     }
 
     @Test
-    fun 배열이_다섯개일_때_문자열_계산이_참_나눗셈() {
+    fun `배열이 다섯개일 때 문자열 계산이 참 나눗셈`() {
         var input = Calculator("6 / 2 / 2")
         assertThat(input.calculate(), equalTo(1.5))
     }
 
     @Test
-    fun 사칙연산() {
+    fun `사칙연산`() {
         var input = Calculator("2 + 3 * 4 / 2")
         assertThat(input.calculate(), equalTo(10.0))
     }
--- app/src/test/java/edu/nextstep/camp/calculator/model/OperatorTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/model/OperatorTest.kt
@@ -10,31 +10,31 @@ class OperatorTest {
     private val secondOperand = 1.0
 
     @Test
-    fun 덧셈을_해야한다(){
+    fun `덧셈을 해야한다`(){
         val result = Operator.PLUS.formula(firstOperand, secondOperand)
         assertThat(result, equalTo(4.0))
     }
 
     @Test
-    fun 뺄샘을_해야한다(){
+    fun `뺄샘을 해야한다`(){
         val result = Operator.MINUS.formula(firstOperand, secondOperand)
         assertThat(result, equalTo(2.0))
     }
 
     @Test
-    fun 곱셈을_해야한다(){
+    fun `곱셈을 해야한다`(){
         val result = Operator.MULTIPLY.formula(firstOperand, secondOperand)
         assertThat(result, equalTo(3.0))
     }
 
     @Test
-    fun 나눗셈을_해야한다(){
+    fun `나눗셈을 해야한다`(){
         val result = Operator.DIVIDE.formula(firstOperand, secondOperand)
         assertThat(result, equalTo(3.0))
     }
 
     @Test
-    fun 나눗셈을_해야한다_0으로_나눌_때_예외발생(){
+    fun `나눗셈을 해야한다 0으로 나눌 때 예외발생`(){
         assertThatThrownBy { Operator.DIVIDE.formula(firstOperand, 0.0) }
             .isInstanceOf(ArithmeticException::class.java)
             .hasMessageContaining("Divide by zero should trow")
