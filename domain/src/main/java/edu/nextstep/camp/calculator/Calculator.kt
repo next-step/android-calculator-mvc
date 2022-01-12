fbccbebac5528fbe4aa6f6acde26de75b11d9720
Pyro <rhwjddhks@gmail.com>
2022-01-12 23:00:39 +0900

    refactor: typealias 를 통해 Calculator 안에 쓰인 Pair 의 가독성 강화
    - first, second, list 와 같은 모호한 네이밍 제거
    - 2 와 같은 매직 넘버 제거

--- domain/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
@@ -1,11 +1,30 @@
 package edu.nextstep.camp.calculator
 
+private typealias Chunk = List<String>
+private typealias ParsedChunk = Pair<IntArithmetics, Int>
+
 object Calculator {
+    private const val DELIMITER = " "
+    private const val CHUNK_SIZE = 2
+
     fun calculate(input: String): Int {
-        val list = input.split(" ")
-        return list.subList(1, list.size)
-            .chunked(2)
-            .map { Pair(IntArithmetics.from(it[0]), it[1].toInt()) }
-            .fold(list[0].toInt()) { acc, curr -> curr.first.apply(acc, curr.second) }
+        val (identity, words) = input.split(DELIMITER)
+        return words.chunked(CHUNK_SIZE)
+            .map { parse(it) }
+            .fold(identity.toInt()) { acc, cur -> accumulate(acc, cur) }
+    }
+
+    private operator fun Chunk.component2(): List<String> = this.drop(1)
+
+    private fun parse(chunk: Chunk): ParsedChunk {
+        val arithmetic = IntArithmetics.from(chunk[0])
+        val number = chunk[1].toInt()
+        return Pair(arithmetic, number)
+    }
+
+    private fun accumulate(acc: Int, cur: ParsedChunk): Int {
+        val arithmetic = cur.first
+        val number = cur.second
+        return arithmetic.apply(acc, number)
     }
 }
