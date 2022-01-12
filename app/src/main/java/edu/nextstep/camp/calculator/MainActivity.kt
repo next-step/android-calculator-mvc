abcf27283ea14e9f4cf259a739f2188929c804b3
seth =) <luvyum4e@gmail.com>
2022-01-12 23:58:18 +0900

    feat : 버튼 클릭 기능 구현

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -5,11 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupUi()
+    }
+
+    private fun setupUi() {
+        val buttonViews = listOf(
+            binding.button0, binding.button1, binding.button2, binding.button3, binding.button4,
+            binding.button5, binding.button6, binding.button7, binding.button8, binding.button9,
+        )
+
+        buttonViews.forEach { button ->
+            button.setOnClickListener { binding.tvResultDisplay.text = button.text }
+        }
     }
 }
