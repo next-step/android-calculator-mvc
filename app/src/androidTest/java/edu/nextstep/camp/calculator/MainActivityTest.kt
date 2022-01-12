401d7c13ea7fc17121663bd0f7ef8abdb67c40cd
jinsu4755 <parkjinsu4755@gmail.com>
2022-01-12 19:30:02 +0900

    feat(step2): step1 피드백 반영
    - 테스트 코드 이름을 보다 정확한 이름으로 변경

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -14,7 +14,7 @@ class MainActivityTest {
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun test0() {
+    fun whenClick0Button_thenShow0InTextView() {
         // when: 사용자가 피연산자 0 버튼을 누르면
         onView(withId(R.id.button0)).perform(click())
         // then: 화면에 0이 보여야한다.
@@ -22,7 +22,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test1() {
+    fun whenClick1Button_thenShow1InTextView() {
         // when: 사용자가 피연산자 1 버튼을 누르면
         onView(withId(R.id.button1)).perform(click())
         // then: 화면에 1이 보여야한다.
@@ -30,7 +30,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test2() {
+    fun whenClick2Button_thenShow2InTextView() {
         // when: 사용자가 피연산자 2 버튼을 누르면
         onView(withId(R.id.button2)).perform(click())
         // then: 화면에 2이 보여야한다.
@@ -38,7 +38,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test3() {
+    fun whenClick3Button_thenShow3InTextView() {
         // when: 사용자가 피연산자 3 버튼을 누르면
         onView(withId(R.id.button3)).perform(click())
         // then: 화면에 3이 보여야한다.
@@ -46,7 +46,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test4() {
+    fun whenClick4Button_thenShow4InTextView() {
         // when: 사용자가 피연산자 4 버튼을 누르면
         onView(withId(R.id.button4)).perform(click())
         // then: 화면에 4이 보여야한다.
@@ -54,7 +54,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test5() {
+    fun whenClick5Button_thenShow5InTextView() {
         // when: 사용자가 피연산자 5 버튼을 누르면
         onView(withId(R.id.button5)).perform(click())
         // then: 화면에 5이 보여야한다.
@@ -62,7 +62,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test6() {
+    fun whenClick6Button_thenShow6InTextView() {
         // when: 사용자가 피연산자 6 버튼을 누르면
         onView(withId(R.id.button6)).perform(click())
         // then: 화면에 6이 보여야한다.
@@ -70,7 +70,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test7() {
+    fun whenClick7Button_thenShow7InTextView() {
         // when: 사용자가 피연산자 7 버튼을 누르면
         onView(withId(R.id.button7)).perform(click())
         // then: 화면에 7이 보여야한다.
@@ -78,7 +78,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test8() {
+    fun whenClick8Button_thenShow8InTextView() {
         // when: 사용자가 피연산자 8 버튼을 누르면
         onView(withId(R.id.button8)).perform(click())
         // then: 화면에 8이 보여야한다.
@@ -86,7 +86,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test9() {
+    fun whenClick9Button_thenShow9InTextView() {
         // when: 사용자가 피연산자 9 버튼을 누르면
         onView(withId(R.id.button9)).perform(click())
         // then: 화면에 9이 보여야한다.
