10d7645b9a4b8f8927b937c68277fe76ee484d2b
devsoupe <luvyum4e@gmail.com>
2022-01-15 22:36:01 +0900

    refactor : 불필요한 파일 삭제

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -2,7 +2,4 @@ package edu.nextstep.camp.calculator.domain
 
 class Calculator {
 
-    fun evaluate(expression: String): Int {
-        return 6
-    }
 }
--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
@@ -1,15 +1,5 @@
 package edu.nextstep.camp.calculator.domain
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-
 class CalculatorTest {
 
-    @Test
-    fun evaluatesExpression() {
-        val calculator = Calculator()
-        val actual = calculator.evaluate("1+2+3")
-
-        assertEquals(6, actual)
-    }
 }
