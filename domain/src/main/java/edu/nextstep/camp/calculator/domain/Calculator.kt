7a32a9bb91ede2870ea0f173418ca31952d8fa8b
devsoupe <luvyum4e@gmail.com>
2022-01-16 20:32:01 +0900

    feed : 코드리뷰 요청사항 수정

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -4,8 +4,8 @@ object Calculator {
 
     fun evaluate(expression: String?, delimiter: String): Int {
         val chunks = ExpressionParser.parse(expression, delimiter)
-        val firstDigit = chunks.head().toInt()
-        val remainChunks = chunks.tail()
+        val firstDigit = chunks.first().toInt()
+        val remainChunks = chunks.drop(1)
 
         return remainChunks.chunked(2)
             .fold(firstDigit) { acc, (token, digit) -> BinaryCalculator.of(token).evaluate(acc, digit.toInt()) }
--- domain/src/main/java/edu/nextstep/camp/calculator/domain/ExpressionParser.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/ExpressionParser.kt
@@ -2,9 +2,6 @@ package edu.nextstep.camp.calculator.domain
 
 typealias Chunks = List<String>
 
-fun Chunks.head() = this.first()
-fun Chunks.tail() = this.drop(1)
-
 internal object ExpressionParser {
 
     fun parse(expression: String?, delimiter: String): Chunks {
