efb9fdb1e2a88f41d9f7be8d14bdbe522c3e9423
luvimperfection <luvimperfection@gmail.com>
2022-01-17 07:18:08 +0900

    STEP3: Add simple testcase for +-*/

--- app/src/test/java/com/example/domain/CalculatorTest.kt
+++ app/src/test/java/com/example/domain/CalculatorTest.kt
@@ -7,10 +7,32 @@ import org.junit.Test
 class CalculatorTest{
 
     @Test
-    fun evaluatesExpression(){
+    fun evaluatesExpression_add(){
         val calculator = Calculator()
         val actual:Int = calculator.evaluate("1+2+3")
         assertThat(actual).isEqualTo(6)
+    }
+
+
+    @Test
+    fun evaluatesExpression_subtract(){
+        val calculator = Calculator()
+        val actual:Int = calculator.evaluate("3-2-1")
+        assertThat(actual).isEqualTo(0)
+    }
+
+    @Test
+    fun evaluatesExpression_multiply(){
+        val calculator = Calculator()
+        val actual:Int = calculator.evaluate("1*2*3")
+        assertThat(actual).isEqualTo(6)
+    }
+
+    @Test
+    fun evaluatesExpression_divide(){
+        val calculator = Calculator()
+        val actual:Int = calculator.evaluate("6/3/2")
+        assertThat(actual).isEqualTo(1)
     }
 
 }
