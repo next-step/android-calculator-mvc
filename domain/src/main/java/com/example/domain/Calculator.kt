38064152e81ea81401348f4c7eb8a4732f4a0bc6
luvimperfection <luvimperfection@gmail.com>
2022-01-17 07:18:31 +0900

    STEP: Rough implementation of calculator

--- domain/src/main/java/com/example/domain/Calculator.kt
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -1,4 +1,54 @@
 package com.example.domain
 
-class MyClass {
+class Calculator {
+    fun evaluate(expression: String?): Int{
+        if(expression.isNullOrEmpty()) throw IllegalArgumentException()
+
+        var currentValue = 0
+        expression.split(" ").forEach { chunk ->
+            println("chunk: $chunk")
+            currentValue = calculate(currentValue, chunk)
+            println("currentValue: $currentValue")
+
+        }
+        return currentValue
+    }
+
+    fun calculate(currentValue: Int, chunk: String): Int {
+        var currentOperation : Operation? = null
+        var updatedValue : Int = currentValue
+        chunk?.forEach {
+            println("char: $it")
+            when(it){
+                '+' -> currentOperation = Operation.Add
+                '-' -> currentOperation = Operation.Subtract
+                '*' -> currentOperation = Operation.Multiply
+                '/' -> currentOperation = Operation.Divide
+                else -> when(currentOperation){
+                    is Operation.Add ->  updatedValue = add(updatedValue, it.digitToInt())
+                    is Operation.Subtract ->  updatedValue = subtract(updatedValue, it.digitToInt())
+                    is Operation.Multiply ->  updatedValue = multiply(updatedValue, it.digitToInt())
+                    is Operation.Divide ->  updatedValue = divide(updatedValue, it.digitToInt())
+                    else -> updatedValue = it.digitToInt()
+                }
+
+            }
+        }
+        return updatedValue
+    }
+
+    fun add(operandA: Int, operandB: Int): Int = operandA + operandB
+
+    fun subtract(operandA: Int, operandB: Int): Int = operandA - operandB
+
+    fun multiply(operandA: Int, operandB: Int): Int = operandA * operandB
+
+    fun divide(operandA: Int, operandB: Int): Int = operandA / operandB
+}
+
+sealed class Operation{
+    object Add:Operation()
+    object Subtract:Operation()
+    object Multiply:Operation()
+    object Divide:Operation()
 }
