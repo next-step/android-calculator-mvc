44ca8a18bd9f346d58aaf09e112012621b669a55
luvimperfection <luvimperfection@gmail.com>
2022-01-18 07:39:21 +0900

    STEP3: 리뷰 반영 - @Before 추가, 테스트함수명 한글화, 함수 분리

--- app/src/test/java/com/example/domain/CalculatorTest.kt
+++ app/src/test/java/com/example/domain/CalculatorTest.kt
@@ -2,76 +2,71 @@ package com.example.domain
 
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.Assert.assertThrows
+import org.junit.Before
 import org.junit.Test
-import java.lang.IllegalArgumentException
 
 class CalculatorTest{
+    private lateinit var calculator: Calculator
+    @Before
+    fun init(){
+        calculator = Calculator()
+    }
+
 
     @Test
-    fun evaluatesExpression_add(){
-        val calculator = Calculator()
+    fun `공백없는 덧셈이 주어진 경우 계산에 성공한다`(){
         val actual:Int = calculator.evaluate("1+2+3")
         assertThat(actual).isEqualTo(6)
     }
 
 
     @Test
-    fun evaluatesExpression_subtract(){
-        val calculator = Calculator()
+    fun `공백없는 뺄셈이 주어진 경우 계산에 성공한다`(){
         val actual:Int = calculator.evaluate("3-2-1")
         assertThat(actual).isEqualTo(0)
     }
 
     @Test
-    fun evaluatesExpression_multiply(){
-        val calculator = Calculator()
+    fun `공백없는 곱셈이 주어진 경우 계산에 성공한다`(){
         val actual:Int = calculator.evaluate("1*2*3")
         assertThat(actual).isEqualTo(6)
     }
 
     @Test
-    fun evaluatesExpression_divide(){
-        val calculator = Calculator()
+    fun `공백없는 나눗셈이 주어진 경우 계산에 성공한다`(){
         val actual:Int = calculator.evaluate("6/3/2")
         assertThat(actual).isEqualTo(1)
     }
 
     @Test
-    fun evaluatesExpression_withSpace_add_subtract(){
-        val calculator = Calculator()
+    fun `공백이 있는 덧셈과 뺄셈이 주어진 경우 계산에 성공한다`(){
         val actual:Int = calculator.evaluate("1+2+3 +4 - 5")
         assertThat(actual).isEqualTo(5)
     }
 
     @Test
-    fun evaluatesExpression_withSpace_multiply_divide(){
-        val calculator = Calculator()
+    fun `공백이 있는 곱셈과 나눗셈이 주어진 경우 계산에 성공한다`(){
         val actual:Int = calculator.evaluate("1*2*3 /4 * 5")
         assertThat(actual).isEqualTo(5)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun evaluatesExpression_input_is_null(){
-        val calculator = Calculator()
+    fun `입력값이 null인 경우 IllegalArgumentException throw`(){
         val actual = calculator.evaluate(null)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun evaluatesExpression_input_is_empty(){
-        val calculator = Calculator()
+    fun `입력값이 빈 문자열인 경우 IllegalArgumentException throw`(){
         val actual = calculator.evaluate("")
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun evaluatesExpression_input_is_blank(){
-        val calculator = Calculator()
+    fun `입력값이 공백인 경우 IllegalArgumentException throw`(){
         val actual = calculator.evaluate("   ")
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun evaluatesExpression_input_is_invalid(){
-        val calculator = Calculator()
+    fun `입력값이 사칙연산 기호가 아닌 경우 IllegalArgumentException throw`(){
         val actual = calculator.evaluate(" $  & 3")
     }
 }
--- domain/src/main/java/com/example/domain/Calculator.kt
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -10,25 +10,31 @@ class Calculator {
         return currentValue
     }
 
-    fun calculate(currentValue: Int, chunk: String): Int {
+    fun calculate(currentValue: Int, expression: String): Int {
         var currentOperation : Operation? = null
-        var updatedValue : Int = currentValue
-        chunk?.forEach {
+        var currentValue = currentValue
+        expression.forEach {
             when(it){
                 '+' -> currentOperation = Operation.Add
                 '-' -> currentOperation = Operation.Subtract
                 '*' -> currentOperation = Operation.Multiply
                 '/' -> currentOperation = Operation.Divide
-                else -> when(currentOperation){
-                    is Operation.Add ->  updatedValue = add(updatedValue, it.digitToInt())
-                    is Operation.Subtract ->  updatedValue = subtract(updatedValue, it.digitToInt())
-                    is Operation.Multiply ->  updatedValue = multiply(updatedValue, it.digitToInt())
-                    is Operation.Divide ->  updatedValue = divide(updatedValue, it.digitToInt())
-                    else -> updatedValue = it.digitToIntOrNull() ?: throw IllegalArgumentException()
-                }
+                else ->
+                    currentValue = if (it.isDigit()) updateValue(currentOperation, currentValue, it.digitToInt())
+                    else throw IllegalArgumentException()
             }
         }
-        return updatedValue
+        return currentValue
+    }
+
+    fun updateValue(operation : Operation?, operandA: Int, operandB: Int): Int {
+        return when(operation){
+            is Operation.Add -> add(operandA, operandB)
+            is Operation.Subtract ->  subtract(operandA, operandB)
+            is Operation.Multiply ->  multiply(operandA, operandB)
+            is Operation.Divide ->  divide(operandA, operandB)
+            else -> operandB
+        }
     }
 
     fun add(operandA: Int, operandB: Int): Int = operandA + operandB
