8ad1af399b3a8f3211e1c38338cd1480bce4ecbe
ganghee <idiosauri@naver.com>
2022-01-19 02:35:54 +0900

    refact: 입력시 띄어쓰기를 기준으로 숫자와 연산자를 나눈다고 가정하고 구현

--- domain/src/main/java/com/nextstep/domain/Calculator.kt
+++ domain/src/main/java/com/nextstep/domain/Calculator.kt
@@ -4,37 +4,24 @@ class Calculator {
 
     fun calculate(input: String?): Int {
         if (input.isNullOrBlank()) throw IllegalArgumentException(IS_NOT_OR_BLANK)
-        return calculateList(inputOperandOrOperator(input))
+        return operandAndOperatorList(input)
     }
 
-    private fun inputOperandOrOperator(inputString: String): List<String> {
-        val operandAndOperatorList = mutableListOf<String>()
-        var mutableOperand = ""
-        inputString.forEach { singleCharacter ->
-            if (singleCharacter.isNumber()) {
-                mutableOperand += singleCharacter
-            } else if (singleCharacter.isLetter() && mutableOperand.isNotEmpty()) {
-                val operator = singleCharacter.toString()
-                operandAndOperatorList.add(mutableOperand)
-                operandAndOperatorList.add(operator)
-                mutableOperand = ""
-            }
+    private fun operandAndOperatorList(input: String): Int {
+        val operandAndOperatorList = input.split(DELIMITER)
+        val head = operandAndOperatorList.findFirstOperand()
+        return operandAndOperatorList.drop(1).chunked(2).fold(head) { acc, element ->
+            calculateTwoNumber(acc, element[0], element[1].toInt())
         }
-        operandAndOperatorList.add(mutableOperand)
-        return operandAndOperatorList
     }
 
-    private fun calculateList(operandAndOperatorList: List<String>): Int {
-        var result = 0
-        var operator = "+"
-        operandAndOperatorList.forEach { str ->
-            if (str.isOperand()) {
-                result = calculateTwoNumber(result, operator, str.toInt())
-            } else if (str.isLetter()) {
-                operator = str
-            }
+    private fun List<String>.findFirstOperand(): Int {
+        val operandAndOperatorList = this
+        if (operandAndOperatorList.first().isLetter() || operandAndOperatorList.last().isLetter()) {
+            throw IllegalArgumentException(WRONG_INPUT)
+        } else {
+            return operandAndOperatorList[0].toInt()
         }
-        return result
     }
 
     private fun calculateTwoNumber(num1: Int, operator: String, num2: Int): Int {
@@ -48,12 +35,11 @@ class Calculator {
     }
 
     private fun String.isLetter(): Boolean = this.toIntOrNull() == null
-    private fun String.isOperand(): Boolean = this.toIntOrNull() is Int
-    private fun Char.isLetter(): Boolean = !this.isDigit() && !this.isWhitespace()
-    private fun Char.isNumber(): Boolean = this.isDigit() && !this.isWhitespace()
 
     companion object {
+        const val DELIMITER = " "
         const val IS_NOT_OR_BLANK: String = "입력값이 null 이거나 공백 문자입니다."
         const val IS_NOT_OPERATOR: String = "사칙연산 기호가 아닙니다."
+        const val WRONG_INPUT: String = "잘못된 입력입니다."
     }
 }
