972158b1745b47fa86cf25242e4993f85be96aa8
luvimperfection <luvimperfection@gmail.com>
2022-01-19 07:27:57 +0900

    STEP3: ktlint autofix 적용

--- domain/src/main/java/com/example/domain/Calculator.kt
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -1,8 +1,8 @@
 package com.example.domain
 
 class Calculator {
-    fun evaluate(expression: String?): Int{
-        if(expression.isNullOrBlank()) throw IllegalArgumentException()
+    fun evaluate(expression: String?): Int {
+        if (expression.isNullOrBlank()) throw IllegalArgumentException()
         var currentValue = 0
         expression.split(" ").forEach { chunk ->
             currentValue = calculate(currentValue, chunk)
@@ -11,28 +11,31 @@ class Calculator {
     }
 
     fun calculate(currentValue: Int, expression: String): Int {
-        var currentOperation : Operation? = null
+        var currentOperation: Operation? = null
         var currentValue = currentValue
         expression.forEach {
-            when(it){
+            when (it) {
                 '+' -> currentOperation = Operation.Add
                 '-' -> currentOperation = Operation.Subtract
                 '*' -> currentOperation = Operation.Multiply
                 '/' -> currentOperation = Operation.Divide
                 else ->
-                    currentValue = if (it.isDigit()) updateValue(currentOperation, currentValue, it.digitToInt())
-                    else throw IllegalArgumentException()
+                    currentValue = if (it.isDigit()) {
+                        updateValue(currentOperation, currentValue, it.digitToInt())
+                    } else {
+                        throw IllegalArgumentException()
+                    }
             }
         }
         return currentValue
     }
 
-    fun updateValue(operation : Operation?, operandA: Int, operandB: Int): Int {
-        return when(operation){
+    fun updateValue(operation: Operation?, operandA: Int, operandB: Int): Int {
+        return when (operation) {
             is Operation.Add -> add(operandA, operandB)
-            is Operation.Subtract ->  subtract(operandA, operandB)
-            is Operation.Multiply ->  multiply(operandA, operandB)
-            is Operation.Divide ->  divide(operandA, operandB)
+            is Operation.Subtract -> subtract(operandA, operandB)
+            is Operation.Multiply -> multiply(operandA, operandB)
+            is Operation.Divide -> divide(operandA, operandB)
             else -> operandB
         }
     }
@@ -46,9 +49,9 @@ class Calculator {
     fun divide(operandA: Int, operandB: Int): Int = operandA / operandB
 }
 
-sealed class Operation{
-    object Add:Operation()
-    object Subtract:Operation()
-    object Multiply:Operation()
-    object Divide:Operation()
+sealed class Operation {
+    object Add : Operation()
+    object Subtract : Operation()
+    object Multiply : Operation()
+    object Divide : Operation()
 }
