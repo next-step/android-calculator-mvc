167ca63d31b4453443ae459e196c7c24d7deef3b
Junpil Park <iamfeel@gmail.com>
2022-01-18 12:21:36 +0900

    test: calculateTest 들의 expected 파라메터 타입을 Double 로 변경

--- app/src/test/java/com/nextstep/domain/StringCalculatorTest.kt
+++ app/src/test/java/com/nextstep/domain/StringCalculatorTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.EmptySource
-import kotlin.math.roundToInt
 
 internal class StringCalculatorTest {
 
@@ -23,45 +22,45 @@ internal class StringCalculatorTest {
 
     @DisplayName("+ 로 이루어진 연산자들의 계산이 정상적으로 된다.")
     @ParameterizedTest
-    @CsvSource(value = ["1 + 2,3", "1 + 4 + 6,11", "10 + 21 + 30 + 0,61"])
-    fun calculateTest_plus(formula: String, expected: String) {
+    @CsvSource(value = ["1 + 2,3.0", "1 + 4 + 6,11.0", "10 + 21 + 30 + 0,61.0"])
+    fun calculateTest_plus(formula: String, expected: Double) {
         val stringCalculator = StringCalculator()
 
         val result = stringCalculator.calculate(formula)
 
-        Truth.assertThat(result.roundToInt().toString()).isEqualTo(expected)
+        Truth.assertThat(result).isEqualTo(expected)
     }
 
     @DisplayName("* 로 이루어진 연산자들의 계산이 정상적으로 된다.")
     @ParameterizedTest
     @CsvSource(value = ["1 * 2,2", "1 * 4 * 6,24", "2 * 21 * 0 * 1,0"])
-    fun calculateTest_multiple(formula: String, expected: String) {
+    fun calculateTest_multiple(formula: String, expected: Double) {
         val stringCalculator = StringCalculator()
 
         val result = stringCalculator.calculate(formula)
 
-        Truth.assertThat(result.roundToInt().toString()).isEqualTo(expected)
+        Truth.assertThat(result).isEqualTo(expected)
     }
 
     @DisplayName("- 로 이루어진 연산자들의 계산이 정상적으로 된다.")
     @ParameterizedTest
     @CsvSource(value = ["1 - 2,-1", "-1 - 4 - 6,-11", "10 - 2,8"])
-    fun calculateTest_minus(formula: String, expected: String) {
+    fun calculateTest_minus(formula: String, expected: Double) {
         val stringCalculator = StringCalculator()
 
         val result = stringCalculator.calculate(formula)
 
-        Truth.assertThat(result.roundToInt().toString()).isEqualTo(expected)
+        Truth.assertThat(result).isEqualTo(expected)
     }
 
     @DisplayName("/ 로 이루어진 연산자들의 계산이 정상적으로 된다.")
     @ParameterizedTest
     @CsvSource(value = ["1 / 2,0.5", "8 / 4 / 2,1", "10 / 2,5"])
-    fun calculateTest_divide(formula: String, expected: String) {
+    fun calculateTest_divide(formula: String, expected: Double) {
         val stringCalculator = StringCalculator()
 
         val result = stringCalculator.calculate(formula)
 
-        Truth.assertThat(result.roundToInt().toString()).isEqualTo(expected)
+        Truth.assertThat(result).isEqualTo(expected)
     }
 }
