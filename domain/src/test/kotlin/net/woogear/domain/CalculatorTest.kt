9898d18bd02b587328154e4ca7b6e2ae1caa9c43
Woogear Kwon <techkwon1717@hanmail.net>
2022-01-20 08:35:57 +0900

    [test] 테스트 함수명 수정

--- domain/src/test/kotlin/net/woogear/domain/CalculatorTest.kt
+++ domain/src/test/kotlin/net/woogear/domain/CalculatorTest.kt
@@ -7,47 +7,47 @@ class CalculatorTest {
     private val calculator = Calculator()
 
     @Test
-    fun test_one_plus_two_plus_three() {
+    fun `1 plus 2 plus 3 equals 6`() {
         val result: Int = calculator.evaluate("1 + 2 + 3")
         assertEquals(6, result)
     }
 
     @Test
-    fun ten_minus_two_minus_five() {
+    fun `10 minus 2 minus 5 equals 3`() {
         val result = calculator.evaluate("10 - 2 - 5")
         assertEquals(3, result)
     }
 
     @Test
-    fun three_times_four_times_two() {
+    fun `3 multiplied by 4 multiplied by 2 equals 24`() {
         val result = calculator.evaluate("3 * 4 * 2")
         assertEquals(24, result)
     }
 
     @Test
-    fun fifty_divided_by_twenty_five() {
+    fun `50 divided by 25 equals 2`() {
         val result = calculator.evaluate("50 / 25")
         assertEquals(2, result)
     }
 
     @Test
-    fun two_plus_three_times_four_divided_by_five_minus_one() {
+    fun `2 plus 3 multiplied by 4 divided by 6 minus 1 equals 3`() {
         val result = calculator.evaluate("2 + 3 * 4 / 5 - 1")
         assertEquals(3, result)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun null_input() {
+    fun `null input throws an error`() {
         calculator.evaluate(null)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun empty_input() {
+    fun `empty input text throws an error`() {
         calculator.evaluate("")
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun no_supported_input_text() {
+    fun `input text contains illegal character throws an_error`() {
         calculator.evaluate("2 & 5")
     }
 }
