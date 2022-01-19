db431af511ead76ad92735c7a20c4bfabac3f268
dlwls5201 <dlwls5201@naver.com>
2022-01-19 20:13:33 +0900

    feature : operation 인터페이스 추가

--- domain/src/main/java/com/example/domain/Operation.kt
+++ domain/src/main/java/com/example/domain/Operation.kt
@@ -1,10 +1,18 @@
 package com.example.domain
 
-enum class Operation(val operation: String) {
-    PLUS("+"),
-    SUBTRACTION("-"),
-    MULTIPLICATION("*"),
-    DIVISION("/"),
+enum class Operation(val operation: String) : IOperation {
+    PLUS("+") {
+        override fun calculate(input1: Float, input2: Float) = input1 + input2
+    },
+    SUBTRACTION("-") {
+        override fun calculate(input1: Float, input2: Float) = input1 - input2
+    },
+    MULTIPLICATION("*") {
+        override fun calculate(input1: Float, input2: Float) = input1 * input2
+    },
+    DIVISION("/") {
+        override fun calculate(input1: Float, input2: Float) = input1 / input2
+    },
     ;
 
     companion object {
@@ -13,19 +21,13 @@ enum class Operation(val operation: String) {
 
         fun get(operation: String) = values().find { it.operation == operation }
 
-        fun calculate(input1: Float, operator: Operation, input2: Float) = when (operator) {
-            PLUS -> {
-                input1 + input2
-            }
-            SUBTRACTION -> {
-                input1 - input2
-            }
-            MULTIPLICATION -> {
-                input1 * input2
-            }
-            DIVISION -> {
-                input1 / input2
-            }
-        }
+        fun calculate(input1: Float, operator: Operation, input2: Float) =
+            operator.calculate(input1, input2)
     }
 }
+
+interface IOperation {
+
+    fun calculate(input1: Float, input2: Float): Float
+}
+
