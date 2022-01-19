9f042fe3f55ff0c909dcc80e6d9a6852aec89c09
luvimperfection <luvimperfection@gmail.com>
2022-01-20 07:53:59 +0900

    STEP3: ExpressionElement, Operation, Operand 추가해서 구조 변경

--- domain/src/main/java/com/example/domain/Calculator.kt
+++ domain/src/main/java/com/example/domain/Calculator.kt
@@ -5,53 +5,66 @@ class Calculator {
         if (expression.isNullOrBlank()) throw IllegalArgumentException()
         var currentValue = 0
         expression.split(" ").forEach { chunk ->
-            currentValue = calculate(currentValue, chunk)
-        }
-        return currentValue
-    }
-
-    fun calculate(currentValue: Int, expression: String): Int {
-        var currentOperation: Operation? = null
-        var currentValue = currentValue
-        expression.forEach {
-            when (it) {
-                '+' -> currentOperation = Operation.Add
-                '-' -> currentOperation = Operation.Subtract
-                '*' -> currentOperation = Operation.Multiply
-                '/' -> currentOperation = Operation.Divide
-                else ->
-                    currentValue = if (it.isDigit()) {
-                        updateValue(currentOperation, currentValue, it.digitToInt())
-                    } else {
-                        throw IllegalArgumentException()
-                    }
+            var currentOperation: Operation? = null
+            ExpressionElement.parse(chunk).forEach {
+                when (it) {
+                    is Operation -> currentOperation = it
+                    is Operand -> currentValue = currentOperation?.let { operation ->
+                        updateValue(operation, Operand(currentValue), it).toInt()
+                    } ?: it.toInt()
+                }
             }
         }
         return currentValue
     }
 
-    fun updateValue(operation: Operation?, operandA: Int, operandB: Int): Int {
+    fun updateValue(operation: Operation, operandA: Operand, operandB: Operand): Operand {
         return when (operation) {
             is Operation.Add -> add(operandA, operandB)
             is Operation.Subtract -> subtract(operandA, operandB)
             is Operation.Multiply -> multiply(operandA, operandB)
             is Operation.Divide -> divide(operandA, operandB)
-            else -> operandB
         }
     }
 
-    fun add(operandA: Int, operandB: Int): Int = operandA + operandB
+    fun add(operandA: Operand, operandB: Operand): Operand = operandA + operandB
 
-    fun subtract(operandA: Int, operandB: Int): Int = operandA - operandB
+    fun subtract(operandA: Operand, operandB: Operand): Operand = operandA - operandB
 
-    fun multiply(operandA: Int, operandB: Int): Int = operandA * operandB
+    fun multiply(operandA: Operand, operandB: Operand): Operand = operandA * operandB
 
-    fun divide(operandA: Int, operandB: Int): Int = operandA / operandB
+    fun divide(operandA: Operand, operandB: Operand): Operand = operandA / operandB
 }
 
-sealed class Operation {
+sealed class ExpressionElement {
+    companion object {
+        fun parse(rawExpression: String): List<ExpressionElement> {
+            return rawExpression.map {
+                when (it) {
+                    '+' -> Operation.Add
+                    '-' -> Operation.Subtract
+                    '*' -> Operation.Multiply
+                    '/' -> Operation.Divide
+                    else -> it.digitToIntOrNull()?.let { value ->
+                        Operand(value)
+                    } ?: throw IllegalArgumentException()
+                }
+            }
+        }
+    }
+}
+
+sealed class Operation : ExpressionElement() {
     object Add : Operation()
     object Subtract : Operation()
     object Multiply : Operation()
     object Divide : Operation()
+}
+
+class Operand(private val value: Int) : ExpressionElement() {
+    operator fun plus(other: Operand): Operand = Operand(this.value + other.value)
+    operator fun minus(other: Operand): Operand = Operand(this.value - other.value)
+    operator fun times(other: Operand): Operand = Operand(this.value * other.value)
+    operator fun div(other: Operand): Operand = Operand(this.value / other.value)
+    fun toInt(): Int = value
 }
