0e7a97981eb28a74080af0147be8d1201c52e243
Woogear Kwon <techkwon1717@hanmail.net>
2022-01-20 08:25:12 +0900

    [Refactor] Calculator 전역변수를 로컬변수로 수정

--- domain/src/main/java/net/woogear/domain/Calculator.kt
+++ domain/src/main/java/net/woogear/domain/Calculator.kt
@@ -4,19 +4,19 @@ import net.woogear.domain.OperationType.Companion.getOperationType
 import net.woogear.domain.OperationType.Companion.isOperator
 
 class Calculator {
-    private var answer = 0
-    private var operationType: OperationType = OperationType.PLUS
 
     fun evaluate(s: String?): Int {
         if (s.isNullOrEmpty()) {
             throw IllegalArgumentException("Input Text Can't Be a Null or Empty")
         }
 
-        calculate(s.split(" "))
-        return answer
+        return calculate(s.split(" "))
     }
 
-    private fun calculate(splitTexts: List<String>) {
+    private fun calculate(splitTexts: List<String>): Int {
+        var answer = 0
+        var operationType: OperationType = OperationType.PLUS
+
         for (text in splitTexts) {
             when {
                 text.isOperator() -> operationType = getOperationType(text)
@@ -24,6 +24,8 @@ class Calculator {
                 else -> throw IllegalArgumentException("$text is not supported type for input text")
             }
         }
+
+        return answer
     }
 
     private fun String.isOperator(): Boolean {
