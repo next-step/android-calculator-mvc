93c9ea7756b497c21e7c2d3f4b01708846e68fb0
chohohee <gone1005@gmail.com>
2022-01-20 19:26:34 +0900

    feat: 문자열 사칙 연산 계산기 구현

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -2,6 +2,36 @@ package edu.nextstep.camp.calculator.domain
 
 class Calculator {
 
+    fun evaluate(data: String): Float {
+        var result = 0f
+        var operator = "none"
+
+        if (data.isEmpty()) {
+            throw IllegalArgumentException("exception data")
+        } else {
+            data.forEach {
+                if (it.isDigit()) { // 숫자
+                    when (operator) {
+                        "+" -> result = plus(result, Character.getNumericValue(it).toFloat())
+                        "-" -> result = minus(result, Character.getNumericValue(it).toFloat())
+                        "*" -> result = multipliedBy(result, Character.getNumericValue(it).toFloat())
+                        "/" -> result = dividedBy(result, Character.getNumericValue(it).toFloat())
+                        "none" -> result = Character.getNumericValue(it).toFloat()
+                    }
+                } else { // 연산
+                    when (it.toString()) {
+                        "+" -> operator = "+"
+                        "-" -> operator = "-"
+                        "×" -> operator = "*"
+                        "÷" -> operator = "/"
+                        else -> throw IllegalArgumentException("exception operator")
+                    }
+                }
+            }
+        }
+        return result
+    }
+
     fun plus(first: Float, second: Float): Float {
         return first + second
     }
@@ -17,5 +47,4 @@ class Calculator {
     fun dividedBy(first: Float, second: Float): Float {
         return first / second
     }
-
 }
