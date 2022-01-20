031058382fded8494a67a730a4e86239c065715e
jeeunman <manjee.official@gmail.com>
2022-01-21 02:09:44 +0900

    feat 계산 로직 추가

--- domain/src/main/java/com/manjee/domain/Calculator.kt
+++ domain/src/main/java/com/manjee/domain/Calculator.kt
@@ -3,10 +3,21 @@ package com.manjee.domain
 class Calculator {
 
     fun calculate(input: String?): Int {
+        var currentValue = 0
+        var formulaType = FormulaType.PLUS
+
         if (input.isNullOrEmpty()) {
             throw IllegalArgumentException("입력 값은 null 이거나 비어있을 수 없습니다")
         }
 
-        return 246
+        input.split(" ").forEach {
+            when {
+                FormulaType.isFormulaType(it) -> formulaType = FormulaType.getFormulaType(it)
+                it.toIntOrNull() != null -> currentValue =
+                    formulaType.calculate(currentValue, it.toInt())
+            }
+        }
+
+        return currentValue
     }
 }
