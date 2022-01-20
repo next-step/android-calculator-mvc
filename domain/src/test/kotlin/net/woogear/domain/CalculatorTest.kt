e24a43daf82c1b1ca8564de41c4fbd03a7646ce9
Woogear Kwon <techkwon1717@hanmail.net>
2022-01-20 12:16:48 +0900

    [test] 테스트 함수명 한글로 수정

--- domain/src/test/kotlin/net/woogear/domain/CalculatorTest.kt
+++ domain/src/test/kotlin/net/woogear/domain/CalculatorTest.kt
@@ -7,47 +7,47 @@ class CalculatorTest {
     private val calculator = Calculator()
 
     @Test
-    fun `1 plus 2 plus 3 equals 6`() {
+    fun `1 더하기 2 더하기 3 = 6`() {
         val result: Int = calculator.evaluate("1 + 2 + 3")
         assertEquals(6, result)
     }
 
     @Test
-    fun `10 minus 2 minus 5 equals 3`() {
+    fun `10 빼기 2 빼기 5 = 3`() {
         val result = calculator.evaluate("10 - 2 - 5")
         assertEquals(3, result)
     }
 
     @Test
-    fun `3 multiplied by 4 multiplied by 2 equals 24`() {
+    fun `3 곱하기 by 4 곱하기 2 = 24`() {
         val result = calculator.evaluate("3 * 4 * 2")
         assertEquals(24, result)
     }
 
     @Test
-    fun `50 divided by 25 equals 2`() {
+    fun `50 나누기 25 = 2`() {
         val result = calculator.evaluate("50 / 25")
         assertEquals(2, result)
     }
 
     @Test
-    fun `2 plus 3 multiplied by 4 divided by 6 minus 1 equals 3`() {
+    fun `2 더하기 3 곱하기 4 나누기 6 빼기 1 = 3`() {
         val result = calculator.evaluate("2 + 3 * 4 / 5 - 1")
         assertEquals(3, result)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun `null input throws an error`() {
+    fun `입력값이 null 이면 error 발생`() {
         calculator.evaluate(null)
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun `empty input text throws an error`() {
+    fun `입력값이 공백이면 error 발생`() {
         calculator.evaluate("")
     }
 
     @Test(expected = IllegalArgumentException::class)
-    fun `input text contains illegal character throws an_error`() {
+    fun `입력값에 연산기호 외의 문자가 들어가면 error 발생`() {
         calculator.evaluate("2 & 5")
     }
 }
