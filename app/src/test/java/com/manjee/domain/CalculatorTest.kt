c1127ca0ee3d16b37d424fad3fa1e2cfd41b712f
jeeunman <manjee.official@gmail.com>
2022-01-21 02:08:59 +0900

    feat 사칙연산에 대한 테스트 추가

--- app/src/test/java/com/manjee/domain/CalculatorTest.kt
+++ app/src/test/java/com/manjee/domain/CalculatorTest.kt
@@ -12,6 +12,21 @@ class CalculatorTest {
         assertThat(calculator.calculate("123 + 123")).isEqualTo(246)
     }
 
+    @Test
+    fun `123 - 5 - 10 - 8`() {
+        assertThat(calculator.calculate("123 - 5 - 10 - 8")).isEqualTo(100)
+    }
+
+    @Test
+    fun `125-123 나누기 2 = 1`() {
+        assertThat(calculator.calculate("125 - 123 / 2")).isEqualTo(1)
+    }
+
+    @Test
+    fun `12 * 12 = 144`() {
+        assertThat(calculator.calculate("12 * 12")).isEqualTo(144)
+    }
+
     @Test(expected = IllegalArgumentException::class)
     fun `0으로 나누면 IllegalArgumentException 발생`() {
         calculator.calculate("2 / 0")
