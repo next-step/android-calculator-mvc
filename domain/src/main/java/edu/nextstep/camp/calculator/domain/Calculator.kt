3b12d4b0c7a65e1def91f889c8113dbcc337aaeb
chohohee <gone1005@gmail.com>
2022-01-21 09:10:41 +0900

    refactor: enum 클래스 정의 및 함수 분리

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -1,35 +1,45 @@
 package edu.nextstep.camp.calculator.domain
 
 class Calculator {
+    private var result = 0f
+    private var operator = Operator.NONE
 
     fun evaluate(data: String): Float {
-        var result = 0f
-        var operator = "none"
+        result = 0f
+        operator = Operator.NONE
 
         if (data.isEmpty()) {
             throw IllegalArgumentException("exception data")
         } else {
             data.forEach {
-                if (it.isDigit()) { // 숫자
-                    when (operator) {
-                        "+" -> result = plus(result, Character.getNumericValue(it).toFloat())
-                        "-" -> result = minus(result, Character.getNumericValue(it).toFloat())
-                        "*" -> result = multipliedBy(result, Character.getNumericValue(it).toFloat())
-                        "/" -> result = dividedBy(result, Character.getNumericValue(it).toFloat())
-                        "none" -> result = Character.getNumericValue(it).toFloat()
-                    }
-                } else { // 연산
-                    when (it.toString()) {
-                        "+" -> operator = "+"
-                        "-" -> operator = "-"
-                        "×" -> operator = "*"
-                        "÷" -> operator = "/"
-                        else -> throw IllegalArgumentException("exception operator")
-                    }
+                if (it.isDigit()) {
+                    calculate(it)
+                } else {
+                    setOperator(it)
                 }
             }
         }
         return result
+    }
+
+    private fun calculate(char: Char) {
+        result = when (operator) {
+            Operator.PLUS -> plus(result, Character.getNumericValue(char).toFloat())
+            Operator.MINUS -> minus(result, Character.getNumericValue(char).toFloat())
+            Operator.MULTIPLY -> multipliedBy(result, Character.getNumericValue(char).toFloat())
+            Operator.DIVIDE -> dividedBy(result, Character.getNumericValue(char).toFloat())
+            Operator.NONE -> Character.getNumericValue(char).toFloat()
+        }
+    }
+
+    private fun setOperator(char: Char) {
+        operator = when (char.toString()) {
+            "+" -> Operator.PLUS
+            "-" -> Operator.MINUS
+            "×" -> Operator.MULTIPLY
+            "÷" -> Operator.DIVIDE
+            else -> throw IllegalArgumentException("exception operator")
+        }
     }
 
     fun plus(first: Float, second: Float): Float {
---
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Operator.kt
@@ -0,0 +1,5 @@
+package edu.nextstep.camp.calculator.domain
+
+enum class Operator {
+    PLUS, MINUS, MULTIPLY, DIVIDE, NONE
+}
