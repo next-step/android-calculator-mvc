2ebb2fe11f3c82a6237cbe54055608145d9a2364
chohohee <gone1005@gmail.com>
2022-01-21 10:30:36 +0900

    test: exception 처리 방식 수정 및 메서드 명 구체적으로 수정

--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
@@ -1,60 +1,77 @@
 package edu.nextstep.camp.calculator.domain
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertThrows
 import org.junit.Test
+
 
 class CalculatorTest() {
 
     @Test
-    fun `두 수를 더하면 정답이 나와야 한다`() {
+    fun `두 수를 더하면 더한 값이 나와야 한다`() {
         // when
         val result = Calculator().plus(5f, 2f)
+
         // then
         assertThat(result).isEqualTo(7)
     }
 
     @Test
-    fun `두 수를 빼면 정답이 나와야 한다`() {
+    fun `두 수를 빼면 뺀 값이 나와야 한다`() {
         // when
         val result = Calculator().minus(5f, 2f)
+
         // then
         assertThat(result).isEqualTo(3)
     }
 
     @Test
-    fun `두 수를 곱하면 정답이 나와야 한다`() {
+    fun `두 수를 곱하면 곱한 값이 나와야 한다`() {
         // when
         val result = Calculator().multipliedBy(5f, 2f)
+
         // then
         assertThat(result).isEqualTo(10)
     }
 
     @Test
-    fun `두 수를 나누면 정답이 나와야 한다`() {
+    fun `두 수를 나누면 나눈 값이 나와야 한다`() {
         // when
         val result = Calculator().dividedBy(5f, 2f)
+
         // then
         assertThat(result).isEqualTo(2.5f)
     }
 
     @Test
-    fun `입력된 값은 정답이 나와야 한다`() {
+    fun `입력된 수식은 연산에 맞는 값이 나와야 한다`() {
         // when
         val result = Calculator().evaluate("8+2×5÷4")
+
         // then
         assertThat(result).isEqualTo(12.5f)
     }
 
-    @Test(expected = IllegalArgumentException::class)
+    @Test
     fun `입력된 값이 없으면 오류가 난다`() {
         // when
-        val result = Calculator().evaluate("")
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            Calculator().evaluate("")
+        }
+
+        //then
+        assertThat(exception.message).isEqualTo("exception data")
     }
 
-    @Test(expected = IllegalArgumentException::class)
-    fun `사칙연산이 아닌 경우 오류가 난다`() {
+    @Test
+    fun `사칙연산이 아닌 숫자가 아닌 문자가 존재하는 경우 오류가 난다`() {
         // when
-        val result = Calculator().evaluate("2^3")
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            Calculator().evaluate("2^3")
+        }
+
+        //then
+        assertThat(exception.message).isEqualTo("exception operator")
     }
 
 }
