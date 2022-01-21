47767370a1ba9fc0fdab6e8c09aba2b845989f99
LeeChungWoo <skcjddn@gmail.com>
2022-01-21 16:37:15 +0900

    refactor: OperationType class 수정 및 함수추가

--- domain/src/main/java/com/lcw/study/nextstep/domain/OperationType.kt
+++ domain/src/main/java/com/lcw/study/nextstep/domain/OperationType.kt
@@ -1,8 +1,23 @@
 package com.lcw.study.nextstep.domain
 
-enum class OperationType(operation: String) {
-    PLUS("+"),
-    MINUS("-"),
-    MULTIPLY("*"),
-    DIVIDE("/")
+enum class OperationType {
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    NOTOPERATIONTYPE;
+
+     fun checkOperationType(text: String): Boolean { //입력된 문자가 사칙 연산 기호인지 체크
+        return text == "+" || text == "-" || text == "*" || text == "/"
+    }
+
+     fun changeTextToOperation(text: String):OperationType{
+        when(text){
+            "+"->return PLUS
+            "-"->return MINUS
+            "*"->return MULTIPLY
+            "/"->return DIVIDE
+        }
+         return NOTOPERATIONTYPE
+    }
 }
