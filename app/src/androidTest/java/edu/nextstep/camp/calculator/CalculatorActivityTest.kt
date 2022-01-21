3f422e7e6c71fcb098c48c627f31bd164dc200df
B2110353-F5\BGF <juseyo86@naver.com>
2022-01-21 12:00:54 +0900

    feat : UI 테스트시 파일 새로 생성

---
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/CalculatorActivityTest.kt
@@ -0,0 +1,11 @@
+package edu.nextstep.camp.calculator
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+
+class CalculatorActivityTest {
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    
+}
