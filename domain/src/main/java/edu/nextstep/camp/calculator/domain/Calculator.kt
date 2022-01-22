52358865755dca88736d2d319dcfe751b10e346d
chohohee <gone1005@gmail.com>
2022-01-22 22:32:29 +0900

    refactor: 객체의 역활 분리

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -1,60 +1,32 @@
 package edu.nextstep.camp.calculator.domain
 
 class Calculator {
-    private var result = 0f
-    private var operator = Operator.NONE
 
     fun evaluate(data: String): Float {
-        result = 0f
-        operator = Operator.NONE
+        var result = 0f
+        var operator = Operator.NONE
 
         if (data.isEmpty()) {
             throw IllegalArgumentException("exception data")
         } else {
             data.forEach {
                 if (it.isDigit()) {
-                    calculate(it)
+                    result = calculate(it, operator, result)
                 } else {
-                    setOperator(it)
+                    operator = Operator.getOperator(it.toString())
                 }
             }
         }
         return result
     }
 
-    private fun calculate(char: Char) {
-        result = when (operator) {
-            Operator.PLUS -> plus(result, Character.getNumericValue(char).toFloat())
-            Operator.MINUS -> minus(result, Character.getNumericValue(char).toFloat())
-            Operator.MULTIPLY -> multipliedBy(result, Character.getNumericValue(char).toFloat())
-            Operator.DIVIDE -> dividedBy(result, Character.getNumericValue(char).toFloat())
+    private fun calculate(char: Char, operator: Operator, result: Float): Float {
+        return when (operator) {
+            Operator.PLUS -> Operator.plus(result, Character.getNumericValue(char).toFloat())
+            Operator.MINUS -> Operator.minus(result, Character.getNumericValue(char).toFloat())
+            Operator.MULTIPLY -> Operator.multipliedBy(result, Character.getNumericValue(char).toFloat())
+            Operator.DIVIDE -> Operator.dividedBy(result, Character.getNumericValue(char).toFloat())
             Operator.NONE -> Character.getNumericValue(char).toFloat()
         }
-    }
-
-    private fun setOperator(char: Char) {
-        operator = when (char.toString()) {
-            "+" -> Operator.PLUS
-            "-" -> Operator.MINUS
-            "×" -> Operator.MULTIPLY
-            "÷" -> Operator.DIVIDE
-            else -> throw IllegalArgumentException("exception operator")
-        }
-    }
-
-    fun plus(first: Float, second: Float): Float {
-        return first + second
-    }
-
-    fun minus(first: Float, second: Float): Float {
-        return first - second
-    }
-
-    fun multipliedBy(first: Float, second: Float): Float {
-        return first * second
-    }
-
-    fun dividedBy(first: Float, second: Float): Float {
-        return first / second
     }
 }
