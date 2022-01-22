f13d57ddccc98a1b6319ae4aacd3060310da0e49
Woogear Kwon <woogear.kwon@huray.net>
2022-01-22 23:23:18 +0900

    [Refactor] 숫자 & 연산 기호 버튼의 setOnClickListener 설정에서 자신의 텍스트 값을 넘기도록 수정

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.nextstep.camp.calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
@@ -16,15 +18,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setClickListeners() {
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        binding.button0.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button1.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button2.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button3.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button4.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button5.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button6.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button7.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button8.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.button9.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.buttonDivide.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.buttonMultiply.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.buttonMinus.setOnClickListener { input((it as TextView).text.toString()) }
+        binding.buttonPlus.setOnClickListener { input((it as TextView).text.toString()) }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun input(newText: String) {
+        binding.textView.text = newText
     }
 }
