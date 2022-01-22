3eca5be8a7b289712130742df93d36f96e675fcc
devsoupe <luvyum4e@gmail.com>
2022-01-22 10:30:30 +0900

    feat : 계산기 UI 작성 및 Calculator 기능 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,8 +1,12 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
+import edu.nextstep.camp.calculator.domain.Calculator
+import edu.nextstep.camp.calculator.domain.Calculator.Result
+import edu.nextstep.camp.calculator.domain.Calculator.Result.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,13 +21,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupUi() {
-        val buttonViews = listOf(
+        val buttons = listOf(
             binding.button0, binding.button1, binding.button2, binding.button3, binding.button4,
             binding.button5, binding.button6, binding.button7, binding.button8, binding.button9,
+            binding.buttonPlus, binding.buttonMinus, binding.buttonMultiply, binding.buttonDivide
         )
 
-        buttonViews.forEach { button ->
-            button.setOnClickListener { binding.tvResultDisplay.text = button.text }
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                Calculator.insert(button.text.toString())
+                updateDisplay()
+            }
+        }
+
+        binding.buttonDelete.setOnClickListener {
+            Calculator.delete()
+            updateDisplay()
+        }
+
+        binding.buttonEquals.setOnClickListener {
+            Calculator.evaluate()
+            updateDisplay()
+        }
+    }
+
+    private fun updateDisplay() {
+        when (val result = Calculator.result()) {
+            is Success -> binding.tvResultDisplay.text = result.value
+            is Failure -> Toast.makeText(this, getString(R.string.error_incomplete_expression), Toast.LENGTH_SHORT).show()
         }
     }
 }
--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -1,13 +1,53 @@
 package edu.nextstep.camp.calculator.domain
 
+const val DELIMITER = " "
+
 object Calculator {
 
-    fun evaluate(expression: String?, delimiter: String): Int {
-        val chunks = ExpressionParser.parse(expression, delimiter)
+    sealed class Result {
+
+        data class Success(val value: String): Result()
+        object Failure: Result()
+    }
+
+    private val expressionGenerator = ExpressionGenerator(DELIMITER)
+    private val expressionParser = ExpressionParser(DELIMITER)
+
+    private var result: Result? = null
+
+    fun insert(symbol: String) {
+        expressionGenerator.append(symbol)
+        result = Result.Success(expressionGenerator.generate())
+    }
+
+    fun delete() {
+        expressionGenerator.delete()
+        result = Result.Success(expressionGenerator.generate())
+    }
+
+    fun evaluate() {
+        if (!expressionGenerator.isValid()) {
+            result = Result.Failure
+            return
+        }
+
+        val expression = expressionGenerator.generate()
+        val evaluated = evaluate(expression)
+
+        expressionGenerator.update(evaluated)
+        result = Result.Success(evaluated)
+    }
+
+    fun evaluate(expression: String?): String {
+        val chunks = expressionParser.parse(expression)
         val firstDigit = chunks.first().toInt()
         val remainChunks = chunks.drop(1)
 
         return remainChunks.chunked(2)
             .fold(firstDigit) { acc, (token, digit) -> BinaryCalculator.of(token).evaluate(acc, digit.toInt()) }
+            .toString()
     }
+
+    fun result() = result
 }
+
