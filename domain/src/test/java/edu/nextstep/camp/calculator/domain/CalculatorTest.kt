2d392863b99d9182bee879bc8cd5176294e65fcd
chohohee <gone1005@gmail.com>
2022-01-22 22:38:24 +0900

    test: 테스트 케이스 메서드 명 수정 및 assertThrows 인 경우 assertThat 생략

--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
@@ -8,7 +8,7 @@ import org.junit.Test
 class CalculatorTest() {
 
     @Test
-    fun `두 수를 더하면 더한 값이 나와야 한다`() {
+    fun `5와 2를 더하면 7이 된다`() {
         // when
         val result = Operator.plus(5f, 2f)
 
@@ -17,7 +17,7 @@ class CalculatorTest() {
     }
 
     @Test
-    fun `두 수를 빼면 뺀 값이 나와야 한다`() {
+    fun `5와 2를 빼면 3이 된다`() {
         // when
         val result = Operator.minus(5f, 2f)
 
@@ -26,7 +26,7 @@ class CalculatorTest() {
     }
 
     @Test
-    fun `두 수를 곱하면 곱한 값이 나와야 한다`() {
+    fun `5와 2를 곱하면 10이 된다`() {
         // when
         val result = Operator.multipliedBy(5f, 2f)
 
@@ -35,7 +35,7 @@ class CalculatorTest() {
     }
 
     @Test
-    fun `두 수를 나누면 나눈 값이 나와야 한다`() {
+    fun `5와 2를 나누면 2점5가 된다`() {
         // when
         val result = Operator.dividedBy(5f, 2f)
 
@@ -44,7 +44,7 @@ class CalculatorTest() {
     }
 
     @Test
-    fun `입력된 수식은 연산에 맞는 값이 나와야 한다`() {
+    fun `8 더하기 2 곱하기 5 나누기 4 는 사칙연산 우선순위가 아닌 순서대로 연산하여 12점5가 된다`() {
         // when
         val result = Calculator.evaluate("8+2×5÷4")
 
@@ -53,25 +53,18 @@ class CalculatorTest() {
     }
 
     @Test
-    fun `입력된 값이 없으면 오류가 난다`() {
+    fun `입력된 값이 공백이면 IllegalArgumentException 오류가 발생한다`() {
         // when
-        val exception = assertThrows(IllegalArgumentException::class.java) {
+        assertThrows(IllegalArgumentException::class.java) {
             Calculator.evaluate("")
         }
-
-        //then
-        assertThat(exception.message).isEqualTo("exception data")
     }
 
     @Test
-    fun `사칙연산이 아닌 숫자가 아닌 문자가 존재하는 경우 오류가 난다`() {
+    fun `숫자가 아닌 기호 중에 사칙연산이 아닌 경우 IllegalArgumentException 오류가 발생한다`() {
         // when
-        val exception = assertThrows(IllegalArgumentException::class.java) {
+        assertThrows(IllegalArgumentException::class.java) {
             Calculator.evaluate("2^3")
         }
-
-        //then
-        assertThat(exception.message).isEqualTo("exception operator")
     }
-
 }
