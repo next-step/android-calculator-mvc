ced7d4ba3405e2e87b4c1cd79808033e537c0d13
DoDoBest <av449@naver.com>
2022-01-23 17:07:16 +0900

    feat: 버튼 기능 구현

---
+++ domain/src/main/java/com/github/dodobest/domain/InputFarm.kt
@@ -0,0 +1,55 @@
+package com.github.dodobest.domain
+
+class InputFarm {
+    private val calculator = Calculator()
+    private val _numAndSignArray: MutableList<String> = MutableList(0) { "" }
+    private val _arraySize: Int get() = _numAndSignArray.size
+
+    fun handleInputArithmetic(inputOperation: String) {
+        if (_arraySize == 0) return
+
+        if (calculator.charIsOperation(_numAndSignArray.last().toCharArray().last())) {
+            _numAndSignArray[_arraySize - 1] = inputOperation
+            return
+        }
+
+        _numAndSignArray.add(inputOperation)
+    }
+
+    fun handleInputNum (inputNum: String) {
+        if (_arraySize > 0 && calculator.checkCharIsNum(_numAndSignArray.last().toCharArray().last())) {
+            _numAndSignArray[_arraySize-1] = (_numAndSignArray.last().toInt() * 10 + inputNum.toInt()).toString()
+            return
+        }
+        _numAndSignArray.add(inputNum)
+    }
+
+    fun getString(): String {
+        return _numAndSignArray.joinToString("")
+    }
+
+    fun handleInputDelete() {
+        if (_arraySize == 0) return
+
+        if (_numAndSignArray.last().length > 1) {
+            _numAndSignArray[_arraySize-1] = (_numAndSignArray.last().toDouble().toInt() / 10 ).toString()
+            return
+        }
+
+        _numAndSignArray.removeLast()
+    }
+
+    fun checkExpressionCanCalculated(): Boolean {
+        if (calculator.charIsOperation(_numAndSignArray.last().toCharArray().last())) {
+            return false
+        }
+        return true
+    }
+
+    fun handleEquals(){
+        val res: String = calculator.calculate(_numAndSignArray).toString()
+
+        _numAndSignArray.clear()
+        _numAndSignArray.add(res)
+    }
+}
