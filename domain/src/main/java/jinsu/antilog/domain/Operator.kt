262f4ae3f8bc4ba91c7aa0877980d104ffcd6749
jinsu4755 <parkjinsu4755@gmail.com>
2022-01-24 23:33:14 +0900

    feat(step4): Operator.kt 피드백 반영
    - 각 심볼을 sealed 구현체 안에 넣어 응집도를 높이도록 수정
    - 동반 객체에서 심볼을 구현체 안에 있는 심볼과 비교할 수 있도록 수정

--- domain/src/main/java/jinsu/antilog/domain/Operator.kt
+++ domain/src/main/java/jinsu/antilog/domain/Operator.kt
@@ -2,18 +2,26 @@ package jinsu.antilog.domain
 
 sealed class Operator {
     object Plus : Operator() {
+        override val symbol: String
+            get() = "+"
         override fun operate(left: Double, right: Double): Double = left + right
     }
 
     object Minus : Operator() {
+        override val symbol: String
+            get() = "-"
         override fun operate(left: Double, right: Double): Double = left - right
     }
 
     object Multiply : Operator() {
+        override val symbol: String
+            get() = "*"
         override fun operate(left: Double, right: Double): Double = left * right
     }
 
     object Divide : Operator() {
+        override val symbol: String
+            get() = "/"
         override fun operate(left: Double, right: Double): Double {
             if (right == 0.0) throw IllegalArgumentException("0으로는 나눌 수 없습니다.")
             return left / right
@@ -21,13 +29,14 @@ sealed class Operator {
     }
 
     abstract fun operate(left:Double, right: Double) : Double
+    abstract val symbol : String
 
     companion object {
         fun findOperatorBySymbol(symbol: String) = when (symbol) {
-            "+" -> Plus
-            "-" -> Minus
-            "*" -> Multiply
-            "/" -> Divide
+            Plus.symbol -> Plus
+            Minus.symbol -> Minus
+            Multiply.symbol -> Multiply
+            Divide.symbol -> Divide
             else -> throw IllegalArgumentException("$symbol 을 연산자로 사용할 수 없습니다.")
         }
     }
