830ce3adbe0779de8834f821097fedb111828cc4
Woogear Kwon <woogear.kwon@huray.net>
2022-01-24 20:29:31 +0900

    [Refactor] 멀티 UI 클릭테스트 함수 생성

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -16,9 +16,7 @@ class MainActivityTest {
     fun click_1_2_3_shows_123() {
         // given
         // when: click 1 2 3
-        performClick(R.id.button1)
-        performClick(R.id.button2)
-        performClick(R.id.button3)
+        performClick(R.id.button1, R.id.button2, R.id.button3)
 
         // then: 화면에 123 이 보인다
         assertTextViewEquals("123")
@@ -28,11 +26,7 @@ class MainActivityTest {
     fun click_1_3_plus_2_2_shows_13_plus_22() {
         // given
         // when: click 1 3 + 2 2
-        performClick(R.id.button1)
-        performClick(R.id.button3)
-        performClick(R.id.buttonPlus)
-        performClick(R.id.button2)
-        performClick(R.id.button2)
+        performClick(R.id.button1, R.id.button3, R.id.buttonPlus, R.id.button2, R.id.button2)
 
         // then
         assertTextViewEquals("13 + 22")
@@ -42,10 +36,7 @@ class MainActivityTest {
     fun click_9_multiply_divide_3_shows_9_divide_3() {
         // given
         // when: click 9 * / 3
-        performClick(R.id.button9)
-        performClick(R.id.buttonMultiply)
-        performClick(R.id.buttonDivide)
-        performClick(R.id.button3)
+        performClick(R.id.button9, R.id.buttonMultiply, R.id.buttonDivide, R.id.button3)
 
         // then
         assertTextViewEquals("9 / 3")
@@ -55,14 +46,16 @@ class MainActivityTest {
     fun click_1_2_3_multiply_delete_delete_divide_3_shows_12_divide_3() {
         // given
         // when: click 1 2 3 * delete delete / 3
-        performClick(R.id.button1)
-        performClick(R.id.button2)
-        performClick(R.id.button3)
-        performClick(R.id.buttonMultiply)
-        performClick(R.id.buttonDelete)
-        performClick(R.id.buttonDelete)
-        performClick(R.id.buttonDivide)
-        performClick(R.id.button3)
+        performClick(
+            R.id.button1,
+            R.id.button2,
+            R.id.button3,
+            R.id.buttonMultiply,
+            R.id.buttonDelete,
+            R.id.buttonDelete,
+            R.id.buttonDivide,
+            R.id.button3
+        )
 
         // then
         assertTextViewEquals("12 / 3")
@@ -71,10 +64,7 @@ class MainActivityTest {
     @Test
     fun click_2_multiply_3_3_equals_66() {
         // given: click 2 * 3 3
-        performClick(R.id.button2)
-        performClick(R.id.buttonMultiply)
-        performClick(R.id.button3)
-        performClick(R.id.button3)
+        performClick(R.id.button2, R.id.buttonMultiply, R.id.button3, R.id.button3)
 
         // when: click =
         performClick(R.id.buttonEquals)
@@ -87,25 +77,31 @@ class MainActivityTest {
     fun click_6_6_divide_3_plus_3_multiply_2_equals_50() {
         // given: click 66 / 3 + 3 * 2
         // when: click =
-        performClick(R.id.button6)
-        performClick(R.id.button6)
-        performClick(R.id.buttonDivide)
-        performClick(R.id.button3)
-        performClick(R.id.buttonPlus)
-        performClick(R.id.button3)
-        performClick(R.id.buttonMultiply)
-        performClick(R.id.button2)
-        performClick(R.id.buttonEquals)
+        performClick(
+            R.id.button6,
+            R.id.button6,
+            R.id.buttonDivide,
+            R.id.button3,
+            R.id.buttonPlus,
+            R.id.button3,
+            R.id.buttonMultiply,
+            R.id.button2,
+            R.id.buttonEquals
+        )
 
         // then
         assertTextViewEquals("50")
     }
 
-    private fun performClick(id: Int) {
-        onView(withId(id)).perform(click())
+    private fun performClick(vararg ids: Int) {
+        ids.forEach { id ->
+            onView(withId(id)).perform(click())
+        }
     }
 
     private fun assertTextViewEquals(expected: String) {
         onView(withId(R.id.textView)).check(matches(withText(expected)))
     }
+
+
 }
