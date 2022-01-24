217e630e8e7ac2392859fd73bd94f29447745be6
yhwanshin <yhwanshin@smilegate.com>
2022-01-24 18:22:47 +0900

    feature : UI로직 추가

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,7 +1,6 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,21 +11,90 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val calculator = Calculator()
 
+    private val buttonNumberList: MutableList<Button> by lazy {
+        mutableListOf(
+            binding.button0, binding.button1,
+            binding.button2, binding.button3,
+            binding.button4, binding.button5,
+            binding.button6, binding.button7,
+            binding.button8, binding.button9,
+        )
+    }
+
+    private val buttonOperatorList: MutableList<Button> by lazy {
+        mutableListOf(
+            binding.buttonDivide, binding.buttonMultiply,
+            binding.buttonMinus, binding.buttonPlus
+        )
+    }
+
+    private var currentExpression: String? = ""
+        set(value) {
+            binding.textView.text = value
+            field = value
+        }
+        get() {
+            return binding.textView.text.toString()
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        initViews()
     }
 
+    private fun initViews() {
+        buttonNumberList.forEach { addNumberButtonListener(it) }
+        buttonOperatorList.forEach { addOperatorButtonListener(it) }
+        binding.buttonEquals.setOnClickListener { checkAndCompute() }
+    }
+
+    private fun addNumberButtonListener(button: Button) {
+        button.setOnClickListener { display(button) }
+    }
+
+    private fun addOperatorButtonListener(button: Button) {
+        button.setOnClickListener {
+            displayOperator(button)
+        }
+    }
+
+    private fun checkAndCompute() {
+        if (currentExpression.isNullOrEmpty()) {
+            displayToast(getString(R.string.toastIncompleteExpression))
+            return
+        }
+        if (!currentExpression!!.last().isDigit()) {
+            displayToast(getString(R.string.toastIncompleteExpression))
+            return
+        }
+
+        currentExpression = calculator.evaluate(currentExpression).toString()
+    }
+
+    private fun display(button: Button) {
+        currentExpression += button.text.toString()
+    }
+
+    private fun modify(operator: String) {
+        currentExpression = currentExpression!!
+            .subSequence(0 until currentExpression!!.lastIndex)
+            .toString()
+            .plus(operator)
+    }
+
+    private fun displayOperator(button: Button) {
+        if (currentExpression.isNullOrEmpty()) return
+        if (!currentExpression!!.last().isDigit()) {
+            modify(button.text.toString())
+            return
+        }
+        display(button)
+    }
+
+    private fun displayToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
 }
