4f3d0ced458d1c6485700e7668857ff5e9585dfb
DoDoBest <av449@naver.com>
2022-01-24 10:11:58 +0900

    style: 변수명 정정

--- domain/src/main/java/com/github/dodobest/domain/InputFarm.kt
+++ domain/src/main/java/com/github/dodobest/domain/InputFarm.kt
@@ -2,54 +2,54 @@ package com.github.dodobest.domain
 
 class InputFarm {
     private val calculator = Calculator()
-    private val _numAndSignArray: MutableList<String> = MutableList(0) { "" }
-    private val _arraySize: Int get() = _numAndSignArray.size
+    private val numAndSignArray: MutableList<String> = MutableList(0) { "" }
+    private val arraySize: Int get() = numAndSignArray.size
 
     fun handleInputArithmetic(inputOperation: String) {
-        if (_arraySize == 0) return
+        if (arraySize == 0) return
 
-        if (Operation.charIsOperation(_numAndSignArray.last().toCharArray().last())) {
-            _numAndSignArray[_arraySize - 1] = inputOperation
+        if (Operation.charIsOperation(numAndSignArray.last().toCharArray().last())) {
+            numAndSignArray[arraySize - 1] = inputOperation
             return
         }
 
-        _numAndSignArray.add(inputOperation)
+        numAndSignArray.add(inputOperation)
     }
 
     fun handleInputNum (inputNum: String) {
-        if (_arraySize > 0 && calculator.checkCharIsNum(_numAndSignArray.last().toCharArray().last())) {
-            _numAndSignArray[_arraySize-1] = (_numAndSignArray.last().toInt() * 10 + inputNum.toInt()).toString()
+        if (arraySize > 0 && calculator.checkCharIsNum(numAndSignArray.last().toCharArray().last())) {
+            numAndSignArray[arraySize-1] = (numAndSignArray.last().toInt() * 10 + inputNum.toInt()).toString()
             return
         }
-        _numAndSignArray.add(inputNum)
+        numAndSignArray.add(inputNum)
     }
 
     fun getString(): String {
-        return _numAndSignArray.joinToString("")
+        return numAndSignArray.joinToString("")
     }
 
     fun handleInputDelete() {
-        if (_arraySize == 0) return
+        if (arraySize == 0) return
 
-        if (_numAndSignArray.last().length > 1) {
-            _numAndSignArray[_arraySize-1] = (_numAndSignArray.last().toDouble().toInt() / 10 ).toString()
+        if (numAndSignArray.last().length > 1) {
+            numAndSignArray[arraySize-1] = (numAndSignArray.last().toDouble().toInt() / 10 ).toString()
             return
         }
 
-        _numAndSignArray.removeLast()
+        numAndSignArray.removeLast()
     }
 
     fun checkExpressionCanCalculated(): Boolean {
-        if (Operation.charIsOperation(_numAndSignArray.last().toCharArray().last())) {
+        if (Operation.charIsOperation(numAndSignArray.last().toCharArray().last())) {
             return false
         }
         return true
     }
 
     fun handleEquals(){
-        val res: String = calculator.calculate(_numAndSignArray).toString()
+        val res: String = calculator.calculate(numAndSignArray).toString()
 
-        _numAndSignArray.clear()
-        _numAndSignArray.add(res)
+        numAndSignArray.clear()
+        numAndSignArray.add(res)
     }
 }
