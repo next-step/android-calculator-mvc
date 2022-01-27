03e75db912b97e0952d88559b7f0376215b94eaa
luvimperfection <luvimperfection@gmail.com>
2022-01-28 08:29:09 +0900

    STEP4: Add ExpressionUtils

--- app/src/main/java/edu/nextstep/camp/calculator/MainViewModel.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainViewModel.kt
@@ -3,60 +3,52 @@ package edu.nextstep.camp.calculator
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.example.domain.Calculator
-import com.example.domain.ExpressionElement
-import com.example.domain.Operand
+import com.example.domain.ExpressionUtils
 import com.example.domain.Operation
 import com.jakewharton.rxrelay3.PublishRelay
 import com.jakewharton.rxrelay3.Relay
 
 class MainViewModel() : ViewModel() {
-    val expression = ObservableField<String>("")
+    private val expressionUtils = ExpressionUtils()
+    val expressionObservable = ObservableField<String>("")
     private val calculator = Calculator()
-    private val operatorCharList = listOf<Char>('+', '-', '×', '÷')
 
     val showToast: Relay<Boolean> = PublishRelay.create()
 
-    fun appendExpressionElement(element: ExpressionElement, putSpace: Boolean = true) {
-        val updatedExpression = expression.get() + (if (putSpace) " " else "") + when (element) {
-            is Operation.Add -> "+"
-            is Operation.Subtract -> "-"
-            is Operation.Multiply -> "×"
-            is Operation.Divide -> "÷"
-            is Operand -> element.value
-        }
-        expression.set(updatedExpression)
-        expression.notifyChange()
-    }
-
     fun onClickOperationBtn(operation: Operation) {
-        if (expression.get()?.isEmpty() == false) appendExpressionElement(operation)
+        expressionUtils.appendOperator(getSymbol(operation))
+        expressionObservable.set(expressionUtils.getExpression())
     }
 
     fun onClickNumberBtn(num: Int) {
-        when {
-            operatorCharList.contains(expression.get()?.lastOrNull()) -> appendExpressionElement(Operand(num))
-            expression.get()?.isEmpty() == true -> appendExpressionElement(Operand(num), putSpace = false)
-            expression.get()?.lastOrNull()?.isDigit() == true -> appendExpressionElement(Operand(num), putSpace = false)
-        }
+        expressionUtils.appendNumber(num.toString())
+        expressionObservable.set(expressionUtils.getExpression())
     }
 
     fun onClickDeleteBtn() {
-        if (expression.get().isNullOrEmpty().not()) truncateExpression()
-    }
-
-    fun truncateExpression() {
-        val truncatedExpression = expression.get()?.dropLast(1)
-        if (truncatedExpression?.last()?.isWhitespace() == true) expression.set(truncatedExpression.dropLast(1))
-        else expression.set(truncatedExpression)
-        expression.notifyChange()
+        expressionUtils.removeLast()
+        expressionObservable.set(expressionUtils.getExpression())
     }
 
     fun onClickEqualBtn() {
-        if (expression.get()?.last()?.isDigit()?.not() == true) {
-            showToast.accept(true)
-        } else {
-            val result = calculator.evaluate(expression.get())
-            expression.set(result.toString())
+        if (expressionUtils.endsWithOperator()) showToast.accept(true)
+        else {
+            val result = calculator.evaluate(expressionUtils.getExpression())
+            expressionUtils.replaceToResult(result.toString())
+            expressionObservable.set(result.toString())
         }
+    }
+}
+
+enum class Operator(val symbol: String) {
+    Add("+"), Subtract("-"), Multiply("×"), Divide("÷")
+}
+
+fun getSymbol(operator: Operation): String {
+    return when (operator) {
+        Operation.Add -> Operator.Add.symbol
+        Operation.Divide -> Operator.Divide.symbol
+        Operation.Multiply -> Operator.Multiply.symbol
+        Operation.Subtract -> Operator.Subtract.symbol
     }
 }
---
+++ domain/src/main/java/com/example/domain/ExpressionUtils.kt
@@ -0,0 +1,63 @@
+package com.example.domain
+
+class ExpressionUtils {
+    private val componentList = mutableListOf<String>()
+    private val operatorList = listOf("+", "-", "×", "÷")
+
+    fun addComponent(element: String) = componentList.add(element)
+    fun removeLastComponent() = componentList.removeLast()
+
+    fun appendNumber(number: String) {
+        when {
+            isEmpty() -> addComponent(number)
+            endsWithOperator() -> addComponent(number)
+            endsWithOperand() -> updateLastOperand(number)
+        }
+    }
+
+    fun appendOperator(operator: String) {
+        when {
+            isEmpty() -> {}
+            endsWithOperator() -> replaceLastElement(operator)
+            endsWithOperand() -> addComponent(operator)
+        }
+    }
+
+    fun isEmpty() = componentList.isEmpty()
+
+    fun getLast() = componentList.last()
+
+    fun endsWithOperator() = operatorList.contains(getLast())
+
+    fun endsWithOperand() = (getLast().toIntOrNull() != null)
+
+    private fun updateLastOperand(partial: String) {
+        val lastOperand = componentList.removeLast()
+        val updatedLastOperand = lastOperand + partial
+        addComponent(updatedLastOperand)
+    }
+
+    private fun replaceLastElement(lastToBe: String) {
+        componentList.removeLast()
+        addComponent(lastToBe)
+    }
+
+    fun replaceToResult(result: String) {
+        componentList.clear()
+        componentList.add(result)
+    }
+
+    fun removeLast() {
+        when {
+            isEmpty() -> {}
+            endsWithOperator() -> removeLastComponent()
+            endsWithOperand() -> replaceLastElement(getLast().dropLast(1))
+        }
+    }
+
+    fun getExpression(delimiter: String = " "): String {
+        val expression = componentList.joinToString(delimiter)
+        if (expression.lastOrNull()?.isWhitespace() == true) return expression.trimEnd()
+        return expression
+    }
+}
