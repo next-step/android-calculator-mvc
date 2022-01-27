3943b3178a813e90413ee9a4e3ef02caa41fee48
chohohee <gone1005@gmail.com>
2022-01-28 00:29:28 +0900

    refactor: 주석 제거 및 테스트 함수명 수정

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -13,11 +13,8 @@ class MainActivityTest {
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-    /**
-     * 1. 입력된 피연산자가 없을 때 숫자를 누른 경우
-     */
     @Test
-    fun test0() {
+    fun whenClick0_thenDisplay0() {
         // when: 사용자가 피연산자 0 버튼을 누르면
         onView(withId(R.id.button0)).perform(click())
 
@@ -26,7 +23,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test1() {
+    fun whenClick1_thenDisplay1() {
         // when: 사용자가 피연산자 1 버튼을 누르면
         onView(withId(R.id.button1)).perform(click())
 
@@ -35,7 +32,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test2() {
+    fun whenClick2_thenDisplay2() {
         // when: 사용자가 피연산자 2 버튼을 누르면
         onView(withId(R.id.button2)).perform(click())
 
@@ -44,7 +41,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test3() {
+    fun whenClick3_thenDisplay3() {
         // when: 사용자가 피연산자 3 버튼을 누르면
         onView(withId(R.id.button3)).perform(click())
 
@@ -53,7 +50,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test4() {
+    fun whenClick4_thenDisplay4() {
         // when: 사용자가 피연산자 4 버튼을 누르면
         onView(withId(R.id.button4)).perform(click())
 
@@ -62,7 +59,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test5() {
+    fun whenClick5_thenDisplay5() {
         // when: 사용자가 피연산자 5 버튼을 누르면
         onView(withId(R.id.button5)).perform(click())
 
@@ -71,7 +68,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test6() {
+    fun whenClick6_thenDisplay6() {
         // when: 사용자가 피연산자 6 버튼을 누르면
         onView(withId(R.id.button6)).perform(click())
 
@@ -80,7 +77,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test7() {
+    fun whenClick7_thenDisplay7() {
         // when: 사용자가 피연산자 7 버튼을 누르면
         onView(withId(R.id.button7)).perform(click())
 
@@ -89,7 +86,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test8() {
+    fun whenClick8_thenDisplay8() {
         // when: 사용자가 피연산자 8 버튼을 누르면
         onView(withId(R.id.button8)).perform(click())
 
@@ -98,7 +95,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test9() {
+    fun whenClick9_thenDisplay9() {
         // when: 사용자가 피연산자 9 버튼을 누르면
         onView(withId(R.id.button9)).perform(click())
 
@@ -106,12 +103,8 @@ class MainActivityTest {
         onView(withId(R.id.textView)).check(matches(withText("9")))
     }
 
-    /**
-     * 2. 입력된 피연산자가 있을 때 숫자를 누른 경우
-     */
-
     @Test
-    fun test10() {
+    fun givenDisplay8_whenClick9_thenDisplay89() {
         // given: 입력된 피연산자가 8일 때
         onView(withId(R.id.button8)).perform(click())
 
@@ -123,23 +116,19 @@ class MainActivityTest {
     }
 
     @Test
-    fun test11() {
+    fun givenDisplay5_whenClick4_thenDisplay54() {
         // given: 입력된 피연산자가 5일 때
         onView(withId(R.id.button5)).perform(click())
 
         // when: 사용자가 피연산자 4 버튼을 누르면
         onView(withId(R.id.button4)).perform(click())
 
-        // then: 계산기 텍스트에 89가 화면에 보여야 한다.
+        // then: 계산기 텍스트에 54가 화면에 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("54")))
     }
 
-    /**
-     * 3. 입력된 피연산자가 없을 때 연산자 버튼을 누른 경우
-     */
-
     @Test
-    fun test12() {
+    fun whenClickDivide_thenDisplayEmpty() {
         // when: 사용자가 연산자 나누기 버튼을 누르면
         onView(withId(R.id.buttonDivide)).perform(click())
 
@@ -148,7 +137,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test13() {
+    fun whenClickMultiply_thenDisplayEmpty() {
         // when: 사용자가 연산자 곱하기 버튼을 누르면
         onView(withId(R.id.buttonMultiply)).perform(click())
 
@@ -157,7 +146,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test14() {
+    fun whenClickPlus_thenDisplayEmpty() {
         // when: 사용자가 연산자 더하기 버튼을 누르면
         onView(withId(R.id.buttonPlus)).perform(click())
 
@@ -166,7 +155,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test15() {
+    fun whenClickMinus_thenDisplayEmpty() {
         // when: 사용자가 연산자 빼기 버튼을 누르면
         onView(withId(R.id.buttonMinus)).perform(click())
 
@@ -174,12 +163,8 @@ class MainActivityTest {
         onView(withId(R.id.textView)).check(matches(withText("")))
     }
 
-    /**
-     * 4. 입력된 피연산자가 있을 때 연산자 버튼을 누른 경우
-     */
-
     @Test
-    fun test16() {
+    fun givenDisplay4_whenClickMultiply_thenDisplayExpression() {
         // given: 입력된 피연산자가 4일 때
         onView(withId(R.id.button4)).perform(click())
 
@@ -191,7 +176,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test17() {
+    fun givenDisplay8_whenClickMinus_thenDisplayExpression() {
         // given: 입력된 피연산자가 8일 때
         onView(withId(R.id.button8)).perform(click())
 
@@ -203,7 +188,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test18() {
+    fun givenDisplayExpression_whenClickDivide_thenDisplayExpression() {
         // given: 입력된 피연산자가 3 + 일 때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
@@ -215,12 +200,8 @@ class MainActivityTest {
         onView(withId(R.id.textView)).check(matches(withText("3 ÷")))
     }
 
-    /**
-     * 5. 입력된 수식이 없을 때 지우기 버튼을 누른 경우
-     */
-
     @Test
-    fun test19() {
+    fun whenClickDelete_thenDisplayEmpty() {
         // when: 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
 
@@ -228,12 +209,8 @@ class MainActivityTest {
         onView(withId(R.id.textView)).check(matches(withText("")))
     }
 
-    /**
-     * 6. 입력된 수식이 있을 때 지우기 버튼을 누른 경우
-     */
-
     @Test
-    fun test20() {
+    fun givenDisplayExpression_whenClickDelete_thenDisplayExpression() {
         // given: 입력된 수식이 32 + 1 일 때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.button2)).perform(click())
@@ -248,7 +225,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test21() {
+    fun givenDisplayExpression_whenClickDelete_thenDisplay32() {
         // given: 입력된 수식이 32 + 일 때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.button2)).perform(click())
@@ -262,7 +239,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test22() {
+    fun givenDisplay32_whenClickDelete_thenDisplay3() {
         // given: 입력된 피연산자가 32 일 때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.button2)).perform(click())
@@ -274,12 +251,8 @@ class MainActivityTest {
         onView(withId(R.id.textView)).check(matches(withText("3")))
     }
 
-    /**
-     * 7. 입력된 수식이 완전할 때 = 버튼을 누른 경우
-     */
-
     @Test
-    fun test23() {
+    fun givenDisplayExpression_whenClickEquals_thenDisplayResult() {
         // given: 입력된 수식이 3 + 2 + 1 * 5일 때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
@@ -297,7 +270,7 @@ class MainActivityTest {
     }
 
     @Test
-    fun test24() {
+    fun givenDisplayExpression_whenClickEquals_thenDisplayResult2() {
         // given: 입력된 수식이 4 + 2 + 1 * 5일 때
         onView(withId(R.id.button4)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
@@ -313,10 +286,4 @@ class MainActivityTest {
         // then: 계산기 텍스트에 30.0 으로 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("35.0")))
     }
-
-    /**
-     * 8. 입력된 수식이 완전하지 않을 때 = 버튼을 누른 경우
-     *
-     * 생략
-     */
 }
