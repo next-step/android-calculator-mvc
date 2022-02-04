5c75f7281db50bbf7475171ce6c111aca0975fbc
simjunbo <simuelunbo@gmail.com>
2022-02-05 05:29:12 +0900

    refact: Operator 코드 수정

--- domain/src/main/java/com/example/domain/Operator.kt
+++ domain/src/main/java/com/example/domain/Operator.kt
@@ -1,42 +1,40 @@
 package com.example.domain
 
-sealed class Operator(val symbol: Char) {
-    object Plus : Operator('+') {
-        override fun operate(first: Int, second: Int): Int = first + second
-    }
+import com.example.domain.Operator.Divide.operate
 
-    object Minus : Operator('-') {
-        override fun operate(first: Int, second: Int): Int = first - second
-    }
 
-    object Multiply : Operator('*') {
-        override fun operate(first: Int, second: Int): Int = first * second
-    }
+sealed class Operator(val symbol: Char, val calculateStrategy: (Int, Int) -> Int) {
+    object Plus : Operator('+', { left: Int, right: Int -> left + right })
 
-    object Divide : Operator('/') {
-        override fun operate(first: Int, second: Int): Int {
-            if (first == 0) throw IllegalArgumentException(CANT_DIVIDE)
-            return first / second
+    object Minus : Operator('-', { left: Int, right: Int -> left - right })
+
+    object Multiply : Operator('*', { left: Int, right: Int -> left * right })
+
+    object Divide : Operator('/', { left: Int, right: Int -> operate(left, right) }) {
+        fun operate(left: Int, right: Int): Int{
+            if (left == 0) throw IllegalArgumentException(CANT_DIVIDE)
+            return left / right
         }
     }
 
-    abstract fun operate(first: Int, second: Int): Int
+
 
     companion object {
         const val CANT_DIVIDE = "0을 나눌수가 없습니다"
         const val CANT_EXIST_OPERATOR = "존재 하지 않는 연산자 입니다"
 
 
-        fun findOperator(operator: Char) =
-            Operator::class.sealedSubclasses
-                .map { it.objectInstance as Operator }
-                .firstOrNull { it.symbol == operator }
-                .let { symbol ->
-                    when (symbol) {
-                        null -> throw IllegalArgumentException(CANT_EXIST_OPERATOR)
-                        else -> symbol
-                    }
+        fun find(symbol: Char): Operator {
+            return when (symbol) {
+                Plus.symbol -> Plus
+                Minus.symbol -> Minus
+                Multiply.symbol -> Multiply
+                Divide.symbol -> Divide
+                else -> {
+                    throw IllegalArgumentException(CANT_EXIST_OPERATOR)
                 }
+            }
+        }
     }
 
 }
