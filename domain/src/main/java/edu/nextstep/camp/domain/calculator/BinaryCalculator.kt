3f24ce4bbf7df8c43ae567470315b231cb34ce9a
jennkimm <seohkim@student.42seoul.kr>
2022-02-09 02:35:44 +0900

    feat: Operator enum 클래스 수정

--- domain/src/main/java/edu/nextstep/camp/domain/calculator/BinaryCalculator.kt
+++ domain/src/main/java/edu/nextstep/camp/domain/calculator/BinaryCalculator.kt
@@ -4,8 +4,21 @@ import java.lang.IllegalArgumentException
 
 typealias CalculationStrategy = (Int, Int) -> Int
 
-enum class Operator {
-    PLUS, MINUS, MULTIPLY, DIVIDE
+enum class Formula(val operator: String, val formula: CalculationStrategy) {
+    PLUS("+", { x, y -> x + y }),
+    MINUS("-", { x, y -> x - y }),
+    MULTIPLY("×", { x, y -> x * y }),
+    DIVIDE("÷", { x, y -> x / y });
+
+    companion object {
+        fun matchFormula(token: String): CalculationStrategy = when(token) {
+            PLUS.operator -> PLUS.formula
+            MINUS.operator -> MINUS.formula
+            MULTIPLY.operator -> MULTIPLY.formula
+            DIVIDE.operator -> DIVIDE.formula
+            else -> throw IllegalArgumentException("사칙연산 기호가 아닙니다")
+        }
+    }
 }
 
 internal class BinaryCalculator private constructor(private val calculationStrategy: CalculationStrategy) {
@@ -13,21 +26,6 @@ internal class BinaryCalculator private constructor(private val calculationStrat
     fun calculate(operator1: Int, operator2: Int) = calculationStrategy(operator1, operator2)
 
     companion object {
-        fun searchOperator(token: String) = BinaryCalculator(setCalculationStrategy(matchOperator(token)))
-
-        fun setCalculationStrategy(token: Operator): CalculationStrategy = when (token) {
-            Operator.PLUS -> { x, y -> x + y }
-            Operator.MINUS -> { x, y -> x - y }
-            Operator.MULTIPLY -> { x, y -> x * y }
-            Operator.DIVIDE -> { x, y -> x / y }
-        }
-
-        private fun matchOperator(token: String): Operator = when(token) {
-            "+" -> Operator.PLUS
-            "-" -> Operator.MINUS
-            "×" -> Operator.MULTIPLY
-            "÷" -> Operator.DIVIDE
-            else -> throw IllegalArgumentException("사칙연산 기호가 아닙니다")
-        }
+        fun searchOperator(token: String) = BinaryCalculator(Formula.matchFormula(token))
     }
 }
