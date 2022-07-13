51ad576c84ab2bca0d4a16b5d64f6194535560fa
Keich.Park <svclaw2000@gmail.com>
2022-07-13 21:17:34 +0900

    feat: evaluate 메인 함수를 분리한다

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -3,15 +3,23 @@ package edu.nextstep.camp.calculator.domain
 class Calculator {
 
     fun evaluate(expression: String?): Int {
-        if (expression.isNullOrBlank()) {
+        if (expression.isNullOrBlank())
             throw IllegalArgumentException("null 또는 빈 공백 문자열은 수식이 아닙니다.")
-        }
-
-        if (""".*[^0-9+\-*/ ].*""".toRegex().matches(expression)) {
+        if (containsInvalidSymbol(expression))
             throw IllegalArgumentException("수식에는 +, -, *, /만 사용될 수 있습니다.")
-        }
 
-        val symbols = expression.split(' ')
+        return evaluate(splitToSymbols(expression))
+    }
+
+    private fun containsInvalidSymbol(expression: String): Boolean {
+        return expression.matches(""".*[^0-9+\-*/ ].*""".toRegex())
+    }
+
+    private fun splitToSymbols(expression: String): List<String> {
+        return expression.split(' ')
+    }
+
+    private fun evaluate(symbols: List<String>): Int {
         var result = 0
         var sign: String? = null
 
