27270d01917c03924202cad0a27cd242c7bcb2ff
hyowan park <woga1999@gmail.com>
2022-07-14 00:38:39 +0900

    feat: 버튼 0~9 버튼을 누르면 화면에 0~9가 보이도록 구현 (#135)
    Co-authored-by: hyowan <hyowan.park@myrealtrip.com>

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -11,5 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button0.setOnClickListener { binding.tvResult.text = "0" }
+        binding.button1.setOnClickListener { binding.tvResult.text = "1" }
+        binding.button2.setOnClickListener { binding.tvResult.text = "2" }
+        binding.button3.setOnClickListener { binding.tvResult.text = "3" }
+        binding.button4.setOnClickListener { binding.tvResult.text = "4" }
+        binding.button5.setOnClickListener { binding.tvResult.text = "5" }
+        binding.button6.setOnClickListener { binding.tvResult.text = "6" }
+        binding.button7.setOnClickListener { binding.tvResult.text = "7" }
+        binding.button8.setOnClickListener { binding.tvResult.text = "8" }
+        binding.button9.setOnClickListener { binding.tvResult.text = "9" }
     }
 }
