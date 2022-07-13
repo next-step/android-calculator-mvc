9b820b40d56778fe845ce80644eb06beb177df2e
Keich.Park <svclaw2000@gmail.com>
2022-07-13 21:55:41 +0900

    feat: 수식의 내용을 일급 컬렉션으로 분리한다

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -5,40 +5,52 @@ class Calculator {
     fun evaluate(expression: String?): Int {
         if (expression.isNullOrBlank())
             throw IllegalArgumentException("null 또는 빈 공백 문자열은 수식이 아닙니다.")
-        if (containsInvalidSymbol(expression))
-            throw IllegalArgumentException("수식에는 +, -, *, /만 사용될 수 있습니다.")
 
         return evaluateSymbols(splitToSymbols(expression))
     }
 
-    private fun containsInvalidSymbol(expression: String): Boolean {
-        return expression.matches(""".*[^0-9+\-*/ ].*""".toRegex())
-    }
-
-    private fun splitToSymbols(expression: String): List<String> {
+    private fun splitToSymbols(expression: String): List<Symbol> {
         return expression.split(' ')
-    }
-
-    private fun evaluateSymbols(symbols: List<String>): Int {
-        var sign: String? = null
-
-        return symbols.fold(0) { prevResult, symbol ->
-            if ("+-*/".contains(symbol)) {
-                sign = symbol
-                return@fold prevResult
+            .map { symbol ->
+                when (symbol) {
+                    "+" -> Symbol.Sign.PLUS
+                    "-" -> Symbol.Sign.MINUS
+                    "*" -> Symbol.Sign.TIMES
+                    "/" -> Symbol.Sign.DIVISION
+                    else -> symbol.toIntOrNull()?.let {
+                        Symbol.Number(it)
+                    } ?: throw IllegalArgumentException("수식에는 +, -, *, /만 사용될 수 있습니다.")
+                }
             }
-
-            evaluateOne(prevResult, sign, symbol.toInt())
-        }
     }
 
-    private fun evaluateOne(prevResult: Int, sign: String?, number: Int): Int {
+    private fun evaluateSymbols(symbols: List<Symbol>): Int {
+        var sign: Symbol.Sign? = null
+
+        return symbols.fold(Symbol.Number(0)) { prevResult, symbol ->
+            when (symbol) {
+                is Symbol.Sign -> {
+                    sign = symbol
+                    prevResult
+                }
+                is Symbol.Number -> {
+                    evaluateOne(prevResult, sign, symbol)
+                }
+            }
+        }.n
+    }
+
+    private fun evaluateOne(
+        prevResult: Symbol.Number,
+        sign: Symbol.Sign?,
+        number: Symbol.Number,
+    ): Symbol.Number {
         return when (sign) {
-            "+" -> prevResult + number
-            "-" -> prevResult - number
-            "*" -> prevResult * number
-            "/" -> prevResult / number
-            else -> number
+            Symbol.Sign.PLUS -> prevResult + number
+            Symbol.Sign.MINUS -> prevResult - number
+            Symbol.Sign.TIMES -> prevResult * number
+            Symbol.Sign.DIVISION -> prevResult / number
+            null -> number
         }
     }
 }
