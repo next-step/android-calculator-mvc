d2e5d5937f7f9829f218092891740fe54eee248b
link-js <link.js@kakaocorp.com>
2022-07-13 23:56:35 +0900

    feat : Calculator 도메인 클래스 완성

--- app/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
@@ -1,5 +1,6 @@
 package edu.nextstep.camp.calculator.domain
 
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
 
@@ -10,22 +11,26 @@ class CalculatorTest {
 
     @Test
     fun `덧셈이 잘 동작한다`() {
-        TODO("Not yet implemented")
+        val calculator = Calculator()
+        assertEquals(calculator.evaluate("2 + 3"), 5)
     }
 
     @Test
     fun `뺄셈이 잘 동작한다`() {
-        TODO("Not yet implemented")
+        val calculator = Calculator()
+        assertEquals(calculator.evaluate("4 - 3"), 1)
     }
 
     @Test
     fun `곱셈이 잘 동작한다`() {
-        TODO("Not yet implemented")
+        val calculator = Calculator()
+        assertEquals(calculator.evaluate("2 * 3"), 6)
     }
 
     @Test
     fun `나눗셈이 잘 동작한다`() {
-        TODO("Not yet implemented")
+        val calculator = Calculator()
+        assertEquals(calculator.evaluate("4 / 2"), 2)
     }
 
     @Test
@@ -44,11 +49,30 @@ class CalculatorTest {
 
     @Test
     fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw`() {
-        TODO("Not yet implemented")
+        assertThrows(IllegalArgumentException::class.java) {
+            Calculator().evaluate("2 [ 3")
+        }
     }
 
     @Test
     fun `숫자 위치에 숫자가 아닌 경우 IllegalArgumentException throw`() {
-        TODO("Not yet implemented")
+        assertThrows(IllegalArgumentException::class.java) {
+            Calculator().evaluate("2 3 3")
+        }
+    }
+
+    @Test
+    fun `Operator는 연산자를 잘 찾아내는가`() {
+        assertEquals(Operator.of("+"), Operator.PLUS)
+        assertEquals(Operator.of("-"), Operator.MINUS)
+        assertEquals(Operator.of("/"), Operator.DIVISION)
+        assertEquals(Operator.of("*"), Operator.MULTIPLY)
+    }
+
+    @Test
+    fun `0으로 나눌 경우 IllegalArgumentException throw`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Calculator().evaluate("2 / 0")
+        }
     }
 }
--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -6,10 +6,50 @@ class Calculator {
             throw IllegalArgumentException(IS_NULL_OR_BLANK)
         }
 
-        return 0
+        val inputList = splitByDelimiter(input)
+        if (inputList.size % EVEN_COMPARISON_NUMBER == RESULT_WHEN_EVEN_NUMBER) {
+            throw IllegalArgumentException(NOT_MATCH_OPERATORS_AND_OPERANDS)
+        }
+
+        // 첫숫자는 바로 계산하기 위해 저장한다.
+        var output = changeStringToInt(inputList.first())
+        for (index in NUMBER_OF_EXCLUDING_THE_FIRST_INDEX until inputList.size step SIZE_OF_CALCULATION_UNIT) {
+            output =
+                Operator.of(inputList[index])
+                    .calculate(output, changeStringToInt(inputList[index + INDEX_OF_NUMBER]))
+        }
+
+        return output
+    }
+
+    /**
+     * 문자열을 스페이스로 나눠 List로 돌려주는 메소드.
+     */
+    private fun splitByDelimiter(input: String): List<String> {
+        return input.split(DELIMITER)
+    }
+
+    /**
+     * 계산가능한 숫자인지 확인하는 메소드.
+     */
+    private fun changeStringToInt(input: String): Int {
+        return input.toIntOrNull()
+            ?: throw IllegalArgumentException(IS_NON_DIGIT_CHARACTER)
     }
 
     companion object {
-        private const val IS_NULL_OR_BLANK = "Input is Null or Blank."
+        private const val DELIMITER = " "
+
+        private const val EVEN_COMPARISON_NUMBER = 2
+        private const val RESULT_WHEN_EVEN_NUMBER = 0
+
+        private const val NUMBER_OF_EXCLUDING_THE_FIRST_INDEX = 1
+        private const val SIZE_OF_CALCULATION_UNIT = 2
+
+        private const val INDEX_OF_NUMBER = 1
+
+        private const val IS_NULL_OR_BLANK = "인풋이 null이거나 blank입니다."
+        private const val NOT_MATCH_OPERATORS_AND_OPERANDS = "연산자와 피연산자 갯수가 맞지 않습니다."
+        private const val IS_NON_DIGIT_CHARACTER = "숫자 위치에 숫자가 오지 않았습니다."
     }
 }
