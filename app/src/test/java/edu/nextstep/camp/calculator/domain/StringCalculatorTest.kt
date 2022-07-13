b7441366ec25e5fcdddb034e795d713ce96530b6
Suhyeon Kim <wisemuji@gmail.com>
2022-07-13 21:27:33 +0900

    test: 덧셈 테스트 ParameterizedTest로 보강

--- app/src/test/java/edu/nextstep/camp/calculator/domain/StringCalculatorTest.kt
+++ app/src/test/java/edu/nextstep/camp/calculator/domain/StringCalculatorTest.kt
@@ -1,18 +1,25 @@
 package edu.nextstep.camp.calculator.domain
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class StringCalculatorTest {
 
     private val stringCalculator = StringCalculator
 
-    @Test
-    internal fun `덧셈 연산자가 포함된 문자열의 계산 결과값을 알 수 있다`() {
+    @ParameterizedTest(name = "{0}의 계산 결과가 {1}를 만족한다.")
+    @CsvSource(
+        "1 + 2, 3",
+        "11 + 12, 23",
+        "5 + 9 + 10, 24",
+        "1000 + 4, 1004",
+    )
+    fun `덧셈 연산자가 포함된 문자열의 계산 결과값을 알 수 있다`(original: String, expected: Int) {
         // when
-        val result = stringCalculator.calculate("1 + 2")
+        val result = stringCalculator.calculate(original)
 
         // then
-        assertThat(result).isEqualTo(Number(3))
+        assertThat(result).isEqualTo(Number(expected))
     }
 }
