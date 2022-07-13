cefd685aee0fd8d05e13413a48616ed2006ddb6d
Keich.Park <svclaw2000@gmail.com>
2022-07-13 21:04:07 +0900

    feat: 사칙 연산을 슌서대로 수행한다

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -7,10 +7,30 @@ class Calculator {
             throw IllegalArgumentException("null 또는 빈 공백 문자열은 수식이 아닙니다.")
         }
 
-        if (""".*[0-9+\-*/ ].*""".toRegex().matches(expression)) {
+        if (""".*[^0-9+\-*/ ].*""".toRegex().matches(expression)) {
             throw IllegalArgumentException("수식에는 +, -, *, /만 사용될 수 있습니다.")
         }
-        
-        TODO()
+
+        val symbols = expression.split(' ')
+        var result = 0
+        var sign: String? = null
+
+        symbols.forEach { symbol ->
+            when (symbol) {
+                "+", "-", "*", "/" -> sign = symbol
+                else -> {
+                    val number = symbol.toInt()
+                    when (sign) {
+                        "+" -> result += number
+                        "-" -> result -= number
+                        "*" -> result *= number
+                        "/" -> result /= number
+                        null -> result = number
+                    }
+                }
+            }
+        }
+
+        return result
     }
 }
