a6fd8b83714b5dde55e38dfbc653dd09dcf068db
link-js <link.js@kakaocorp.com>
2022-07-13 23:28:25 +0900

    feat : TDD를 위한 테스트 먼저 제작

---
+++ app/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
@@ -0,0 +1,44 @@
+package edu.nextstep.camp.calculator.domain
+
+import org.junit.Test
+
+/**
+ * Created by link.js on 2022. 07. 13..
+ */
+class CalculatorTest {
+
+    @Test
+    fun `덧셈이 잘 동작한다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `뺄셈이 잘 동작한다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `곱셈이 잘 동작한다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `나눗셈이 잘 동작한다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException throw`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `숫자 위치에 숫자가 아닌 경우 IllegalArgumentException throw`() {
+        TODO("Not yet implemented")
+    }
+}
