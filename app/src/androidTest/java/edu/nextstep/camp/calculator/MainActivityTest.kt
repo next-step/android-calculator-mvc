f65675555c523fe333dcec40a29886104e9c0d8d
Sooleeandtomas <62638990+Lee-ji-soo@users.noreply.github.com>
2022-07-13 23:10:31 +0900

    Step 2 domain 모듈 분리 (#133)
    * create domain module
    
    * remove unused dependency
    
    * remove unused import

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -3,11 +3,9 @@ package edu.nextstep.camp.calculator
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
