abfaea06924c9d354118d32ac771a63ecfb31f29
Suhyeon Kim <wisemuji@gmail.com>
2022-07-13 23:33:05 +0900

    feat: 입력된 피연산자가 없을 때, 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자가 화면에 보여야 한다

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.junit.jupiter.params.provider.ValueSource
 
 class MainActivityTest {
@@ -24,6 +25,27 @@ class MainActivityTest {
 
         // then
         onView(withId(R.id.textView)).check(matches(withText(text)))
+    }
+
+    @ParameterizedTest(name = "{0}에서 피연산자 {1}을 누르면 {2}가 보여야 한다.")
+    @CsvSource(
+        "'', 1, 1",
+        "'5 + ', 1, 5 + 1",
+    )
+    fun 입력된_피연산자가_없을_때_사용자가_피연산자_버튼을_누르면_화면에_해당_숫자가_보여야_한다(
+        original: String,
+        pressed: String,
+        expected: String,
+    ) {
+        // given
+        val textView = withId(R.id.textView)
+        onView(textView).perform(setTextInTextView(original))
+
+        // when
+        onView(withText(pressed)).perform(click())
+
+        // then
+        onView(textView).check(matches(withText(expected)))
     }
 
 }
--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,46 +1,36 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private val displayedText: String
+        get() = binding.textView.text.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initViewBinding()
+        initNumberButtons()
+    }
+
+    private fun initNumberButtons() {
+        (0..9).forEach { number ->
+            val viewId = resources.getIdentifier("button$number", "id", packageName)
+            val button: View = findViewById(viewId)
+            button.setOnClickListener {
+                val text = displayedText + number
+                binding.textView.text = text
+            }
+        }
+    }
+
+    private fun initViewBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.button0.setOnClickListener {
-            binding.textView.text = "0"
-        }
-        binding.button1.setOnClickListener {
-            binding.textView.text = "1"
-        }
-        binding.button2.setOnClickListener {
-            binding.textView.text = "2"
-        }
-        binding.button3.setOnClickListener {
-            binding.textView.text = "3"
-        }
-        binding.button4.setOnClickListener {
-            binding.textView.text = "4"
-        }
-        binding.button5.setOnClickListener {
-            binding.textView.text = "5"
-        }
-        binding.button6.setOnClickListener {
-            binding.textView.text = "6"
-        }
-        binding.button7.setOnClickListener {
-            binding.textView.text = "7"
-        }
-        binding.button8.setOnClickListener {
-            binding.textView.text = "8"
-        }
-        binding.button9.setOnClickListener {
-            binding.textView.text = "9"
-        }
     }
 }
