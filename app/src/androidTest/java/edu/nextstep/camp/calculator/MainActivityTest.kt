2f796d835712fecc9ede64ef617ca0e3e0efe584
1stpie <1stpie@naver.com>
2022-07-14 03:16:10 +0900

    [UI테스트][2단계] 모듈 분리 - 피드백 반영

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -13,41 +13,37 @@ import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 
 @RunWith(Parameterized::class)
-class MainActivityTest(input: Pair<Int, String>) {
-    private var buttonId: Int
-    private var experted: String
-
+class MainActivityTest(
+    private var buttonId: Int,
+    private var expected: String
+) {
     @get:Rule
     var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-    init {
-        this.buttonId = input.first
-        this.experted = input.second
-    }
-
     companion object {
-        @Parameters
         @JvmStatic
-        fun getTestParameters() = arrayOf(
-            R.id.button0 to "0",
-            R.id.button1 to "1",
-            R.id.button2 to "2",
-            R.id.button3 to "3",
-            R.id.button4 to "4",
-            R.id.button5 to "5",
-            R.id.button6 to "6",
-            R.id.button7 to "7",
-            R.id.button8 to "8",
-            R.id.button9 to "9"
+        @Parameters(name = "버튼id : {0}, text : {1}")
+        fun getTestParameters() = listOf(
+            arrayOf(R.id.button0, "0"),
+            arrayOf(R.id.button1, "1"),
+            arrayOf(R.id.button2, "2"),
+            arrayOf(R.id.button3, "3"),
+            arrayOf(R.id.button4, "4"),
+            arrayOf(R.id.button5, "5"),
+            arrayOf(R.id.button6, "6"),
+            arrayOf(R.id.button7, "7"),
+            arrayOf(R.id.button8, "8"),
+            arrayOf(R.id.button9, "9")
+
         )
     }
 
     @Test
-    fun `숫자_버튼을_누르면_화면에_숫자가_보여야_한다`() {
+    fun `버튼을_누르면_화면에_숫자가_보여야_한다`() {
         //when: 0 버튼을 누르면
         onView(withId(buttonId)).perform(ViewActions.click())
 
         //then: 화면에 0이 보여야 한다
-        onView(withId(R.id.textView)).check(matches(withText(experted)))
+        onView(withId(R.id.textView)).check(matches(withText(expected)))
     }
 }
