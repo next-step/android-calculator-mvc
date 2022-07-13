e34d3ae2d228ae91f1a86784348bfac6616ad755
Suhyeon Kim <wisemuji@gmail.com>
2022-07-13 23:35:51 +0900

    feat: 입력된 피연산자가 있을 때, 기존 숫자 뒤에 해당 숫자가 화면에 보여야 한다

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -9,7 +9,6 @@ import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
 
 class MainActivityTest {
 
@@ -17,22 +16,14 @@ class MainActivityTest {
     @RegisterExtension
     val scenarioExtension = ActivityScenarioExtension.launch<MainActivity>()
 
-    @ParameterizedTest
-    @ValueSource(strings = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
-    fun 사용자가_피연산자를_누르면_해당_숫자가_화면에_보여야_한다(text: String) {
-        // when
-        onView(withText(text)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText(text)))
-    }
-
     @ParameterizedTest(name = "{0}에서 피연산자 {1}을 누르면 {2}가 보여야 한다.")
     @CsvSource(
         "'', 1, 1",
         "'5 + ', 1, 5 + 1",
+        "8, 9, 89",
+        "123, 4, 1234",
     )
-    fun 입력된_피연산자가_없을_때_사용자가_피연산자_버튼을_누르면_화면에_해당_숫자가_보여야_한다(
+    fun 사용자가_피연산자_버튼을_누르면_화면에_해당_숫자가_화면에_보여야_한다(
         original: String,
         pressed: String,
         expected: String,
