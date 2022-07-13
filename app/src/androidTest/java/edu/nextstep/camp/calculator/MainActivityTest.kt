8122533dc4e1d540012d7f604fd0fd880bf3185c
Suhyeon Kim <wisemuji@gmail.com>
2022-07-13 11:40:19 +0900

    🚀 2단계 - 모듈 분리 (#116)
    * build: domain kotlin 모듈 추가
    
    * build: domain kotlin 모듈 의존성을 app 모듈에서 가져올 수 있도록 구현
    
    * build, feat: JUnit5를 사용할 수 있는 환경 세팅 및 테스트에 적용

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -5,103 +5,25 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Rule
-import org.junit.Test
+import de.mannodermaus.junit5.ActivityScenarioExtension
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class MainActivityTest {
 
-    @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    @JvmField
+    @RegisterExtension
+    val scenarioExtension = ActivityScenarioExtension.launch<MainActivity>()
 
-    @Test
-    fun 버튼_0을_누르면_화면에_0이_보여야_한다() {
+    @ParameterizedTest
+    @ValueSource(strings = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    fun 사용자가_피연산자를_누르면_해당_숫자가_화면에_보여야_한다(text: String) {
         // when
-        onView(withId(R.id.button0)).perform(click())
+        onView(withText(text)).perform(click())
 
         // then
-        onView(withId(R.id.textView)).check(matches(withText("0")))
-    }
-
-    @Test
-    fun 버튼_1을_누르면_화면에_1이_보여야_한다() {
-        // when
-        onView(withId(R.id.button1)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("1")))
-    }
-
-    @Test
-    fun 버튼_2를_누르면_화면에_2가_보여야_한다() {
-        // when
-        onView(withId(R.id.button2)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("2")))
-    }
-
-    @Test
-    fun 버튼_3을_누르면_화면에_3이_보여야_한다() {
-        // when
-        onView(withId(R.id.button3)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("3")))
-    }
-
-    @Test
-    fun 버튼_4를_누르면_화면에_4가_보여야_한다() {
-        // when
-        onView(withId(R.id.button4)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("4")))
-    }
-
-    @Test
-    fun 버튼_5를_누르면_화면에_5가_보여야_한다() {
-        // when
-        onView(withId(R.id.button5)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("5")))
-    }
-
-    @Test
-    fun 버튼_6을_누르면_화면에_6이_보여야_한다() {
-        // when
-        onView(withId(R.id.button6)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("6")))
-    }
-
-    @Test
-    fun 버튼_7을_누르면_화면에_7이_보여야_한다() {
-        // when
-        onView(withId(R.id.button7)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("7")))
-    }
-
-    @Test
-    fun 버튼_8을_누르면_화면에_8이_보여야_한다() {
-        // when
-        onView(withId(R.id.button8)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("8")))
-    }
-
-    @Test
-    fun 버튼_9를_누르면_화면에_9가_보여야_한다() {
-        // when
-        onView(withId(R.id.button9)).perform(click())
-
-        // then
-        onView(withId(R.id.textView)).check(matches(withText("9")))
+        onView(withId(R.id.textView)).check(matches(withText(text)))
     }
 
 }
