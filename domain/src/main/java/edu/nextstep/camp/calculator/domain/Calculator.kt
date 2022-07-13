3ab2a267ab4b31980583eb37dbdd82f248472c31
Keich.Park <svclaw2000@gmail.com>
2022-07-13 20:54:00 +0900

    feat: 입력값을 검증한다

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -3,6 +3,14 @@ package edu.nextstep.camp.calculator.domain
 class Calculator {
 
     fun evaluate(expression: String?): Int {
+        if (expression.isNullOrBlank()) {
+            throw IllegalArgumentException("null 또는 빈 공백 문자열은 수식이 아닙니다.")
+        }
+
+        if (""".*[0-9+\-*/ ].*""".toRegex().matches(expression)) {
+            throw IllegalArgumentException("수식에는 +, -, *, /만 사용될 수 있습니다.")
+        }
+        
         TODO()
     }
 }
