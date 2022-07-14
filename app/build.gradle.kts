a7c3bd88bcd805373b296204e435dab3695bb5a0
nerd.y <nerd.y@kakaocorp.com>
2022-07-15 01:56:00 +0900

    step2: 도메인 모듈을 분리한다.

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+import Versions.compileSdk
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = Versions.compileSdk
+
+    defaultConfig {
+        applicationId = "edu.nextstep.camp.calculator"
+        minSdk = Versions.minSdk
+        targetSdk = Versions.targetSdk
+        versionCode = Versions.versionCode
+        versionName = Versions.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+    testOptions {
+        animationsDisabled = true
+    }
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.bundles.androidx)
+
+    implementation(libs.google.material)
+    implementation(libs.androidx.constraintlayout)
+
+
+    testImplementation(libs.bundles.test)
+    androidTestImplementation(libs.bundles.test.android)
+
+    implementation(project(":caculator"))
+}
--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -22,6 +22,5 @@ class MainActivity : AppCompatActivity() {
         binding.button7.setOnClickListener { binding.textView.text = "7" }
         binding.button8.setOnClickListener { binding.textView.text = "8" }
         binding.button9.setOnClickListener { binding.textView.text = "9" }
-
     }
 }
