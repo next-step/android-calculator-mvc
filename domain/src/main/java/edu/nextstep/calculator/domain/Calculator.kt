cabeb2233f56faf51a1af992e093d84e4099dcb5
AnKyungMoo <sodp2@naver.com>
2022-07-14 16:09:46 +0900

    feat: 입력 기능 추가

--- domain/src/main/java/edu/nextstep/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/calculator/domain/Calculator.kt
@@ -1,6 +1,20 @@
 package edu.nextstep.calculator.domain
 
 class Calculator {
+    private val expressionContents = mutableListOf<String>()
+
+    fun input(expression: String?) {
+        require(!expression.isNullOrBlank()) {
+            "입력값이 null이거나 빈 공백 문자입니다."
+        }
+
+        expressionContents.addAll(splitExpression(expression))
+    }
+
+    private fun splitExpression(expression: String): List<String> {
+        return expression.split(" ")
+    }
+
     fun plus(first: Int, second: Int): Int = first + second
     fun minus(first: Int, second: Int): Int = first - second
     fun multiply(first: Int, second: Int): Int = first * second
