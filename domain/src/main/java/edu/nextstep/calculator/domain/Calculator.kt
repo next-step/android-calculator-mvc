fda5d61dea434ecea8405e9e43b2ba8f1bdce9b7
AnKyungMoo <sodp2@naver.com>
2022-07-14 15:26:59 +0900

    feat: 사칙연산 구현

--- domain/src/main/java/edu/nextstep/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/calculator/domain/Calculator.kt
@@ -2,4 +2,7 @@ package edu.nextstep.calculator.domain
 
 class Calculator {
     fun plus(first: Int, second: Int): Int = first + second
+    fun minus(first: Int, second: Int): Int = first - second
+    fun multiply(first: Int, second: Int): Int = first * second
+    fun divide(first: Int, second: Int): Int = first / second
 }
