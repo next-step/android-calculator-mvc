380aa5f150e6399527b22073f9e4aa56404a39fc
heungsoo <jaengsama@gmail.com>
2022-07-15 21:43:24 +0900

    step2: 도메인 모듈 분리  (#148)
    * feat: 버튼 0~9 버튼을 누르면 화면에 0~9가 보이도록 구현한다. (#120) (#1)
    
    - 버튼 0~9 버튼을 누르면 화면에 0~9가 보이도록 구현한다.
    
    * step2: 도메인 모듈을 분리한다.
    
    Co-authored-by: nerd.y <nerd.y@kakaocorp.com>

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.android.gradle)
+        classpath(libs.kotlin.plugin)
+
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean",Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+    const val compileSdk = 30
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
+
