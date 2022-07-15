2ae1cef4dabf254732f20231c46bb6487509bc0d
Keich.Park <svclaw2000@gmail.com>
2022-07-15 22:28:25 +0900

    test: 공백으로 구분된 올바른 수식을 파싱한다

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/ExpressionParser.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/ExpressionParser.kt
@@ -2,18 +2,7 @@ package edu.nextstep.camp.calculator.domain
 
 class ExpressionParser {
 
-    fun parse(expression: String): List<Symbol> {
-        return expression.split(' ')
-            .map { symbol ->
-                when (symbol) {
-                    "+" -> Symbol.Sign.PLUS
-                    "-" -> Symbol.Sign.MINUS
-                    "*" -> Symbol.Sign.TIMES
-                    "/" -> Symbol.Sign.DIVISION
-                    else -> symbol.toIntOrNull()?.let {
-                        Symbol.Number(it)
-                    } ?: throw IllegalArgumentException("수식에는 +, -, *, /만 사용될 수 있습니다.")
-                }
-            }
+    fun parse(expression: String): Expression {
+        TODO()
     }
 }
---
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/ExpressionParserTest.kt
@@ -0,0 +1,40 @@
+package edu.nextstep.camp.calculator.domain
+
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.*
+import org.junit.Before
+import org.junit.Test
+
+class ExpressionParserTest {
+
+    private lateinit var parser: ExpressionParser
+
+    @Before
+    fun setUp() {
+        parser = ExpressionParser()
+    }
+
+    @Test
+    fun `공백으로 구분된 올바른 수식을 파싱한다`() {
+        // when
+        val actual = parser.parse("1 + 2 / 3")
+
+        // then
+        assertThat(actual).isEqualTo(Expression(listOf(1, 2, 3), listOf(Symbol.Sign.PLUS, Symbol.Sign.DIVISION)))
+    }
+
+    @Test
+    fun `공백으로 구분되지 않은 올바른 수식을 파싱한다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `기호가 연속으로 등장하면 오류를 반환한다`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `공백으로 구분된 숫자가 연속으로 등장하면 오류를 반환한다`() {
+        TODO("Not yet implemented")
+    }
+}
