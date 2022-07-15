d3da412ff738406a6271924fe6e71207434c918b
Keich.Park <svclaw2000@gmail.com>
2022-07-15 22:55:23 +0900

    refactor: 파싱 함수를 분리한다

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/ExpressionParser.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/ExpressionParser.kt
@@ -3,7 +3,17 @@ package edu.nextstep.camp.calculator.domain
 class ExpressionParser {
 
     fun parse(expression: String): Expression {
-        val signList = """[^0-9 ]""".toRegex()
+        val signList = findSigns(expression)
+        val numberList = findNumbers(expression)
+
+        return Expression(
+            numberList = numberList,
+            signList = signList
+        )
+    }
+
+    private fun findSigns(expression: String): List<Symbol.Sign> {
+        return """[^0-9 ]""".toRegex()
             .findAll(expression)
             .map {
                 when (it.value) {
@@ -15,17 +25,14 @@ class ExpressionParser {
                 }
             }
             .toList()
+    }
 
-        val numberList = """[^0-9 ]""".toRegex()
+    private fun findNumbers(expression: String): List<Int> {
+        return """[^0-9 ]""".toRegex()
             .split(expression)
             .map {
                 if (it.isBlank()) throw IllegalArgumentException("수식은 연속된 연산자를 포함할 수 없습니다.")
                 it.trim().toIntOrNull() ?: throw IllegalArgumentException("수식은 연속된 피연산자를 포함할 수 없습니다.")
             }
-
-        return Expression(
-            numberList = numberList,
-            signList = signList
-        )
     }
 }
