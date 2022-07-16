c4a14af07e87195078f9b6570748bc4f040f94a3
Suhyeon Kim <wisemuji@gmail.com>
2022-07-17 00:04:38 +0900

    feat: 입력된 피연산자가 없을 때, 사용자가 연산자 버튼을 누르면 화면에 아무런 변화가 없어야 한다

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -9,6 +9,7 @@ import de.mannodermaus.junit5.ActivityScenarioExtension
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
 
 class MainActivityTest {
 
@@ -16,7 +17,7 @@ class MainActivityTest {
     @RegisterExtension
     val scenarioExtension = ActivityScenarioExtension.launch<MainActivity>()
 
-    @ParameterizedTest(name = "{0}에서 피연산자 {1}을 누르면 {2}가 보여야 한다.")
+    @ParameterizedTest
     @CsvSource(
         "'', 1, 1",
         "'5 + ', 1, 5 + 1",
@@ -37,6 +38,22 @@ class MainActivityTest {
 
         // then
         onView(textView).check(matches(withText(expected)))
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["+", "-", "×", "÷"])
+    fun 입력된_피연산자가_없을_때_사용자가_연산자_버튼을_누르면_화면에_아무런_변화가_없어야_한다(
+        operator: String
+    ) {
+        // given
+        val textView = withId(R.id.textView)
+        onView(textView).perform(setTextInTextView(""))
+
+        // when
+        onView(withText(operator)).perform(click())
+
+        // then
+        onView(textView).check(matches(withText("")))
     }
 
 }
