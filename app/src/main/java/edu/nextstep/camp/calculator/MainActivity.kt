7fdfa4d9d682f82ffa0b479d2c463e19c07b0b93
Lee-ji-soo <nbsp3@naver.com>
2022-07-16 19:00:59 +0900

    onClick이벤트 중복 제거 및 계산기 이벤트 등록

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,46 +1,41 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.soolee.domain.Calculator
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var calculator: Calculator
 
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        textView = binding.textView
+    }
 
-        binding.button0.setOnClickListener {
-            binding.textView.text = "0"
-        }
-        binding.button1.setOnClickListener {
-            binding.textView.text = "1"
-        }
-        binding.button2.setOnClickListener {
-            binding.textView.text = "2"
-        }
-        binding.button3.setOnClickListener {
-            binding.textView.text = "3"
-        }
-        binding.button4.setOnClickListener {
-            binding.textView.text = "4"
-        }
-        binding.button5.setOnClickListener {
-            binding.textView.text = "5"
-        }
-        binding.button6.setOnClickListener {
-            binding.textView.text = "6"
-        }
-        binding.button7.setOnClickListener {
-            binding.textView.text = "7"
-        }
-        binding.button8.setOnClickListener {
-            binding.textView.text = "8"
-        }
-        binding.button9.setOnClickListener {
-            binding.textView.text = "9"
-        }
+    fun onClickNumber(view: View) {
+        textView.append((view as Button).text)
+    }
+
+    fun onClickOperation(view: View) {
+        textView.append((view as Button).text)
+    }
+
+    fun onClickEqual(view: View) {
+        val text = textView.text.toString()
+        calculator = Calculator()
+        val value = calculator.calculate(text)
+        textView.text = value.toString()
+    }
+
+    fun onClickDelete(view: View) {
+        textView.text = ""
     }
 }
