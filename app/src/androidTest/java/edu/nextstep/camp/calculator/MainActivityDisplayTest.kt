7e122e6f1c500c7b5f6b6cc914f965aa010fbb31
greedy0110 <greedy0110@gmail.com>
2022-07-16 18:02:34 +0900

    display 테스트 작성
    - 입력된 피연산자가 없을 때, 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자가 화면에 보여야 한다.

---
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityDisplayTest.kt
@@ -0,0 +1,73 @@
+package edu.nextstep.camp.calculator
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityDisplayTest {
+
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun `display_number_when_the_present_operand_is_absent`() {
+        // when
+        click(1)
+
+        // then
+        checkDisplayText("1")
+    }
+
+    @Test
+    fun `display_number_when_the_present_operand_is_absent_2`() {
+        // given "5 +"
+        click(5)
+        click("+")
+
+
+        // when
+        click(1)
+
+        // then
+        checkDisplayText("5 + 1")
+    }
+
+    private fun checkDisplayText(text: String) {
+        onView(withId(R.id.textView)).check(matches(withText(text)))
+    }
+
+    private fun click(number: Int) {
+        val buttonId = when (number) {
+            0 -> R.id.button0
+            1 -> R.id.button1
+            2 -> R.id.button2
+            3 -> R.id.button3
+            4 -> R.id.button4
+            5 -> R.id.button5
+            6 -> R.id.button6
+            7 -> R.id.button7
+            8 -> R.id.button8
+            9 -> R.id.button9
+            else -> throw IllegalArgumentException("number must be between 0 and 9")
+        }
+        onView(withId(buttonId)).perform(click())
+    }
+
+    private fun click(op: String) {
+        val buttonId = when (op) {
+            "+" -> R.id.buttonPlus
+            "-" -> R.id.buttonMinus
+            "*" -> R.id.buttonMultiply
+            "/" -> R.id.buttonDivide
+            "=" -> R.id.buttonEquals
+            "<" -> R.id.buttonDelete
+            else -> throw IllegalArgumentException("not supported op: $op")
+        }
+        onView(withId(buttonId)).perform(click())
+    }
+}
--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -12,15 +12,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        binding.button0.setOnClickListener { appendNumber(0) }
+        binding.button1.setOnClickListener { appendNumber(1) }
+        binding.button2.setOnClickListener { appendNumber(2) }
+        binding.button3.setOnClickListener { appendNumber(3) }
+        binding.button4.setOnClickListener { appendNumber(4) }
+        binding.button5.setOnClickListener { appendNumber(5) }
+        binding.button6.setOnClickListener { appendNumber(6) }
+        binding.button7.setOnClickListener { appendNumber(7) }
+        binding.button8.setOnClickListener { appendNumber(8) }
+        binding.button9.setOnClickListener { appendNumber(9) }
+        binding.buttonPlus.setOnClickListener { appendOp("+") }
+        binding.buttonMinus.setOnClickListener { appendOp("-") }
+        binding.buttonMultiply.setOnClickListener { appendOp("*") }
+        binding.buttonDivide.setOnClickListener { appendOp("/") }
+        binding.buttonEquals.setOnClickListener { appendOp("=") }
+        binding.buttonDelete.setOnClickListener { appendOp("<") }
+    }
+
+    private fun appendNumber(number: Int) {
+        appendText(number.toString())
+    }
+
+    private fun appendOp(op: String) {
+        appendText(op)
+    }
+
+    private fun appendText(text: String) {
+        val origin = binding.textView.text
+        if (origin.isEmpty()) binding.textView.append(text)
+        else binding.textView.append(" $text")
     }
 }
