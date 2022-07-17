032ead92e670c2562db68df23521b57ce25ee560
link-js <link.js@kakaocorp.com>
2022-07-17 13:05:40 +0900

    refactor : 테스트 주석 given, when, then으로 변경

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -99,8 +99,9 @@ class MainActivityTest {
 
     @Test
     fun `입력된_피연산자가_있을_때_기존_숫자_뒤에_해당_숫자가_화면에_보여야_한다`() {
-        // when : 8 이 적혀있는 상태에서 9버튼을 누르면
+        // given : 8이 적혀있는 상태에서
         onView(withId(R.id.button8)).perform(click())
+        // when : 9버튼을 누르면
         onView(withId(R.id.button9)).perform(click())
         // then : 화면에 89가 보여야한다.
         onView(withId(R.id.textView)).check(matches(withText("89")))
@@ -108,8 +109,9 @@ class MainActivityTest {
 
     @Test
     fun `입력된_피연산자가_있을_때_사용자가_연산자_버튼을_누르면_해당_기호가_화면에_보여야_한다`() {
-        // when : 피연산자가 입력된 상태에서 연산자 버튼을 누르면
+        // given : 피연산자 1이 입력된 상태에서
         onView(withId(R.id.button1)).perform(click())
+        // when : 연산자 버튼 +를 누르면
         onView(withId(R.id.buttonPlus)).perform(click())
         // then : 화면에 해당 기호가 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("1 +")))
@@ -117,9 +119,10 @@ class MainActivityTest {
 
     @Test
     fun `마지막_글자가_연산자인경우_사용자가_연산자_버튼을_누르면_기호가_바뀌어서_화면에_보여야_한다`() {
-        // when : 마지막 글자가 연산자인경우 입력된 상태에서 연산자 버튼을 누르면
+        // given : 마지막 글자가 연산자로 입력된 상태에서
         onView(withId(R.id.button1)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
+        // when : 다른 연산자 버튼을 누르면
         onView(withId(R.id.buttonMinus)).perform(click())
         // then : 화면에 해당 기호가 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("1 -")))
@@ -135,9 +138,10 @@ class MainActivityTest {
 
     @Test
     fun `마지막_글자가_연산자인경우_사용자가_피연산자_버튼을_누르면_해당_숫자가_화면에_보여야_한다`() {
-        // when : 마지막 글자가 연산자인경우 사용자가 피연산자 버튼을 누르면
+        // given : 마지막 글자가 연산자일때
         onView(withId(R.id.button1)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
+        // when : 사용자가 피연산자 버튼을 누르면
         onView(withId(R.id.button3)).perform(click())
         // then : 해당 숫자가 화면에 보여야 한다
         onView(withId(R.id.textView)).check(matches(withText("1 + 3")))
@@ -153,9 +157,10 @@ class MainActivityTest {
 
     @Test
     fun `마지막_글자가_연산자일때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_연산자가_지워져야_한다`() {
-        // when : 1 + 가 입력 되어있을때, 사용자가 지우기 버튼을 누르면
+        // given : "1 +" 가 입력 되어있을때
         onView(withId(R.id.button1)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
+        // when : 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
         // then : 수식에 마지막으로 입력된 연산자가 지워져야 한다
         onView(withId(R.id.textView)).check(matches(withText("1")))
@@ -163,11 +168,12 @@ class MainActivityTest {
 
     @Test
     fun `마지막_글자가_한자리_숫자일때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_숫자가_지워져야_한다`() {
-        // when : "32 + 1" 이 입력 되어있을때, 사용자가 지우기 버튼을 누르면
+        // given : "32 + 1" 이 입력 되어있을때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.button2)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
         onView(withId(R.id.button1)).perform(click())
+        // when : 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
         // then : "32 +"가 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("32 +")))
@@ -175,9 +181,10 @@ class MainActivityTest {
 
     @Test
     fun `마지막_글자가_두자리이상_숫자일때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_숫자만_지워져야_한다`() {
-        // when : "32" 이 입력 되어있을때, 사용자가 지우기 버튼을 누르면
+        // given : "32" 이 입력 되어있을때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.button2)).perform(click())
+        // when : 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
         // then : "3"가 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("3")))
@@ -185,10 +192,11 @@ class MainActivityTest {
 
     @Test
     fun `입력된_수식이_완전할_때_사용자가_equals_버튼을_누르면_입력된_수식의_결과가_화면에_보여야_한다`() {
-        // when : "3 + 2" 이 입력 되어있을때, 사용자가 = 버튼을 누르면
+        // given : "3 + 2" 이 입력 되어있을때
         onView(withId(R.id.button3)).perform(click())
         onView(withId(R.id.buttonPlus)).perform(click())
         onView(withId(R.id.button2)).perform(click())
+        // when : 사용자가 = 버튼을 누르면
         onView(withId(R.id.buttonEquals)).perform(click())
         // then : "5"가 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("5")))
