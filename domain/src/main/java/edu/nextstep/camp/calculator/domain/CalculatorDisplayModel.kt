4324802ee60066885ea71c9fe0e25e7f8fe7ba55
greedy0110 <greedy0110@gmail.com>
2022-07-17 14:03:41 +0900

    ♻️ CalculatorDisplayModel가 token을 생성하는 책임을 가진다.

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/CalculatorDisplayModel.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/CalculatorDisplayModel.kt
@@ -1,12 +1,12 @@
 package edu.nextstep.camp.calculator.domain
 
+import java.util.Stack
+
 class CalculatorDisplayModel {
 
-    private val tokenizer = Tokenizer()
-    private var expression = ""
-    private val tokens get() = tokenizer.tokenize(expression)
+    private val _tokens = Stack<Token>()
     val displayText
-        get() = tokens.joinToString(
+        get() = _tokens.joinToString(
             separator = " ",
             transform = {
                 when (it) {
@@ -18,25 +18,61 @@ class CalculatorDisplayModel {
 
     fun put(number: Int) {
         require(number in 0..9) { "Invalid number $number, must be between 0 and 9." }
-        expression += number
+        if (_tokens.isEmpty()) {
+            _tokens.add(number.toOperand())
+            return
+        }
+
+        when (val last = _tokens.last()) {
+            is Operand -> {
+                _tokens.pop()
+                _tokens.push(last.merge(number))
+            }
+            is Operator -> {
+                _tokens.push(number.toOperand())
+            }
+        }
     }
 
     fun put(op: String) {
-        if (expression.isEmpty()) return
-        if (expression.last().isDigit()) expression += op
-        else if (expression.last() in Operator.values().map { it.symbol[0] }) {
-            expression = expression.dropLast(1) + op
+        if (_tokens.isEmpty()) {
+            return
         }
-        Operator.values().map { it.symbol }
+
+        when (_tokens.last()) {
+            is Operand -> {
+                _tokens.push(op.toOperator())
+            }
+            is Operator -> {
+                _tokens.pop()
+                _tokens.push(op.toOperator())
+            }
+        }
     }
 
     fun delete() {
-        expression = expression.dropLast(1)
+        if (_tokens.isEmpty()) return
+        when (val last = _tokens.last()) {
+            is Operand -> {
+                if (last.number < 10) _tokens.pop()
+                else {
+                    _tokens.pop()
+                    _tokens.push(last.dropLast())
+                }
+            }
+            is Operator -> _tokens.pop()
+        }
     }
 
     fun calculate() {
         val calculator = Calculator()
-        val result = calculator.evaluate(tokens)
-        expression = result.toString()
+        val result = calculator.evaluate(_tokens)
+        _tokens.clear()
+        _tokens.push(result.toOperand())
     }
+
+    private fun Int.toOperand() = Operand(this)
+    private fun Operand.merge(number: Int) = Operand("${this.number}$number".toInt())
+    private fun String.toOperator() = Operator.of(this)
+    private fun Operand.dropLast() = Operand(number / 10)
 }
