98d85ed92d7b9b1dff04092494d459ed4708d714
1stpie <1stpie@naver.com>
2022-07-18 03:04:21 +0900

    [UI테스트][4단계] 계산기 UI - UI 구현

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -12,37 +12,168 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
 
-@RunWith(Parameterized::class)
+//@RunWith(Parameterized::class)
 class MainActivityTest(
-    buttonDescription: String,
-    private val buttonId: Int,
-    private val expectedText: String
+//    buttonDescription: String,
+//    private val buttonId: Int,
+//    private val expectedText: String
 ) {
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-    companion object {
-        @JvmStatic
-        @Parameters(name = "버튼 : {0}, 동작 예상 텍스트 : {2}")
-        fun getTestParameters() = listOf(
-            arrayOf("버튼0", R.id.button0, "0"),
-            arrayOf("버튼1", R.id.button1, "1"),
-            arrayOf("버튼2", R.id.button2, "2"),
-            arrayOf("버튼3", R.id.button3, "3"),
-            arrayOf("버튼4", R.id.button4, "4"),
-            arrayOf("버튼5", R.id.button5, "5"),
-            arrayOf("버튼6", R.id.button6, "6"),
-            arrayOf("버튼7", R.id.button7, "7"),
-            arrayOf("버튼8", R.id.button8, "8"),
-            arrayOf("버튼9", R.id.button9, "9")
-        )
+//    companion object {
+//        @JvmStatic
+//        @Parameters(name = "버튼 : {0}, 동작 예상 텍스트 : {2}")
+//        fun getTestParameters() = listOf(
+//            arrayOf("버튼0", R.id.button0, "0"),
+//            arrayOf("버튼1", R.id.button1, "1"),
+//            arrayOf("버튼2", R.id.button2, "2"),
+//            arrayOf("버튼3", R.id.button3, "3"),
+//            arrayOf("버튼4", R.id.button4, "4"),
+//            arrayOf("버튼5", R.id.button5, "5"),
+//            arrayOf("버튼6", R.id.button6, "6"),
+//            arrayOf("버튼7", R.id.button7, "7"),
+//            arrayOf("버튼8", R.id.button8, "8"),
+//            arrayOf("버튼9", R.id.button9, "9")
+//        )
+//    }
+
+//    @Test
+//    fun `버튼_동작`() {
+//        //when: 버튼을 누르면
+//        onView(withId(buttonId)).perform(ViewActions.click())
+//        //then: 화면에 버튼의 텍스트가 보여야 한다
+//        onView(withId(R.id.textView)).check(matches(withText(expectedText)))
+//    }
+
+    //기능 요구 사항
+    /* 입력된 피연산자가 없을 때, 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자가 화면에 보여야 한다.
+    -> 1 클릭 -> 1 */
+    @Test
+    fun `입력된_피연산자가_없을때_숫자_클릭은_해당_숫자가_표시된다`() {
+        onView(withId(R.id.button1)).perform(ViewActions.click())
+        onView(withId(R.id.textView)).check(matches(withText("1")))
+    }
+
+    /* 5 + -> 1 클릭 -> 5 + 1 */
+    @Test
+    fun `입력된_피연산자가_없을때_숫자_클릭은_해당_숫자가_표시된다_Case2`() {
+        onView(withId(R.id.button5)).perform(ViewActions.click())
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+
+        onView(withId(R.id.button1)).perform(ViewActions.click())
+
+        onView(withId(R.id.textView)).check(matches(withText("5 + 1")))
     }
 
     @Test
-    fun `버튼_동작`() {
-        //when: 버튼을 누르면
-        onView(withId(buttonId)).perform(ViewActions.click())
-        //then: 화면에 버튼의 텍스트가 보여야 한다
-        onView(withId(R.id.textView)).check(matches(withText(expectedText)))
+    fun `입력된_피연산자가_있을때_숫자_클릭은_기존_숫자_뒤에_해당_숫자가_표시된다`() {
+        onView(withId(R.id.button8)).perform(ViewActions.click())
+
+        onView(withId(R.id.button9)).perform(ViewActions.click())
+
+        onView(withId(R.id.textView)).check(matches(withText("89")))
+    }
+
+    @Test
+    fun `입력된_피연산자가_없을때_연산자_클릭은_변화가_없다`() {
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+        onView(withId(R.id.buttonDivide)).perform(ViewActions.click())
+        onView(withId(R.id.buttonMultiply)).perform(ViewActions.click())
+        onView(withId(R.id.buttonMinus)).perform(ViewActions.click())
+
+        onView(withId(R.id.textView)).check(matches(withText("")))
+    }
+
+    @Test
+    fun `입력된_피연산자가_있을때_연산자_클릭은_해당_기호가_보인다`() {
+        onView(withId(R.id.button1)).perform(ViewActions.click())
+
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+
+        onView(withId(R.id.textView)).check(matches(withText("1 +")))
+    }
+
+    @Test
+    fun `입력된_피연산자가_있을때_연산자_클릭은_해당_기호가_보인다_Case2`() {
+        onView(withId(R.id.button1)).perform(ViewActions.click())
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+
+        onView(withId(R.id.buttonMinus)).perform(ViewActions.click())
+
+        onView(withId(R.id.textView)).check(matches(withText("1 -")))
+    }
+
+    @Test
+    fun `입력된_수식이_없을때_지우기_클릭은_변화가_없다`() {
+        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+
+        onView(withId(R.id.textView)).check(matches(withText("")))
+    }
+
+    @Test
+    fun `입력된_수식이_있을때_지우기_클릭은_마지막_연산자_또는_피연산자가_삭제된다`() {
+        //given
+        onView(withId(R.id.button3)).perform(ViewActions.click())
+        onView(withId(R.id.button2)).perform(ViewActions.click())
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+        onView(withId(R.id.button1)).perform(ViewActions.click())
+
+        //when
+        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("32 +")))
+
+        //when
+        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("32")))
+
+        //when
+        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("3")))
+
+        //when
+        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("")))
+
+        //when
+        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("")))
+    }
+
+    @Test
+    fun `입력된_수식이_완전할때_등호_클릭은_수식의_결과가_표시된다`() {
+        //given
+        onView(withId(R.id.button3)).perform(ViewActions.click())
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+        onView(withId(R.id.button2)).perform(ViewActions.click())
+
+        //when
+        onView(withId(R.id.buttonEquals)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("5")))
+    }
+
+    @Test
+    fun `입력된_수식이_완전하지_않을때_등호_클릭은_변화가_없다`() {
+        //given
+        onView(withId(R.id.button3)).perform(ViewActions.click())
+        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+
+        //when
+        onView(withId(R.id.buttonEquals)).perform(ViewActions.click())
+
+        //then
+        onView(withId(R.id.textView)).check(matches(withText("3 +")))
     }
 }
--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,26 +1,117 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
+import edu.nextstep.camp.calculator.domain.Calculator
+import edu.nextstep.camp.calculator.domain.Operation
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var viewState by Delegates.observable(MainState.DisplayText("")) { _, _, newValue ->
+        onViewState(newValue)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        initializeLayout()
     }
+
+    private fun initializeLayout() {
+        setContentView(binding.root)
+        binding.button0.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 0)) }
+        binding.button1.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 1)) }
+        binding.button2.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 2)) }
+        binding.button3.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 3)) }
+        binding.button4.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 4)) }
+        binding.button5.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 5)) }
+        binding.button6.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 6)) }
+        binding.button7.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 7)) }
+        binding.button8.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 8)) }
+        binding.button9.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 9)) }
+        binding.buttonDelete.setOnClickListener { onEvent(MainEvent.DeleteLast("${binding.textView.text}")) }
+        binding.buttonDivide.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Div.operator)) }
+        binding.buttonPlus.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Plus.operator)) }
+        binding.buttonMinus.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Minus.operator)) }
+        binding.buttonMultiply.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Mult.operator)) }
+        binding.buttonEquals.setOnClickListener { onEvent(MainEvent.Evalute("${binding.textView.text}")) }
+    }
+
+    //ui update
+    private fun onViewState(state: MainState) = when(state) {
+        is MainState.DisplayText -> binding.textView.text = state.displayText
+    }
+
+    //logic
+    private fun onEvent(event: MainEvent) {
+        when(event) {
+            is MainEvent.DeleteLast -> eventDeleteLast(event)
+            is MainEvent.AddOperator -> eventAddOperator(event)
+            is MainEvent.AddNumber -> eventAddNumber(event)
+            is MainEvent.Evalute -> eventEvalute(event)
+        }
+    }
+
+    private fun eventDeleteLast(event: MainEvent.DeleteLast) {
+        val text = getLastDeletedText(event.displayedText)
+        viewState = MainState.DisplayText(text)
+    }
+
+    private fun getLastDeletedText(displayedText: String) = displayedText
+        .dropLastWhile { it == ' ' || it == '.' }
+        .dropLast(1)
+        .dropLastWhile { it == ' ' || it == '.' }
+
+    private fun eventAddOperator(event: MainEvent.AddOperator) {
+        val lastChar = getTextViewLastChar(event.displayedText) ?: return
+        val displayedText = when(lastChar.isDigit()) {
+            true -> event.displayedText
+            false -> getLastDeletedText(event.displayedText)
+        }
+
+        val text = "$displayedText ${event.operator}"
+        viewState = MainState.DisplayText(text)
+    }
+
+    private fun eventEvalute(event: MainEvent.Evalute) {
+        runCatching {
+            Calculator().evalute(event.displayedText)
+        }.onSuccess {
+            viewState = MainState.DisplayText(getDisplayTextByEvalute(it))
+        }.onFailure {
+            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getDisplayTextByEvalute(computed: Double): String = when(computed - computed.toInt()) {
+        0.0 -> "${computed.toInt()}"
+        else -> "$computed"
+    }
+
+    private fun eventAddNumber(event: MainEvent.AddNumber) {
+        viewState = MainState.DisplayText(getDisplayTextByAddNumber(event, event.displayedText))
+    }
+
+    private fun getDisplayTextByAddNumber(
+        event: MainEvent.AddNumber,
+        displayedText: CharSequence
+    ) : String {
+        val lastChar: Char? = getTextViewLastChar(displayedText)
+        return when {
+            displayedText == "0" -> "${event.num}"
+            lastChar == null -> "${event.num}"
+            lastChar.isDigit() -> "$displayedText${event.num}"
+            lastChar == ' ' -> "$displayedText${event.num}"
+            else -> "$displayedText ${event.num}"
+        }
+    }
+
+    private fun getTextViewLastChar(
+        text: CharSequence
+    ) = text.getOrNull(text.lastIndex)
 }
