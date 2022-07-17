69c01935a7809d3758629cc5a074c3556a552c81
greedy0110 <greedy0110@gmail.com>
2022-07-17 16:45:59 +0900

    🧹 CalculatorDisplayModel 코드 정리

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/CalculatorDisplayModel.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/CalculatorDisplayModel.kt
@@ -4,9 +4,9 @@ import java.util.Stack
 
 class CalculatorDisplayModel {
 
-    private val _tokens = Stack<Token>()
+    private val tokens = Stack<Token>()
     val displayText
-        get() = _tokens.joinToString(
+        get() = tokens.joinToString(
             separator = " ",
             transform = {
                 when (it) {
@@ -18,57 +18,58 @@ class CalculatorDisplayModel {
 
     fun put(number: Int) {
         require(number in 0..9) { "Invalid number $number, must be between 0 and 9." }
-        if (_tokens.isEmpty()) {
-            _tokens.add(number.toOperand())
+        if (tokens.isEmpty()) {
+            tokens.add(number.toOperand())
             return
         }
 
-        when (val last = _tokens.last()) {
+        when (val last = tokens.last()) {
             is Operand -> {
-                _tokens.pop()
-                _tokens.push(last.merge(number))
+                tokens.pop()
+                tokens.push(last.merge(number))
             }
             is Operator -> {
-                _tokens.push(number.toOperand())
+                tokens.push(number.toOperand())
             }
         }
     }
 
     fun put(op: String) {
-        if (_tokens.isEmpty()) {
+        if (tokens.isEmpty()) {
             return
         }
 
-        when (_tokens.last()) {
+        when (tokens.last()) {
             is Operand -> {
-                _tokens.push(op.toOperator())
+                tokens.push(op.toOperator())
             }
             is Operator -> {
-                _tokens.pop()
-                _tokens.push(op.toOperator())
+                tokens.pop()
+                tokens.push(op.toOperator())
             }
         }
     }
 
     fun delete() {
-        if (_tokens.isEmpty()) return
-        when (val last = _tokens.last()) {
+        if (tokens.isEmpty()) return
+        when (val last = tokens.last()) {
             is Operand -> {
-                if (last.number < 10) _tokens.pop()
-                else {
-                    _tokens.pop()
-                    _tokens.push(last.dropLast())
+                if (last.number < 10) {
+                    tokens.pop()
+                } else {
+                    tokens.pop()
+                    tokens.push(last.dropLast())
                 }
             }
-            is Operator -> _tokens.pop()
+            is Operator -> tokens.pop()
         }
     }
 
     fun calculate() {
         val calculator = Calculator()
-        val result = calculator.evaluate(_tokens)
-        _tokens.clear()
-        _tokens.push(result.toOperand())
+        val result = calculator.evaluate(tokens)
+        tokens.clear()
+        tokens.push(result.toOperand())
     }
 
     private fun Int.toOperand() = Operand(this)
