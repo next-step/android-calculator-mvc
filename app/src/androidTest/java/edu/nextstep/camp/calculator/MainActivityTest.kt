2bec661bf72945b18a26f44596ea7344bb663723
link-js <link.js@kakaocorp.com>
2022-07-17 13:29:03 +0900

    refactor: 클릭한번에 하는 테스트 메소드 추가

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -120,8 +120,7 @@ class MainActivityTest {
     @Test
     fun `마지막_글자가_연산자인경우_사용자가_연산자_버튼을_누르면_기호가_바뀌어서_화면에_보여야_한다`() {
         // given : 마지막 글자가 연산자로 입력된 상태에서
-        onView(withId(R.id.button1)).perform(click())
-        onView(withId(R.id.buttonPlus)).perform(click())
+        clicks(R.id.button1, R.id.buttonPlus)
         // when : 다른 연산자 버튼을 누르면
         onView(withId(R.id.buttonMinus)).perform(click())
         // then : 화면에 해당 기호가 보여야 한다.
@@ -139,8 +138,7 @@ class MainActivityTest {
     @Test
     fun `마지막_글자가_연산자인경우_사용자가_피연산자_버튼을_누르면_해당_숫자가_화면에_보여야_한다`() {
         // given : 마지막 글자가 연산자일때
-        onView(withId(R.id.button1)).perform(click())
-        onView(withId(R.id.buttonPlus)).perform(click())
+        clicks(R.id.button1, R.id.buttonPlus)
         // when : 사용자가 피연산자 버튼을 누르면
         onView(withId(R.id.button3)).perform(click())
         // then : 해당 숫자가 화면에 보여야 한다
@@ -158,8 +156,7 @@ class MainActivityTest {
     @Test
     fun `마지막_글자가_연산자일때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_연산자가_지워져야_한다`() {
         // given : "1 +" 가 입력 되어있을때
-        onView(withId(R.id.button1)).perform(click())
-        onView(withId(R.id.buttonPlus)).perform(click())
+        clicks(R.id.button1, R.id.buttonPlus)
         // when : 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
         // then : 수식에 마지막으로 입력된 연산자가 지워져야 한다
@@ -169,10 +166,7 @@ class MainActivityTest {
     @Test
     fun `마지막_글자가_한자리_숫자일때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_숫자가_지워져야_한다`() {
         // given : "32 + 1" 이 입력 되어있을때
-        onView(withId(R.id.button3)).perform(click())
-        onView(withId(R.id.button2)).perform(click())
-        onView(withId(R.id.buttonPlus)).perform(click())
-        onView(withId(R.id.button1)).perform(click())
+        clicks(R.id.button3, R.id.button2, R.id.buttonPlus, R.id.button1)
         // when : 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
         // then : "32 +"가 보여야 한다.
@@ -182,8 +176,7 @@ class MainActivityTest {
     @Test
     fun `마지막_글자가_두자리이상_숫자일때_사용자가_지우기_버튼을_누르면_수식에_마지막으로_입력된_숫자만_지워져야_한다`() {
         // given : "32" 이 입력 되어있을때
-        onView(withId(R.id.button3)).perform(click())
-        onView(withId(R.id.button2)).perform(click())
+        clicks(R.id.button3, R.id.button2)
         // when : 사용자가 지우기 버튼을 누르면
         onView(withId(R.id.buttonDelete)).perform(click())
         // then : "3"가 보여야 한다.
@@ -193,12 +186,16 @@ class MainActivityTest {
     @Test
     fun `입력된_수식이_완전할_때_사용자가_equals_버튼을_누르면_입력된_수식의_결과가_화면에_보여야_한다`() {
         // given : "3 + 2" 이 입력 되어있을때
-        onView(withId(R.id.button3)).perform(click())
-        onView(withId(R.id.buttonPlus)).perform(click())
-        onView(withId(R.id.button2)).perform(click())
+        clicks(R.id.button3, R.id.buttonPlus, R.id.button2)
         // when : 사용자가 = 버튼을 누르면
         onView(withId(R.id.buttonEquals)).perform(click())
         // then : "5"가 보여야 한다.
         onView(withId(R.id.textView)).check(matches(withText("5")))
+    }
+
+    private fun clicks(vararg ids: Int) {
+        ids.forEach {
+            onView(withId(it)).perform(click())
+        }
     }
 }
