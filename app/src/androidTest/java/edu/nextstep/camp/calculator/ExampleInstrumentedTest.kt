a158a29ebd4110c300e03f3f39f5766e91f7025e
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-07-18 02:18:58 +0900

    중복된 edu경로 제거(2)

--- app/src/androidTest/java/edu/nextstep/camp/calculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("edu.edu.nextstep.camp.calculator", appContext.packageName)
+        assertEquals("edu.nextstep.camp.calculator", appContext.packageName)
     }
 }
