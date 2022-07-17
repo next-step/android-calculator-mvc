569e8eb960ad2b537e752522fc73fc9750062e29
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-07-18 02:32:25 +0900

    Splitter 리팩토링
    - Calculator.evaluate()에서 유효성 검사를 진행할 수 있도록 기존 식의 유효성 검사를 하던 코드 삭제
    - companion object -> object
    - Queue를 쓸 이유가 없어 List로 변경

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Splitter.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Splitter.kt
@@ -1,49 +1,20 @@
 package edu.nextstep.camp.calculator.domain
 
-import java.util.*
 
+object Splitter {
 
-class Splitter {
-    companion object {
-        private const val CHECKING_START_INDEX = 1
+    fun split(expression: String): List<Node> {
+        val splitList = expression.split(" ")
 
-        fun split(expression: String): Queue<Node> {
-            val splitList = expression.split(" ")
+        return splitList.map { toNode(it) }
+    }
 
-            var beforeNode: Node = toNode(splitList[0])
-
-            if (beforeNode is Operator) {
-                throw IllegalArgumentException("첫번째에는 연산자가 들어올 수 없습니다.")
-            }
-
-            val nodeQueue: Queue<Node> = LinkedList()
-            nodeQueue.offer(beforeNode)
-
-            for (i in CHECKING_START_INDEX until splitList.size) {
-                val node = toNode(splitList[i])
-                if (node is Operand && beforeNode !is Operator) {
-                    throw IllegalArgumentException("피연산자 이전 항목은 연산자여야만 합니다.")
-                } else if (node is Operator && beforeNode !is Operand) {
-                    throw IllegalArgumentException("연산자 이전 항목은 피연산자이어야만 합니다.")
-                } else {
-                    nodeQueue.offer(node)
-                    beforeNode = node
-                }
-            }
-
-            if (nodeQueue.last() is Operator) throw IllegalArgumentException("마지막에는 연산자가 들어올 수 없습니다.")
-
-            return nodeQueue
-        }
-
-        private fun toNode(token: String): Node {
-            val value = token.toIntOrNull()
-
-            return if (value != null) {
-                Operand(value)
-            } else {
-                Operator.getFrom(token)
-            }
+    private fun toNode(token: String): Node {
+        val number = token.toIntOrNull()
+        return if (number != null) {
+            Operand(number)
+        } else {
+            Operator(token)
         }
     }
 }
