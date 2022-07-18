a809eeb23a26ef679b01d2a011ab327bd40b819d
박찬준 <chanjun.park@navercorp.com>
2022-07-18 20:53:07 +0900

    컴파일 에러 수정

--- domain/src/main/java/edu/nextstep/camp/domain/NumberOperandToken.kt
+++ domain/src/main/java/edu/nextstep/camp/domain/NumberOperandToken.kt
@@ -1,6 +1,6 @@
 package edu.nextstep.camp.domain
 
-internal class NumberOperandToken @Throws(NumberFormatException::class) constructor(
+class NumberOperandToken @Throws(NumberFormatException::class) constructor(
 	tokenString: String
 ) : ExpressionToken(tokenString) {
 	val value = java.lang.Double.parseDouble(tokenString)
--- domain/src/main/java/edu/nextstep/camp/domain/OperatorToken.kt
+++ domain/src/main/java/edu/nextstep/camp/domain/OperatorToken.kt
@@ -1,6 +1,6 @@
 package edu.nextstep.camp.domain
 
-internal sealed class OperatorToken(tokenString: String) : ExpressionToken(tokenString) {
+sealed class OperatorToken(tokenString: String) : ExpressionToken(tokenString) {
 	object Addition: OperatorToken("+")
 	object Subtraction: OperatorToken("-")
 	object Multiplication: OperatorToken("×")
