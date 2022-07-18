8da8d0e30933be6ab4ce1717a64e17ed2f3fff46
KimKwangYoung <kwangyoung.dev@gmail.com>
2022-07-18 21:43:35 +0900

    기존 List의 size 비교에서 node가 정상적으로 들어갔는 지 확인하는 방식으로 변경

--- domain/src/test/java/edu/nextstep/camp/calculator/domain/SplitterTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/SplitterTest.kt
@@ -7,8 +7,11 @@ class SplitterTest {
     @Test
     fun 정상적으로_식이_공백으로_나뉘어지는지_확인한다() {
         val expression = "5 + 3 / 4"
-        val expected = 5
+        val expected = Operand(5)
 
-        assertEquals(Splitter.split(expression).size, expected)
+        val splitNodeList = Splitter.split(expression)
+        val actual = splitNodeList.first() as Operand
+
+        assertEquals(actual, expected)
     }
 }
