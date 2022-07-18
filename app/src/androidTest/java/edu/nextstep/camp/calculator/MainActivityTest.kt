9c4a27e447fe4b2a448a8b792baca1311639825c
1stpie <1stpie@naver.com>
2022-07-18 18:57:19 +0900

    [UI테스트][4단계] UI 계산기 - junit5 적용

--- app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
+++ app/src/androidTest/java/edu/nextstep/camp/calculator/MainActivityTest.kt
@@ -5,175 +5,187 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-import org.junit.runners.Parameterized.Parameters
+import de.mannodermaus.junit5.ActivityScenarioExtension
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
-//@RunWith(Parameterized::class)
-class MainActivityTest(
-//    buttonDescription: String,
-//    private val buttonId: Int,
-//    private val expectedText: String
-) {
-    @get:Rule
-    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+@DisplayName("메인화면 테스트")
+class MainActivityTest {
+    @JvmField
+    @RegisterExtension
+    val scenarioExtension = ActivityScenarioExtension.launch<MainActivity>()
 
-//    companion object {
-//        @JvmStatic
-//        @Parameters(name = "버튼 : {0}, 동작 예상 텍스트 : {2}")
-//        fun getTestParameters() = listOf(
-//            arrayOf("버튼0", R.id.button0, "0"),
-//            arrayOf("버튼1", R.id.button1, "1"),
-//            arrayOf("버튼2", R.id.button2, "2"),
-//            arrayOf("버튼3", R.id.button3, "3"),
-//            arrayOf("버튼4", R.id.button4, "4"),
-//            arrayOf("버튼5", R.id.button5, "5"),
-//            arrayOf("버튼6", R.id.button6, "6"),
-//            arrayOf("버튼7", R.id.button7, "7"),
-//            arrayOf("버튼8", R.id.button8, "8"),
-//            arrayOf("버튼9", R.id.button9, "9")
-//        )
-//    }
+    @Nested
+    @DisplayName("버튼 동작")
+    inner class `버튼_기본_동작` {
 
-//    @Test
-//    fun `버튼_동작`() {
-//        //when: 버튼을 누르면
-//        onView(withId(buttonId)).perform(ViewActions.click())
-//        //then: 화면에 버튼의 텍스트가 보여야 한다
-//        onView(withId(R.id.textView)).check(matches(withText(expectedText)))
-//    }
 
-    //기능 요구 사항
-    /* 입력된 피연산자가 없을 때, 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자가 화면에 보여야 한다.
-    -> 1 클릭 -> 1 */
-    @Test
-    fun `입력된_피연산자가_없을때_숫자_클릭은_해당_숫자가_표시된다`() {
-        onView(withId(R.id.button1)).perform(ViewActions.click())
-        onView(withId(R.id.textView)).check(matches(withText("1")))
+        @CsvSource("'버튼0', ${R.id.button0}, '0'",
+            "'버튼1', ${R.id.button1}, '1'",
+            "'버튼2', ${R.id.button2}, '2'",
+            "'버튼3', ${R.id.button3}, '3'",
+            "'버튼4', ${R.id.button4}, '4'",
+            "'버튼5', ${R.id.button5}, '5'",
+            "'버튼6', ${R.id.button6}, '6'",
+            "'버튼7', ${R.id.button7}, '7'",
+            "'버튼8', ${R.id.button8}, '8'",
+            "'버튼9', ${R.id.button9}, '9'")
+        @DisplayName("동작")
+        @ParameterizedTest(name = "{0} 선택, 예상결과 = {2}")
+        fun `버튼_동작`(buttonDescription: String, buttonId: Int, expectedText: String) {
+            //when: 버튼을 누르면
+            onView(withId(buttonId)).perform(ViewActions.click())
+            //then: 화면에 버튼의 텍스트가 보여야 한다
+            onView(withId(R.id.textView)).check(matches(withText(expectedText)))
+        }
     }
 
-    /* 5 + -> 1 클릭 -> 5 + 1 */
-    @Test
-    fun `입력된_피연산자가_없을때_숫자_클릭은_해당_숫자가_표시된다_Case2`() {
-        onView(withId(R.id.button5)).perform(ViewActions.click())
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+    @Nested
+    @DisplayName("연산 동작")
+    inner class `연산_동작` {
+        //기능 요구 사항
+        /* 입력된 피연산자가 없을 때, 사용자가 피연산자 0 ~ 9 버튼을 누르면 화면에 해당 숫자가 화면에 보여야 한다.
+        -> 1 클릭 -> 1 */
+        @Test
+        @DisplayName("' '-> '1' 클릭 = '1'")
+        fun `입력된_피연산자가_없을때_숫자_클릭은_해당_숫자가_표시된다`() {
+            onView(withId(R.id.button1)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("1")))
+        }
 
-        onView(withId(R.id.button1)).perform(ViewActions.click())
+        /* 5 + -> 1 클릭 -> 5 + 1 */
+        @Test
+        @DisplayName("'5 +' -> '1' 클릭 = '5 + 1'")
+        fun `입력된_피연산자가_없을때_숫자_클릭은_해당_숫자가_표시된다_Case2`() {
+            onView(withId(R.id.button5)).perform(ViewActions.click())
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
 
-        onView(withId(R.id.textView)).check(matches(withText("5 + 1")))
-    }
+            onView(withId(R.id.button1)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_피연산자가_있을때_숫자_클릭은_기존_숫자_뒤에_해당_숫자가_표시된다`() {
-        onView(withId(R.id.button8)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("5 + 1")))
+        }
 
-        onView(withId(R.id.button9)).perform(ViewActions.click())
+        @Test
+        @DisplayName("'8' -> '9' 클릭 = '89'")
+        fun `입력된_피연산자가_있을때_숫자_클릭은_기존_숫자_뒤에_해당_숫자가_표시된다`() {
+            onView(withId(R.id.button8)).perform(ViewActions.click())
 
-        onView(withId(R.id.textView)).check(matches(withText("89")))
-    }
+            onView(withId(R.id.button9)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_피연산자가_없을때_연산자_클릭은_변화가_없다`() {
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
-        onView(withId(R.id.buttonDivide)).perform(ViewActions.click())
-        onView(withId(R.id.buttonMultiply)).perform(ViewActions.click())
-        onView(withId(R.id.buttonMinus)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("89")))
+        }
 
-        onView(withId(R.id.textView)).check(matches(withText("")))
-    }
+        @Test
+        @DisplayName("' ' -> '+ or - or / or *' 클릭 = ' '")
+        fun `입력된_피연산자가_없을때_연산자_클릭은_변화가_없다`() {
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+            onView(withId(R.id.buttonDivide)).perform(ViewActions.click())
+            onView(withId(R.id.buttonMultiply)).perform(ViewActions.click())
+            onView(withId(R.id.buttonMinus)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_피연산자가_있을때_연산자_클릭은_해당_기호가_보인다`() {
-        onView(withId(R.id.button1)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("")))
+        }
 
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+        @Test
+        @DisplayName("1 -> '+' 클릭 = '1 +'")
+        fun `입력된_피연산자가_있을때_연산자_클릭은_해당_기호가_보인다`() {
+            onView(withId(R.id.button1)).perform(ViewActions.click())
 
-        onView(withId(R.id.textView)).check(matches(withText("1 +")))
-    }
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_피연산자가_있을때_연산자_클릭은_해당_기호가_보인다_Case2`() {
-        onView(withId(R.id.button1)).perform(ViewActions.click())
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("1 +")))
+        }
 
-        onView(withId(R.id.buttonMinus)).perform(ViewActions.click())
+        @Test
+        @DisplayName("'1 +' -> '-' 클릭 = '1 -'")
+        fun `입력된_피연산자가_있을때_연산자_클릭은_해당_기호가_보인다_Case2`() {
+            onView(withId(R.id.button1)).perform(ViewActions.click())
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
 
-        onView(withId(R.id.textView)).check(matches(withText("1 -")))
-    }
+            onView(withId(R.id.buttonMinus)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_수식이_없을때_지우기_클릭은_변화가_없다`() {
-        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("1 -")))
+        }
 
-        onView(withId(R.id.textView)).check(matches(withText("")))
-    }
+        @Test
+        @DisplayName("' ' -> '<-' 클릭 = ' '")
+        fun `입력된_수식이_없을때_지우기_클릭은_변화가_없다`() {
+            onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_수식이_있을때_지우기_클릭은_마지막_연산자_또는_피연산자가_삭제된다`() {
-        //given
-        onView(withId(R.id.button3)).perform(ViewActions.click())
-        onView(withId(R.id.button2)).perform(ViewActions.click())
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
-        onView(withId(R.id.button1)).perform(ViewActions.click())
+            onView(withId(R.id.textView)).check(matches(withText("")))
+        }
 
-        //when
-        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+        @Test
+        @DisplayName("'32 + 1' -> '<-' 5 회 클릭 = ' '")
+        fun `입력된_수식이_있을때_지우기_클릭은_마지막_연산자_또는_피연산자가_삭제된다`() {
+            //given
+            onView(withId(R.id.button3)).perform(ViewActions.click())
+            onView(withId(R.id.button2)).perform(ViewActions.click())
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+            onView(withId(R.id.button1)).perform(ViewActions.click())
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("32 +")))
+            //when
+            onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
 
-        //when
-        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("32 +")))
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("32")))
+            //when
+            onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
 
-        //when
-        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("32")))
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("3")))
+            //when
+            onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
 
-        //when
-        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("3")))
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("")))
+            //when
+            onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
 
-        //when
-        onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("")))
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("")))
-    }
+            //when
+            onView(withId(R.id.buttonDelete)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_수식이_완전할때_등호_클릭은_수식의_결과가_표시된다`() {
-        //given
-        onView(withId(R.id.button3)).perform(ViewActions.click())
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
-        onView(withId(R.id.button2)).perform(ViewActions.click())
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("")))
+        }
 
-        //when
-        onView(withId(R.id.buttonEquals)).perform(ViewActions.click())
+        @Test
+        @DisplayName("'3 + 2' -> '=' 클릭 = '5'")
+        fun `입력된_수식이_완전할때_등호_클릭은_수식의_결과가_표시된다`() {
+            //given
+            onView(withId(R.id.button3)).perform(ViewActions.click())
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+            onView(withId(R.id.button2)).perform(ViewActions.click())
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("5")))
-    }
+            //when
+            onView(withId(R.id.buttonEquals)).perform(ViewActions.click())
 
-    @Test
-    fun `입력된_수식이_완전하지_않을때_등호_클릭은_변화가_없다`() {
-        //given
-        onView(withId(R.id.button3)).perform(ViewActions.click())
-        onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("5")))
+        }
 
-        //when
-        onView(withId(R.id.buttonEquals)).perform(ViewActions.click())
+        @Test
+        @DisplayName("'3 +' -> '=' 클릭 = '3 +'")
+        fun `입력된_수식이_완전하지_않을때_등호_클릭은_변화가_없다`() {
+            //given
+            onView(withId(R.id.button3)).perform(ViewActions.click())
+            onView(withId(R.id.buttonPlus)).perform(ViewActions.click())
 
-        //then
-        onView(withId(R.id.textView)).check(matches(withText("3 +")))
+            //when
+            onView(withId(R.id.buttonEquals)).perform(ViewActions.click())
+
+            //then
+            onView(withId(R.id.textView)).check(matches(withText("3 +")))
+        }
     }
 }
--- domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
+++ domain/src/test/java/edu/nextstep/camp/calculator/domain/CalculatorTest.kt
@@ -1,160 +1,86 @@
 package edu.nextstep.camp.calculator.domain
 
 import com.google.common.truth.Truth.assertThat
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
-class CalculatorTest {
+@DisplayName("문자열 계산기 테스트")
+internal class CalculatorTest {
+
     private lateinit var calculator: Calculator
 
-    @Before
+    @BeforeEach
     fun setUp() {
         calculator = Calculator()
     }
 
-    @Test
-    fun `덧셈이 되어야 한다`() {
-        //given
-        val requested = "11 +    222"
-        val expected = 233.toDouble()
+    companion object {
+        @JvmStatic
+        fun givenWrongOperationsTestSource() : Stream<Arguments> = Stream.of(
+            Arguments.of(null, IllegalArgumentException("잘못된 요청입니다.")),
+            Arguments.of(" ", IllegalArgumentException("잘못된 요청입니다.")),
+            Arguments.of("121  % 222", IllegalArgumentException("잘못된 연산자가 포함되었습니다.")),
+            Arguments.of("3 * 4 / ", IllegalArgumentException("완성되지 않은 수식입니다.")),
+            Arguments.of("3 *//* 4", IllegalArgumentException("잘못된 연산자가 포함되었습니다.")),
+        )
 
-        //when
-        val actual = evaluteOrException(calculator, requested)
+        fun evaluteOrException(calculator: Calculator, requested: String?) = try {
+            calculator.evalute(requested)
+        } catch (e: Exception) {
+            e
+        }
 
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun  `뺄셈이 되어야 한다`() {
-        //given
-        val requested = "11 -    222"
-        val expected = (-211).toDouble()
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `나눗셈이 되어야 한다`() {
-        //given
-        val requested = "222 /    2 / 111"
-        val expected = 1.toDouble()
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `곱셈이 되어야 한다`() {
-        //given
-        val requested = "222 *    2 * 3"
-        val expected = 1332.toDouble()
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `입력값이 null일 경우 IllegalArgumentException throw`() {
-        //given
-        val requested = null
-        val expected = IllegalArgumentException("잘못된 요청입니다.")
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `입력값이 공백 일 경우 IllegalArgumentException throw`() {
-        //given
-        val requested = " "
-        val expected = IllegalArgumentException("잘못된 요청입니다.")
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `사칙연산 기호가 아닌 경우 IllegalArgumentException throw`() {
-        //given
-        val requested = "121  % 222"
-        val expected = IllegalArgumentException("잘못된 연산자가 포함되었습니다.")
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `사칙 연산을 모두 포함하는 경우`() {
-        //given
-        val requested = "2 + 3 * 4 / 2"
-        val expected = 10.toDouble()
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `완성되지 않는 수식 요청의 경우 IllegaArgumentException throw`() {
-        //given
-        val requested = "3 * 4 / "
-        val expected = IllegalArgumentException("완성되지 않은 수식입니다.")
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    @Test
-    fun `연산자가 연속적으로 입력되었을 경우 IllegalArgumentException throw`() {
-        //given
-        val requested = "3 *//* 4"
-        val expected = IllegalArgumentException("잘못된 연산자가 포함되었습니다.")
-
-        //when
-        val actual = evaluteOrException(calculator, requested)
-
-        //then
-        assertThat(actual, expected)
-    }
-
-    private fun evaluteOrException(calculator: Calculator, requested: String?) = try {
-        calculator.evalute(requested)
-    } catch (e: Exception) {
-        e
-    }
-
-    private fun assertThat(actual: Any, expected: Any) {
-        when {
-            actual is Throwable && expected is Throwable -> {
-                assertThat(actual).isInstanceOf(expected::class.java)
-                assertThat(actual.message).startsWith(expected.message)
+        fun assertThat(actual: Any, expected: Any?) {
+            when {
+                actual is Throwable && expected is Throwable -> {
+                    assertThat(actual).isInstanceOf(expected::class.java)
+                    assertThat(actual.message).startsWith(expected.message)
+                }
+                else -> assertThat(actual).isEqualTo(expected)
             }
-            else -> assertThat(actual).isEqualTo(expected)
+        }
+    }
+
+    @Nested
+    @DisplayName("올바른 연산이 주어지면")
+    inner class GivenRightOperationsTest {
+        @CsvSource(
+            "'11 +    222', ${233.toDouble()}",
+            "'11 -    222', ${(-211).toDouble()}",
+            "'222 /    2 / 111', ${1.toDouble()}",
+            "'222 *    2 * 3', ${1332.toDouble()}",
+            "'2 + 3 * 4 / 2', ${10.toDouble()}"
+        )
+        @ParameterizedTest(name = "{0} = {1}")
+        @DisplayName("계산은 성공해야 한다.")
+        fun `연산을 한다`(requested: String?, expected: Double) {
+            //when
+            val actual = evaluteOrException(calculator, requested)
+
+            //then
+            assertThat(actual, expected)
+        }
+    }
+
+    @Nested
+    @DisplayName("잘못된 연산이 주어지면")
+    inner class GivenWrongOperationsTest {
+
+        @MethodSource("edu.nextstep.camp.calculator.domain.CalculatorTest#givenWrongOperationsTestSource")
+        @ParameterizedTest(name = "[{0}] -> {1}")
+        @DisplayName("throw IllegalArgumentExcetion 해야 한다.")
+        fun `잘못된 연산을 한다`(requested: String?, expected: IllegalArgumentException) {
+            //when
+            val actual = evaluteOrException(calculator, requested)
+
+            //then
+            assertThat(actual, expected)
         }
     }
 }
