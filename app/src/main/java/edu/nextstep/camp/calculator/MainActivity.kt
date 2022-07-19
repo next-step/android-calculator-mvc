e8a256a96309900d693e2c44540e9a0c3601646f
hyowan park <woga1999@gmail.com>
2022-07-20 00:26:20 +0900

    Step3: 문자열 계산기 (#163)
    * feat: 계산 로직 및 테스트 코드 추가
    
    * feat: UI click listener 기능들 추가
    
    * fix: 테스트 코드 수정 및 케이스 추가
    
    * fix: 컬렉션 API를 사용해 간결하게 코드를 수정 및 split시 쓰이는 문자열 네이밍을 변경
    
    * fix: SRP에 따른 클래스 분리 (Calculator, ExpressionStack)

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -6,21 +6,47 @@ import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var calculator: Calculator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.tvResult.text = "0" }
-        binding.button1.setOnClickListener { binding.tvResult.text = "1" }
-        binding.button2.setOnClickListener { binding.tvResult.text = "2" }
-        binding.button3.setOnClickListener { binding.tvResult.text = "3" }
-        binding.button4.setOnClickListener { binding.tvResult.text = "4" }
-        binding.button5.setOnClickListener { binding.tvResult.text = "5" }
-        binding.button6.setOnClickListener { binding.tvResult.text = "6" }
-        binding.button7.setOnClickListener { binding.tvResult.text = "7" }
-        binding.button8.setOnClickListener { binding.tvResult.text = "8" }
-        binding.button9.setOnClickListener { binding.tvResult.text = "9" }
+        calculator = Calculator()
+        setViewClickListener()
+    }
+
+    private fun setViewClickListener() {
+        binding.button0.setOnClickListener { addString("0") }
+        binding.button1.setOnClickListener { addString("1") }
+        binding.button2.setOnClickListener { addString("2") }
+        binding.button3.setOnClickListener { addString("3") }
+        binding.button4.setOnClickListener { addString("4") }
+        binding.button5.setOnClickListener { addString("5") }
+        binding.button6.setOnClickListener { addString("6") }
+        binding.button7.setOnClickListener { addString("7") }
+        binding.button8.setOnClickListener { addString("8") }
+        binding.button9.setOnClickListener { addString("9") }
+        binding.buttonDivide.setOnClickListener { addString(" / ") }
+        binding.buttonMinus.setOnClickListener { addString(" - ") }
+        binding.buttonMultiply.setOnClickListener { addString(" * ") }
+        binding.buttonPlus.setOnClickListener { addString(" + ") }
+        binding.buttonEquals.setOnClickListener { calculateString() }
+        binding.buttonDelete.setOnClickListener { deleteString() }
+    }
+
+    private fun addString(text: String) {
+        val expression = binding.tvResult.text
+        binding.tvResult.text = "$expression$text"
+    }
+
+    private fun calculateString() {
+        val expression = binding.tvResult.text.toString()
+        binding.tvResult.text = calculator.evaluate(expression).toString()
+    }
+
+    private fun deleteString() {
+        binding.tvResult.text = binding.tvResult.text.dropLast(1)
     }
 }
--- domain/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/Calculator.kt
@@ -1,4 +1,51 @@
 package edu.nextstep.camp.calculator
 
 class Calculator {
+
+    fun evaluate(expression: String?): Int {
+        val expressionStack = ExpressionStack()
+        val expressionWithoutBlank = expression?.split(DELIMITER) ?: throw IllegalArgumentException()
+
+        expressionWithoutBlank.forEach { str ->
+            val stack = expressionStack.getStackForCalculating(str)
+            stack?.let {
+                val type = it.pop()
+                val value = it.pop()
+                val result = calculateValue(type, value.toInt(), str.toInt())
+                expressionStack.pushResult(result.toString())
+            }
+        }
+        return expressionStack.getStackPeekIntegerValue()
+    }
+
+    private fun calculateValue(type: String, value: Int, value2: Int) : Int {
+        val result = when(type) {
+            MathematicalSymbol.PLUS.type -> plus(value, value2)
+            MathematicalSymbol.MINUS.type -> minus(value, value2)
+            MathematicalSymbol.MULTIPLE.type -> multiple(value, value2)
+            MathematicalSymbol.DIVIDE.type -> divide(value, value2)
+            else -> throw IllegalArgumentException()
+        }
+        return result
+    }
+
+    private fun plus (value: Int, value2: Int) : Int {
+        return value + value2
+    }
+
+    private fun multiple (value: Int, value2: Int) : Int {
+        return value * value2
+    }
+
+    private fun divide (value: Int, value2: Int) : Int {
+        return value / value2
+    }
+
+    private fun minus (value: Int, value2: Int) : Int {
+        return value - value2
+    }
+
+    companion object {
+        const val DELIMITER = " "
+    }
 }
