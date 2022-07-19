6e824d7f3f9c334170fac57c3ba6021ce4cbc536
1stpie <1stpie@naver.com>
2022-07-19 13:07:56 +0900

    [UI테스트][4단계] UI 계산기 - 로직 분리 피드백 반영

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -4,110 +4,47 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
-import edu.nextstep.camp.calculator.domain.Calculator
 import edu.nextstep.camp.calculator.domain.Operation
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-    private var viewState by Delegates.observable(MainState.DisplayText("")) { _, _, newValue ->
-        onViewState(newValue)
-    }
+    private val mainLogic: MainActivityViewModel = MainActivityViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         initializeLayout()
+        observeViewState()
     }
 
     private fun initializeLayout() {
         setContentView(binding.root)
-        binding.button0.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 0)) }
-        binding.button1.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 1)) }
-        binding.button2.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 2)) }
-        binding.button3.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 3)) }
-        binding.button4.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 4)) }
-        binding.button5.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 5)) }
-        binding.button6.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 6)) }
-        binding.button7.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 7)) }
-        binding.button8.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 8)) }
-        binding.button9.setOnClickListener { onEvent(MainEvent.AddNumber("${binding.textView.text}", 9)) }
-        binding.buttonDelete.setOnClickListener { onEvent(MainEvent.DeleteLast("${binding.textView.text}")) }
-        binding.buttonDivide.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Div.operator)) }
-        binding.buttonPlus.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Plus.operator)) }
-        binding.buttonMinus.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Minus.operator)) }
-        binding.buttonMultiply.setOnClickListener { onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Mult.operator)) }
-        binding.buttonEquals.setOnClickListener { onEvent(MainEvent.Evalute("${binding.textView.text}")) }
+        binding.button0.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 0)) }
+        binding.button1.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 1)) }
+        binding.button2.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 2)) }
+        binding.button3.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 3)) }
+        binding.button4.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 4)) }
+        binding.button5.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 5)) }
+        binding.button6.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 6)) }
+        binding.button7.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 7)) }
+        binding.button8.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 8)) }
+        binding.button9.setOnClickListener { mainLogic.onEvent(MainEvent.AddNumber("${binding.textView.text}", 9)) }
+        binding.buttonDelete.setOnClickListener { mainLogic.onEvent(MainEvent.DeleteLast("${binding.textView.text}")) }
+        binding.buttonDivide.setOnClickListener { mainLogic.onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Div.operator)) }
+        binding.buttonPlus.setOnClickListener { mainLogic.onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Plus.operator)) }
+        binding.buttonMinus.setOnClickListener { mainLogic.onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Minus.operator)) }
+        binding.buttonMultiply.setOnClickListener { mainLogic.onEvent(MainEvent.AddOperator("${binding.textView.text}", Operation.Mult.operator)) }
+        binding.buttonEquals.setOnClickListener { mainLogic.onEvent(MainEvent.Evalute("${binding.textView.text}")) }
+    }
+
+    private fun observeViewState(){
+        mainLogic.viewState.observe(this, this::onViewState)
     }
 
     //ui update
     private fun onViewState(state: MainState) = when(state) {
         is MainState.DisplayText -> binding.textView.text = state.displayText
-    }
-
-    //logic
-    private fun onEvent(event: MainEvent) {
-        when(event) {
-            is MainEvent.DeleteLast -> eventDeleteLast(event)
-            is MainEvent.AddOperator -> eventAddOperator(event)
-            is MainEvent.AddNumber -> eventAddNumber(event)
-            is MainEvent.Evalute -> eventEvalute(event)
-        }
-    }
-
-    private fun eventDeleteLast(event: MainEvent.DeleteLast) {
-        val text = deleteLastOperatorOrNumber(event.displayedText)
-        viewState = MainState.DisplayText(text)
-    }
-
-    private fun deleteLastOperatorOrNumber(displayedText: String) = displayedText
-        .dropLastWhile { it == ' ' || it == '.' }
-        .dropLast(1)
-        .dropLastWhile { it == ' ' || it == '.' }
-
-    private fun eventAddOperator(event: MainEvent.AddOperator) {
-        val lastChar = event.displayedText.getOrNull(event.displayedText.lastIndex) ?: return
-        val displayedText = when(lastChar.isDigit()) {
-            true -> event.displayedText
-            false -> deleteLastOperatorOrNumber(event.displayedText)
-        }
-
-        val text = "$displayedText ${event.operator}"
-        viewState = MainState.DisplayText(text)
-    }
-
-    private fun eventEvalute(event: MainEvent.Evalute) {
-        runCatching {
-            Calculator().evalute(event.displayedText)
-        }.onSuccess {
-            viewState = MainState.DisplayText(getEvalutedDisplayText(it))
-        }.onFailure {
-            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun getEvalutedDisplayText(computed: Double): String = when(computed - computed.toInt()) {
-        0.0 -> "${computed.toInt()}"
-        else -> "$computed"
-    }
-
-    private fun eventAddNumber(event: MainEvent.AddNumber) {
-        viewState = MainState.DisplayText(getNumberAddedDisplayText(event, event.displayedText))
-    }
-
-    private fun getNumberAddedDisplayText(
-        event: MainEvent.AddNumber,
-        displayedText: CharSequence
-    ) : String {
-        val lastChar: Char? = displayedText.getOrNull(displayedText.lastIndex)
-        return when {
-            displayedText == "0" -> "${event.num}"
-            lastChar == null -> "${event.num}"
-            lastChar.isDigit() -> "$displayedText${event.num}"
-            lastChar == ' ' -> "$displayedText${event.num}"
-            else -> "$displayedText ${event.num}"
-        }
+        is MainState.ShowToast -> Toast.makeText(this, state.message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/edu/nextstep/camp/calculator/MainState.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainState.kt
@@ -2,4 +2,5 @@ package edu.nextstep.camp.calculator
 
 sealed class MainState {
     data class DisplayText(val displayText: String): MainState()
+    data class ShowToast(val message: String): MainState()
 }
