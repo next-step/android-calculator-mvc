0020ffea9da0b28e3add9f6d5ba4784bf8a53cd2
sinoso <tpgus123123@naver.com>
2022-07-20 21:26:15 +0900

    refactor : setOperandButtons 함수 분리

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
@@ -11,15 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        setOperandButtons()
     }
+
+    private fun setOperandButtons() {
+        fun Button.setOperandButtonClickListener(number: String) = this.setOnClickListener {
+            binding.textView.text = number
+        }
+        binding.button0.setOperandButtonClickListener("0")
+        binding.button1.setOperandButtonClickListener("1")
+        binding.button2.setOperandButtonClickListener("2")
+        binding.button3.setOperandButtonClickListener("3")
+        binding.button4.setOperandButtonClickListener("4")
+        binding.button5.setOperandButtonClickListener("5")
+        binding.button6.setOperandButtonClickListener("6")
+        binding.button7.setOperandButtonClickListener("7")
+        binding.button8.setOperandButtonClickListener("8")
+        binding.button9.setOperandButtonClickListener("9")
+    }
+
 }
