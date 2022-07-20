b9fdb15031b28c0421a6c1b5b2f752b488171eb5
Lee-ji-soo <nbsp3@naver.com>
2022-07-21 02:47:32 +0900

    onClick이벤트를 setOnClickListener로 변경

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -1,37 +1,60 @@
 package edu.nextstep.camp.calculator
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
-import edu.nextstep.camp.calculator.domain.Calculator
-import edu.nextstep.camp.calculator.domain.CalculatorInputConverter
+import edu.nextstep.camp.calculator.domain.Expression
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var calculator: Calculator = Calculator()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initAddEventListener()
     }
 
-    fun onClickCalculatorButton(view: View) {
-        binding.textView.append((view as Button).text)
+    private fun initAddEventListener() {
+        binding.button0.setOnClickListener { onClickNumberButton(binding.button0.text.toString()) }
+        binding.button1.setOnClickListener { onClickNumberButton(binding.button1.text.toString()) }
+        binding.button2.setOnClickListener { onClickNumberButton(binding.button2.text.toString()) }
+        binding.button3.setOnClickListener { onClickNumberButton(binding.button3.text.toString()) }
+        binding.button4.setOnClickListener { onClickNumberButton(binding.button4.text.toString()) }
+        binding.button5.setOnClickListener { onClickNumberButton(binding.button5.text.toString()) }
+        binding.button6.setOnClickListener { onClickNumberButton(binding.button6.text.toString()) }
+        binding.button7.setOnClickListener { onClickNumberButton(binding.button7.text.toString()) }
+        binding.button8.setOnClickListener { onClickNumberButton(binding.button8.text.toString()) }
+        binding.button9.setOnClickListener { onClickNumberButton(binding.button9.text.toString()) }
+
+        binding.buttonDivide.setOnClickListener { onClickOperatorButton(binding.buttonDivide.text.toString()) }
+        binding.buttonMultiply.setOnClickListener { onClickOperatorButton(binding.buttonMultiply.text.toString()) }
+        binding.buttonMinus.setOnClickListener { onClickOperatorButton(binding.buttonMinus.text.toString()) }
+        binding.buttonPlus.setOnClickListener { onClickOperatorButton(binding.buttonPlus.text.toString()) }
+
+        binding.buttonEquals.setOnClickListener { onClickEqualButton() }
+
+        binding.buttonDelete.setOnClickListener { onClickDelete() }
     }
 
-    fun onClickEqual(view: View) {
-        val text = binding.textView.text.toString()
-        val value = calculator.calculate(
-            CalculatorInputConverter
-                .getCalculatorInputToTextArr(text)
-        )
+    private fun onClickNumberButton(string: String) {
+        binding.textView.append(string)
+        Expression.addNumber(string)
+    }
+
+    private fun onClickOperatorButton(string: String) {
+        binding.textView.append(string)
+        Expression.addOperator(string)
+    }
+
+    private fun onClickEqualButton(){
+        val value = Expression.getResult()
         binding.textView.text = value.toString()
     }
 
-    fun onClickDelete(view: View) {
+    private fun onClickDelete() {
         binding.textView.text = ""
+        Expression.reset()
     }
 }
