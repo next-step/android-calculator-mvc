db025ad7b79c3869cda1cb0fddb06aba5d8609ed
AnKyungMoo <sodp2@naver.com>
2022-07-21 15:34:15 +0900

    feat: 식 입력 관련 코드를 UI에 적용

--- app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
+++ app/src/main/java/edu/nextstep/camp/calculator/MainActivity.kt
@@ -2,25 +2,83 @@ package edu.nextstep.camp.calculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import edu.nextstep.calculator.domain.Editor
+import edu.nextstep.calculator.domain.Operator
 import edu.nextstep.camp.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val editor = Editor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button0.setOnClickListener { binding.textView.text = "0" }
-        binding.button1.setOnClickListener { binding.textView.text = "1" }
-        binding.button2.setOnClickListener { binding.textView.text = "2" }
-        binding.button3.setOnClickListener { binding.textView.text = "3" }
-        binding.button4.setOnClickListener { binding.textView.text = "4" }
-        binding.button5.setOnClickListener { binding.textView.text = "5" }
-        binding.button6.setOnClickListener { binding.textView.text = "6" }
-        binding.button7.setOnClickListener { binding.textView.text = "7" }
-        binding.button8.setOnClickListener { binding.textView.text = "8" }
-        binding.button9.setOnClickListener { binding.textView.text = "9" }
+        bindViews()
+    }
+
+    private fun bindViews() {
+        binding.button0.setOnClickListener {
+            editor.input("0")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button1.setOnClickListener {
+            editor.input("1")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button2.setOnClickListener {
+            editor.input("2")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button3.setOnClickListener {
+            editor.input("3")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button4.setOnClickListener {
+            editor.input("4")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button5.setOnClickListener {
+            editor.input("5")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button6.setOnClickListener {
+            editor.input("6")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button7.setOnClickListener {
+            editor.input("7")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button8.setOnClickListener {
+            editor.input("8")
+            binding.textView.text = editor.getExpression()
+        }
+        binding.button9.setOnClickListener {
+            editor.input("9")
+            binding.textView.text = editor.getExpression()
+        }
+
+        binding.buttonDelete.setOnClickListener {
+            editor.erase()
+            binding.textView.text = editor.getExpression()
+        }
+        binding.buttonPlus.setOnClickListener {
+            editor.input(Operator.PLUS)
+            binding.textView.text = editor.getExpression()
+        }
+        binding.buttonMinus.setOnClickListener {
+            editor.input(Operator.MINUS)
+            binding.textView.text = editor.getExpression()
+        }
+        binding.buttonMultiply.setOnClickListener {
+            editor.input(Operator.MULTIPLY)
+            binding.textView.text = editor.getExpression()
+        }
+        binding.buttonDivide.setOnClickListener {
+            editor.input(Operator.DIVIDE)
+            binding.textView.text = editor.getExpression()
+        }
     }
 }
