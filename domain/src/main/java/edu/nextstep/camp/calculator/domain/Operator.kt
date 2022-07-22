15aaa2127e1c5a0cbc5e1c821950fa4417b24120
sinoso <tpgus123123@naver.com>
2022-07-22 16:04:24 +0900

    refactor : Operator error message 이동

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Operator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Operator.kt
@@ -1,6 +1,5 @@
 package edu.nextstep.camp.calculator.domain
 
-import edu.nextstep.camp.calculator.domain.CalculatorExceptionType.OPERATOR_MUST_PREFIXED
 import java.lang.IllegalArgumentException
 
 enum class Operator(
@@ -12,12 +11,15 @@ enum class Operator(
     DIVIDE(symbol = "÷", operatorBlock = { oldValue, newValue -> oldValue / newValue }),
     MULTIPLY(symbol = "×", operatorBlock = { oldValue, newValue -> oldValue * newValue });
 
-    fun execute(oldValue: Double, newValue: Double):Double = operatorBlock.invoke(oldValue, newValue)
+    fun execute(oldValue: Double, newValue: Double): Double =
+        operatorBlock.invoke(oldValue, newValue)
 
     companion object {
+        const val OPERATOR_MUST_PREFIXED: String = "미리 정해지지 않은 연산자 입니다."
+
         fun findBySymbol(symbol: String): Operator =
             values().find { it.symbol == symbol } ?: throw IllegalArgumentException(
-               "Operator : $symbol  $OPERATOR_MUST_PREFIXED"
+                "Operator : $symbol  $OPERATOR_MUST_PREFIXED"
             )
     }
 }
