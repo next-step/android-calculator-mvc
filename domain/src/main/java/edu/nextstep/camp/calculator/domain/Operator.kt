639387d3b388dc87dea96fe980a6c148e51f6bc8
sinoso <tpgus123123@naver.com>
2022-07-22 18:01:52 +0900

    refactor : Operator 수정

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Operator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Operator.kt
@@ -5,7 +5,7 @@ import java.lang.IllegalArgumentException
 enum class Operator(
     val symbol: String,
     val operatorBlock: (oldValue: Double, newValue: Double) -> Double
-) {
+) : Operational {
     SUM(symbol = "+", operatorBlock = { oldValue, newValue -> oldValue + newValue }),
     MINUS(symbol = "-", operatorBlock = { oldValue, newValue -> oldValue - newValue }),
     DIVIDE(symbol = "÷", operatorBlock = { oldValue, newValue -> oldValue / newValue }),
@@ -14,6 +14,12 @@ enum class Operator(
     fun execute(oldValue: Double, newValue: Double): Double =
         operatorBlock.invoke(oldValue, newValue)
 
+    override fun isOperand(): Boolean = false
+
+    override fun isRemoveAble(): Boolean = true
+
+    override fun toString(): String = symbol
+
     companion object {
         const val OPERATOR_MUST_PREFIXED: String = "미리 정해지지 않은 연산자 입니다."
 
@@ -21,5 +27,10 @@ enum class Operator(
             values().find { it.symbol == symbol } ?: throw IllegalArgumentException(
                 "Operator : $symbol  $OPERATOR_MUST_PREFIXED"
             )
+
+        fun isThisOperator(c: Char): Boolean {
+            val symbol = c.toString()
+            return values().find { it.symbol == symbol } != null
+        }
     }
 }
