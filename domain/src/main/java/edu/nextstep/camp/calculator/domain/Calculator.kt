2cb539297cd407ecbced745ec53c22e070461f55
Sangmeebee <apfhdznzl@gmail.com>
2022-07-23 09:15:13 +0900

    fix: input에 들어오는 값을 non-null String으로 제한 관련이슈: step3-문자열 계산기

--- domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
+++ domain/src/main/java/edu/nextstep/camp/calculator/domain/Calculator.kt
@@ -2,8 +2,8 @@ package edu.nextstep.camp.calculator.domain
 
 class Calculator {
     private val calculation = Calculation()
-    fun evaluate(input: String?): Int {
-        require(!input.isNullOrEmpty()) // 입력값이 null이거나 빈 공백 문자일 경우 IllegalArgumentException
+    fun evaluate(input: String): Int {
+        require(input.isNotEmpty()) // 입력값이 빈 공백 문자일 경우 IllegalArgumentException
         val tokens: List<Token> = input.split(" ").map { TokenFactory(it) }
         calculation.calculate(tokens)
             .onSuccess { result ->
