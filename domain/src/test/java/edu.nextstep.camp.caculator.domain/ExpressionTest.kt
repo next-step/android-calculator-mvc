c55f10793c973c431f6fd3021a9b0709b094519b
Lee-ji-soo <nbsp3@naver.com>
2022-07-23 13:47:42 +0900

    Expression 객체로 변환

--- domain/src/test/java/edu.nextstep.camp.caculator.domain/ExpressionTest.kt
+++ domain/src/test/java/edu.nextstep.camp.caculator.domain/ExpressionTest.kt
@@ -5,36 +5,38 @@ import org.junit.Before
 import org.junit.Test
 
 class ExpressionTest {
+    lateinit var expression: Expression
     @Before
     fun init(){
+        expression = Expression()
     }
 
     @Test
     fun `계산기에_1+2를_입력하면_("1 + 2")의_문자열이_나온다`() {
-        Expression.addNumber("1")
-        Expression.addOperator("+")
-        Expression.addNumber("2")
-        var actual = Expression.express
+        expression.addNumber("1")
+        expression.addOperator("+")
+        expression.addNumber("2")
+        var actual = expression.express
         Truth.assertThat(actual).isEqualTo("1 + 2")
     }
 
     @Test
     fun `계산기에_공백이_포함된_1+ 2를_입력하면_("1 + 2")의_문자열을_express에_저장한다`() {
-        Expression.addNumber("1")
-        Expression.addOperator("+")
-        Expression.addNumber(" ")
-        Expression.addNumber("2")
-        var actual = Expression.express
+        expression.addNumber("1")
+        expression.addOperator("+")
+        expression.addNumber(" ")
+        expression.addNumber("2")
+        var actual = expression.express
         Truth.assertThat(actual).isEqualTo("1 + 2")
     }
 
     @Test
     fun `계산기에_32+3를_입력하면_("32 + 3")의_문자열을_express에_저장한다`() {
-        Expression.addNumber("3")
-        Expression.addNumber("2")
-        Expression.addOperator("+")
-        Expression.addNumber("3")
-        var actual = Expression.express
+        expression.addNumber("3")
+        expression.addNumber("2")
+        expression.addOperator("+")
+        expression.addNumber("3")
+        var actual = expression.express
         Truth.assertThat(actual).isEqualTo("32 + 3")
     }
 }
