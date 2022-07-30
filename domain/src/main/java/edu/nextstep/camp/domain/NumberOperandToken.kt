31b70c7cc173b68238bed66309746a9b3bbfcab1
박찬준 <chanjun.park@navercorp.com>
2022-07-31 08:49:41 +0900

    음수의 경우 NumberOperandToken.removeLastDigit 호출시 크래시 나는 이슈 수정

--- domain/src/main/java/edu/nextstep/camp/domain/NumberOperandToken.kt
+++ domain/src/main/java/edu/nextstep/camp/domain/NumberOperandToken.kt
@@ -12,6 +12,8 @@ class NumberOperandToken @Throws(NumberFormatException::class) constructor(
 	fun removeLastDigit(): NumberOperandToken? {
 		return when {
 			token.length == 1 -> null
+			token[token.lastIndex - 1] == NEGATIVE_NUMBER_INDICATOR -> null
+			token[token.lastIndex - 1] == POSITIVE_NUMBER_INDICATOR -> null
 			token[token.lastIndex - 1] == FLOATING_POINT_CHARACTER -> NumberOperandToken(token.substring(0, token.lastIndex - 1))
 			else -> NumberOperandToken(token.substring(0, token.lastIndex))
 		}
@@ -19,5 +21,7 @@ class NumberOperandToken @Throws(NumberFormatException::class) constructor(
 
 	companion object {
 		private const val FLOATING_POINT_CHARACTER = '.'
+		private const val NEGATIVE_NUMBER_INDICATOR = '-'
+		private const val POSITIVE_NUMBER_INDICATOR = '+'
 	}
 }
