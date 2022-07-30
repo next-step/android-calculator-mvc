ba42f11ac18e98f788722bd43b0dd9f2539b458f
박찬준 <chanjun.park@navercorp.com>
2022-07-31 08:58:15 +0900

    테스트 케이스 보강

--- domain/src/test/kotlin/edu/nextstep/camp/domain/ExpressionValidityCheckerImplTest.kt
+++ domain/src/test/kotlin/edu/nextstep/camp/domain/ExpressionValidityCheckerImplTest.kt
@@ -14,6 +14,24 @@ class ExpressionValidityCheckerImplTest {
     }
 
     @Test(expected = IllegalArgumentException::class)
+    fun `null 을 전달하는 경우 IllegalArgumentException 를 throw 해야 한다`() {
+        //given
+        val targetExpression = null
+
+        // when
+        expressionValidityCheckerImpl.checkOrThrowException(targetExpression)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `공백 문자를 전달하는 경우 IllegalArgumentException 를 throw 해야 한다`() {
+        //given
+        val targetExpression = " "
+
+        // when
+        expressionValidityCheckerImpl.checkOrThrowException(targetExpression)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
     fun `연산식이 빈 문자열인 경우 IllegalArgumentException 를 throw 해야 한다`() {
         //given
         val targetExpression = ""
@@ -41,6 +59,15 @@ class ExpressionValidityCheckerImplTest {
     }
 
     @Test(expected = IllegalArgumentException::class)
+    fun `3 & 4 과 같이 유효하지 않은 사칙 기호 문자가 포함되어 있는 경우 IllegalArgumentException 를 throw 해야한다`() {
+        // given
+        val targetExpression = "3 & 4"
+
+        // when
+        expressionValidityCheckerImpl.checkOrThrowException(targetExpression)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
     fun `a + 1 과 같이 연산식에 숫자, 사칙기호가 아닌 문자가 들어간 경우 IllegalArgumentException 를 throw 해야 한다`() {
         // given
         val targetExpression = "a + 1"
@@ -62,6 +89,15 @@ class ExpressionValidityCheckerImplTest {
     fun `1 + 1(소수점) 과 같이 피연산자의 소수점 아래 숫자들이 나타나지 않은 경우 IllegalArgumentException 를 throw 해야 한다`() {
         // given
         val targetExpression = "1 + 1."
+
+        // when
+        expressionValidityCheckerImpl.checkOrThrowException(targetExpression)
+    }
+
+    @Test
+    fun `1 + 2 + 3 과 같이 유효한 연산식인 경우 예외를 throw 하지 않고 정삭적으로 함수 실행을 종료해야한다`() {
+        // given
+        val targetExpression = "1 + 2 + 3"
 
         // when
         expressionValidityCheckerImpl.checkOrThrowException(targetExpression)
