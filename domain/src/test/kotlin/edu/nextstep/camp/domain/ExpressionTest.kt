b7900ceb59357b5d8c6d231dcdb975309784773f
박찬준 <chanjun.park@navercorp.com>
2022-07-31 07:36:56 +0900

    테스트 코드상에서 반복되는 로직 Before 에서 수행하도록 변경

--- domain/src/test/kotlin/edu/nextstep/camp/domain/ExpressionTest.kt
+++ domain/src/test/kotlin/edu/nextstep/camp/domain/ExpressionTest.kt
@@ -1,15 +1,20 @@
 package edu.nextstep.camp.domain
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 @Suppress("NonAsciiCharacters")
 class ExpressionTest {
-
+	private lateinit var expression: Expression
+	
+	@Before
+	fun setup() {
+		expression = Expression()
+	}
+	
 	@Test
 	fun `빈 연산식일때 피연산자 토큰을 추가하면 해당 피연산자만 있는 연산식이 되어야 한다`() {
-		var expression = Expression()
-
 		expression += NumberOperandToken("0")
 
 		assertThat(expression.toString()).isEqualTo("0")
@@ -17,7 +22,6 @@ class ExpressionTest {
 
 	@Test
 	fun `5 + 연산식이 있을때 피연산자 0 토큰을 추가하면 5 + 0 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("5")
 		expression += OperatorToken.Addition
 
@@ -28,7 +32,6 @@ class ExpressionTest {
 
 	@Test
 	fun `8 연산식이 있을때 9 피연산자를 입력하면 89 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("8")
 
 		expression += NumberOperandToken("9")
@@ -38,8 +41,6 @@ class ExpressionTest {
 
 	@Test
 	fun `빈 연산식일때 + 연산자 토큰을 추가하면 계속 빈 연산식이 되어야 한다`() {
-		var expression = Expression()
-
 		expression += OperatorToken.Addition
 
 		assertThat(expression.toString()).isEqualTo("")
@@ -47,8 +48,6 @@ class ExpressionTest {
 
 	@Test
 	fun `빈 연산식일때 - 연산자 토큰을 추가하면 계속 빈 연산식이 되어야 한다`() {
-		var expression = Expression()
-
 		expression += OperatorToken.Subtraction
 
 		assertThat(expression.toString()).isEqualTo("")
@@ -56,8 +55,6 @@ class ExpressionTest {
 
 	@Test
 	fun `빈 연산식일때 × 연산자 토큰을 추가하면 계속 빈 연산식이 되어야 한다`() {
-		var expression = Expression()
-
 		expression += OperatorToken.Multiplication
 
 		assertThat(expression.toString()).isEqualTo("")
@@ -65,8 +62,6 @@ class ExpressionTest {
 
 	@Test
 	fun `빈 연산식일때 ÷ 연산자 토큰을 추가하면 계속 빈 연산식이 되어야 한다`() {
-		var expression = Expression()
-
 		expression += OperatorToken.Division
 
 		assertThat(expression.toString()).isEqualTo("")
@@ -74,7 +69,6 @@ class ExpressionTest {
 
 	@Test
 	fun `8 연산식이 있을때 + 연산자를 입력하면 8 + 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("8")
 
 		expression += OperatorToken.Addition
@@ -84,7 +78,6 @@ class ExpressionTest {
 
 	@Test
 	fun `8 + 연산식이 있을때 - 연산자를 입력하면 8 - 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("8")
 		expression += OperatorToken.Addition
 
@@ -95,8 +88,6 @@ class ExpressionTest {
 
 	@Test
 	fun `빈 연산식일때 마지막 기호를 지우는 경우 계속 빈 연산식이 되어야 한다`() {
-		var expression = Expression()
-
 		expression = expression.removeLastCharacter()
 
 		assertThat(expression.toString()).isEqualTo("")
@@ -104,7 +95,6 @@ class ExpressionTest {
 
 	@Test
 	fun `32 + 1 연산식이 있을때 마지막 기호를 지우는 경우 32 + 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("32")
 		expression += OperatorToken.Addition
 		expression += NumberOperandToken("1")
@@ -116,7 +106,6 @@ class ExpressionTest {
 
 	@Test
 	fun `32 + 연산식이 있을때 마지막 기호를 지우는 경우 32 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("32")
 		expression += OperatorToken.Addition
 
@@ -127,7 +116,6 @@ class ExpressionTest {
 
 	@Test
 	fun `32 연산식이 있을때 마지막 기호를 지우는 경우 3 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("32")
 
 		expression = expression.removeLastCharacter()
@@ -137,7 +125,6 @@ class ExpressionTest {
 
 	@Test
 	fun `3 연산식이 있을때 마지막 기호를 지우는 경우 빈 연산식이 되어야 한다`() {
-		var expression = Expression()
 		expression += NumberOperandToken("3")
 
 		expression = expression.removeLastCharacter()
